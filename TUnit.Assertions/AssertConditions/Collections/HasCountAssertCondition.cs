30cbf09b02d131dac4d29cde36501122a3424d42
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-01-28 14:12:17 +0000

    HasCountAssertCondition

---
+++ TUnit.Assertions/AssertConditions/Collections/HasCountAssertCondition.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+
+namespace TUnit.Assertions.AssertConditions.Collections;
+
+public class HasCountAssertCondition<TActual> : ExpectedValueAssertCondition<IEnumerable<TActual>, int>
+{
+    public HasCountAssertCondition(int expected) : base(expected)
+    {
+    }
+
+    public override string DefaultMessage => $"Length is {GetCount(ActualValue)} instead of {ExpectedValue}";
+    
+    protected override bool Passes(IEnumerable<TActual> actualValue)
+    {
+        return GetCount(actualValue) == ExpectedValue;
+    }
+
+    private int GetCount(IEnumerable<TActual> actualValue)
+    {
+        if (actualValue is ICollection collection)
+        {
+            return collection.Count;
+        }
+
+        if (actualValue is TActual[] array)
+        {
+            return array.Length;
+        }
+        
+        return actualValue.Count();
+    }
+}
