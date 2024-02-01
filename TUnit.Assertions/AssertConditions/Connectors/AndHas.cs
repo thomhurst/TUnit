4b677ac4518b561b0b8e936404af372f2b2095a4
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-02-01 15:03:16 +0000

    Fix for length and count strong types

--- TUnit.Assertions/AssertConditions/Connectors/AndHas.cs
+++ TUnit.Assertions/AssertConditions/Connectors/AndHas.cs
@@ -11,9 +11,10 @@ public class AndHas<TActual>
         _otherAssertConditions = otherAssertConditions;
     }
     
-    public Property<TActual> Count => new("Count", ConnectorType.And, _otherAssertConditions);
-    public Property<TActual> Length => new("Length", ConnectorType.And, _otherAssertConditions);
+    public Property<TActual, int> Count => new("Count", ConnectorType.And, _otherAssertConditions);
+    public Property<TActual, int> Length => new("Length", ConnectorType.And, _otherAssertConditions);
     public Property<TActual> Value => new("Value", ConnectorType.And, _otherAssertConditions);
     
     public Property<TActual> Property(string name) => new(name, ConnectorType.And, _otherAssertConditions);
+    public Property<TActual, T> Property<T>(string name) => new(name, ConnectorType.And, _otherAssertConditions);
 }
