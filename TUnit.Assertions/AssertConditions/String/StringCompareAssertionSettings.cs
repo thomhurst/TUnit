db84fbd537d3e5647f3b0c1ad779a61da0e82ef5
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-02-03 16:38:21 +0000

    Remove clutter

--- TUnit.Assertions/AssertConditions/String/StringCompareAssertionSettings.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace TUnit.Assertions.AssertConditions.String;
-
-public record StringCompareAssertionSettings
-{
-    public StringComparison StringComparison { get; init; } = StringComparison.Ordinal;
-    public bool Trim { get; init; } = false;
-};
