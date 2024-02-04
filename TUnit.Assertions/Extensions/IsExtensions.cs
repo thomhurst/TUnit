ecfe9d60e662a15e45dad1544d0e2ba5b08ab92d
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-02-04 23:04:34 +0000

    Equivalent to nullability generic fix

--- TUnit.Assertions/Extensions/IsExtensions.cs
+++ TUnit.Assertions/Extensions/IsExtensions.cs
@@ -152,12 +152,12 @@ public static class IsExtensions
 
     #region Enumerables
 
-    public static BaseAssertCondition<TActual, TAnd, TOr> EquivalentTo<TActual, TInner, TAnd, TOr>(this Is<TActual, TAnd, TOr> @is, IEnumerable<TInner> expected)
-        where TActual : IEnumerable<TInner>
+    public static BaseAssertCondition<TActual, TAnd, TOr> EquivalentTo<TActual, TInner, TAnd, TOr>(this Is<TActual, TAnd, TOr> @is, IEnumerable<TInner?> expected)
+        where TActual : IEnumerable<TInner?>
         where TAnd : And<TActual, TAnd, TOr>, IAnd<TAnd, TActual, TAnd, TOr>
         where TOr : Or<TActual, TAnd, TOr>, IOr<TOr, TActual, TAnd, TOr>
     {
-        return @is.Wrap(new EnumerableEquivalentToAssertCondition<TActual, TInner, TAnd, TOr>(@is.AssertionBuilder, expected));
+        return @is.Wrap(new EnumerableEquivalentToAssertCondition<TActual, TInner?, TAnd, TOr>(@is.AssertionBuilder, expected));
     }
     
     public static BaseAssertCondition<TActual, TAnd, TOr> Empty<TActual, TInner, TAnd, TOr>(this Is<TActual, TAnd, TOr> @is)
