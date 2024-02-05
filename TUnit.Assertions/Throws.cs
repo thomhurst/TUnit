41ebf88bf5d6ece15d5772e038463337a5704a6b
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-02-05 00:38:00 +0000

    Tidy

--- TUnit.Assertions/Throws.cs
+++ TUnit.Assertions/Throws.cs
@@ -30,11 +30,7 @@ public class Throws<TActual, TAnd, TOr> : Connector<TActual, TAnd, TOr>
     public BaseAssertCondition<TActual, TAnd, TOr> WithCustomCondition(Func<Exception?, bool> action, Func<Exception?, string> messageFactory) =>
         Wrap(new DelegateAssertCondition<TActual,Exception,TAnd,TOr>(AssertionBuilder,
             default,
-            (actual, exception, arg3) =>
-            {
-                return action(exception);
-            },
+            (actual, exception, arg3) => action(exception),
             (actual, exception) => messageFactory(exception)
         ));
-
 }
