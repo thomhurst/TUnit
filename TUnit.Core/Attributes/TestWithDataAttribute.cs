fee3fa6f9a1441bd32593ef3a22962f6c034b97f
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-02-05 19:17:39 +0000

    Allow nulls in TestWithDataAttribute

--- TUnit.Core/Attributes/TestWithDataAttribute.cs
+++ TUnit.Core/Attributes/TestWithDataAttribute.cs
@@ -5,8 +5,8 @@ public class TestWithDataAttribute : TUnitAttribute
 {
     public object?[] Values { get; }
 
-    public TestWithDataAttribute(params object?[] values)
+    public TestWithDataAttribute(params object?[]? values)
     {
-        Values = values;
+        Values = values ?? [null];
     }
 }
