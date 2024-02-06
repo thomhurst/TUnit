0ee548c0b7a128fd87f0376ab7c461ba0ad758aa
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-02-06 00:23:41 +0000

    ExceptionMessage

---
+++ TUnit.Assertions/Extensions/ExceptionMessage.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using TUnit.Assertions.AssertConditions;
+using TUnit.Assertions.AssertConditions.Operators;
+
+namespace TUnit.Assertions;
+
+public class ExceptionMessage<TActual, TAnd, TOr> : Connector<TActual, TAnd, TOr> 
+    where TActual : Exception
+    where TAnd : And<TActual, TAnd, TOr>, IAnd<TAnd, TActual, TAnd, TOr>
+    where TOr : Or<TActual, TAnd, TOr>, IOr<TOr, TActual, TAnd, TOr>
+{
+    protected internal AssertionBuilder<TActual> AssertionBuilder { get; }
+
+    public ExceptionMessage(AssertionBuilder<TActual> assertionBuilder, ConnectorType connectorType,
+        BaseAssertCondition<TActual, TAnd, TOr>? otherAssertCondition) : base(connectorType, otherAssertCondition)
+    {
+        AssertionBuilder = assertionBuilder;
+    }
+
+    public BaseAssertCondition<TActual, TAnd, TOr> EqualTo(string expected)
+    {
+        return EqualTo(expected, StringComparison.Ordinal);
+    }
+
+    public BaseAssertCondition<TActual, TAnd, TOr> EqualTo(string expected, StringComparison stringComparison)
+    {
+        return Wrap(new DelegateAssertCondition<TActual, string, TAnd, TOr>(AssertionBuilder, expected, (_, _, exception) =>
+            {
+                ArgumentNullException.ThrowIfNull(exception);
+                return string.Equals(exception.Message, expected, stringComparison);
+            },
+            (_, exception) =>
+                $"Exception had a message of '{exception?.Message}' instead of '{expected}'")
+        );
+    }
+}
--- TUnit.Assertions/Extensions/HasExtensions.cs
+++ TUnit.Assertions/Extensions/HasExtensions.cs
@@ -5,6 +5,14 @@ namespace TUnit.Assertions;
 
 public static class HasExtensions
 {
+    public static ExceptionMessage<TActual, TAnd, TOr> Message<TActual, TAnd, TOr>(this Has<TActual, TAnd, TOr> has) 
+        where TActual : Exception
+        where TAnd : And<TActual, TAnd, TOr>, IAnd<TAnd, TActual, TAnd, TOr>
+        where TOr : Or<TActual, TAnd, TOr>, IOr<TOr, TActual, TAnd, TOr>
+    {
+        return new ExceptionMessage<TActual, TAnd, TOr>(has.AssertionBuilder, has.ConnectorType, has.OtherAssertCondition);
+    }
+    
     public static EnumerableCount<TActual, TAnd, TOr> Count<TActual, TAnd, TOr>(this Has<TActual, TAnd, TOr> has) 
         where TActual : IEnumerable?
         where TAnd : And<TActual, TAnd, TOr>, IAnd<TAnd, TActual, TAnd, TOr>
