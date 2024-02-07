7e365bde9014f35b371c41000a1c6af3527d7fd6
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-02-07 19:00:39 +0000

    Caller expression

--- TUnit.Assertions/AssertionBuilder.cs
+++ TUnit.Assertions/AssertionBuilder.cs
@@ -2,5 +2,22 @@
 
 public abstract class AssertionBuilder<TActual>
 {
+    private readonly string? _callerExpression;
+
+    public AssertionBuilder(string? callerExpression)
+    {
+        _callerExpression = callerExpression;
+    }
+
+    internal string GetCallerExpressionPrefix()
+    {
+        if (string.IsNullOrEmpty(_callerExpression))
+        {
+            return string.Empty;
+        }
+
+        return $"{_callerExpression}: ";
+    }
+    
     protected internal abstract Task<AssertionData<TActual>> GetAssertionData();
 }
