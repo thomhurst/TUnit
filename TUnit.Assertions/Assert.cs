7558830a0fc523a556e110ddb6390bdf8ce55df2
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-02-07 20:12:43 +0000

    Non-generic ThrowsAsync

--- TUnit.Assertions/Assert.cs
+++ TUnit.Assertions/Assert.cs
@@ -41,6 +41,10 @@ public static class Assert
     {
         throw new AssertionException(reason);
     }
+
+    public static Task<Exception> ThrowsAsync(Func<Task> @delegate,
+        [CallerArgumentExpression("delegate")] string? doNotPopulateThisValue = null)
+        => ThrowsAsync<Exception>(@delegate, doNotPopulateThisValue);
     
     public static async Task<TException> ThrowsAsync<TException>(Func<Task> @delegate, [CallerArgumentExpression("delegate")] string? doNotPopulateThisValue = null) where TException : Exception
     {
@@ -61,6 +65,10 @@ public static class Assert
 
         return null!;
     }
+
+    public static Exception ThrowsAsync(Action @delegate,
+        [CallerArgumentExpression("delegate")] string? doNotPopulateThisValue = null)
+        => ThrowsAsync<Exception>(@delegate, doNotPopulateThisValue);
     
     public static TException ThrowsAsync<TException>(Action @delegate, [CallerArgumentExpression("delegate")] string? doNotPopulateThisValue = null) where TException : Exception
     {
