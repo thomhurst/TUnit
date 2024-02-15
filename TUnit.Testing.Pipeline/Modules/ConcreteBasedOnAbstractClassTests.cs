cea7f93a0f1f8c0de57ec62306e93c44eadb77bc
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-02-15 18:17:00 +0000

    Testing Pipeline

---
+++ TUnit.Testing.Pipeline/Modules/ConcreteBasedOnAbstractClassTests.cs
@@ -0,0 +1,12 @@
+﻿using ModularPipelines.Context;
+using ModularPipelines.Models;
+
+namespace TUnit.Testing.Pipeline;
+
+public class ConcreteBasedOnAbstractClassTests : TestModule
+{
+    protected override async Task<CommandResult?> ExecuteAsync(IPipelineContext context, CancellationToken cancellationToken)
+    {
+        return await RunTestsWithFilter(context, "TestClass=ConcreteClass1,ConcreteClass2");
+    }
+}
---
+++ TUnit.Testing.Pipeline/Modules/TestModule.cs
@@ -0,0 +1,24 @@
+﻿using ModularPipelines.Context;
+using ModularPipelines.DotNet.Extensions;
+using ModularPipelines.DotNet.Options;
+using ModularPipelines.Extensions;
+using ModularPipelines.Git.Extensions;
+using ModularPipelines.Models;
+using ModularPipelines.Modules;
+
+namespace TUnit.Testing.Pipeline;
+
+
+public abstract class TestModule : Module<CommandResult>
+{
+    protected async Task<CommandResult> RunTestsWithFilter(IPipelineContext context, string filter)
+    {
+        var project = context.Git().RootDirectory.FindFile(x => x.Name == "TUnit.TestProject.csproj").AssertExists();
+
+        return await context.DotNet().Test(new DotNetTestOptions(project)
+        {
+            NoBuild = true,
+            Filter = filter
+        });
+    }
+}
