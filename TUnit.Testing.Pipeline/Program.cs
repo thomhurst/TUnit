54fa4b5eb7b676ead01e4b9ecef52d7bd7013d9b
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-02-15 23:19:01 +0000

    Configure inner pipeline printing

--- TUnit.Testing.Pipeline/Program.cs
+++ TUnit.Testing.Pipeline/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Extensions.Logging;
+using ModularPipelines.Enums;
 using ModularPipelines.Extensions;
 using ModularPipelines.Host;
+using ModularPipelines.Options;
 using TUnit.Testing.Pipeline;
 
 await PipelineHostBuilder.Create()
@@ -9,4 +11,11 @@ await PipelineHostBuilder.Create()
         collection.AddModulesFromAssembly(typeof(Program).Assembly);
     })
     .AddRequirement<BuiltTestProjectRequirement>()
+    .ConfigurePipelineOptions((_, options) =>
+    {
+        options.PrintResults = false;
+        options.DefaultCommandLogging = CommandLogging.Input | CommandLogging.Error;
+        options.ShowProgressInConsole = false;
+        options.ExecutionMode = ExecutionMode.WaitForAllModules;
+    })
     .ExecutePipelineAsync();
