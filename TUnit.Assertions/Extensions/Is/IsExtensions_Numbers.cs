98f67c8566ea0d8cfaf835298c93c7ca453439e3
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-02-18 23:19:50 +0000

    Add numbers back

---
+++ TUnit.Assertions/Extensions/Is/IsExtensions_Numbers.cs
@@ -0,0 +1,181 @@
+﻿#nullable disable
+
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using TUnit.Assertions.AssertConditions;
+using TUnit.Assertions.AssertConditions.Generic;
+using TUnit.Assertions.AssertConditions.Operators;
+
+namespace TUnit.Assertions.Extensions.Is;
+
+public static partial class IsExtensions
+{
+    public static BaseAssertCondition<TActual, TAnd, TOr> EqualToWithTolerance<TActual, TAnd, TOr>(this Is<TActual, TAnd, TOr> @is, TActual expected, TActual tolerance, [CallerArgumentExpression("expected")] string doNotPopulateThisValue1 = "", [CallerArgumentExpression("tolerance")] string doNotPopulateThisValue2 = "")
+        where TActual : INumber<TActual>
+        where TAnd : And<TActual, TAnd, TOr>, IAnd<TAnd, TActual, TAnd, TOr>
+        where TOr : Or<TActual, TAnd, TOr>, IOr<TOr, TActual, TAnd, TOr>
+    {
+        return @is.Wrap(new DelegateAssertCondition<TActual,TActual,TAnd,TOr>(
+            @is.AssertionBuilder.AppendCallerMethodWithMultipleExpressions([doNotPopulateThisValue1, doNotPopulateThisValue2]), 
+            expected,
+            (actual, _, _, self) =>
+            {
+                ArgumentNullException.ThrowIfNull(actual);
+                ArgumentNullException.ThrowIfNull(expected);
+                
+                return actual <= expected + tolerance && actual >= expected - tolerance;
+            },
+            (number, _) => $"{number} is not between {number! - tolerance} and {number! + tolerance}"));
+    }
+    
+    public static BaseAssertCondition<TActual, TAnd, TOr> Zero<TActual, TAnd, TOr>(this Is<TActual, TAnd, TOr> @is)
+        where TActual : INumber<TActual>
+        where TAnd : And<TActual, TAnd, TOr>, IAnd<TAnd, TActual, TAnd, TOr>
+        where TOr : Or<TActual, TAnd, TOr>, IOr<TOr, TActual, TAnd, TOr>
+    {
+        return @is.Wrap(new EqualsAssertCondition<TActual, TAnd, TOr>(@is.AssertionBuilder.AppendCallerMethod(null), TActual.Zero));
+    }
+    
+    public static BaseAssertCondition<TActual, TAnd, TOr> GreaterThan<TActual, TAnd, TOr>(this Is<TActual, TAnd, TOr> @is, TActual expected, [CallerArgumentExpression("expected")] string doNotPopulateThisValue = "") where TActual : INumber<TActual>
+        where TAnd : And<TActual, TAnd, TOr>, IAnd<TAnd, TActual, TAnd, TOr>
+        where TOr : Or<TActual, TAnd, TOr>, IOr<TOr, TActual, TAnd, TOr>
+    {
+        return @is.Wrap(new DelegateAssertCondition<TActual, TActual, TAnd, TOr>(@is.AssertionBuilder.AppendCallerMethod(doNotPopulateThisValue), expected, (value, _, _, self) =>
+            {
+                if (value is null)
+                {
+                    self.WithMessage((_, _) => $"{@is.AssertionBuilder.RawActualExpression ?? typeof(TActual).Name} is null");
+                    return false;
+                }
+                
+                return value > expected;
+            },
+            (value, _) => $"{value} was not greater than {expected}"));
+    }
+    
+    public static BaseAssertCondition<TActual, TAnd, TOr> GreaterThanOrEqualTo<TActual, TAnd, TOr>(this Is<TActual, TAnd, TOr> @is, TActual expected, [CallerArgumentExpression("expected")] string doNotPopulateThisValue = "") 
+        where TActual : INumber<TActual>
+        where TAnd : And<TActual, TAnd, TOr>, IAnd<TAnd, TActual, TAnd, TOr>
+        where TOr : Or<TActual, TAnd, TOr>, IOr<TOr, TActual, TAnd, TOr>
+    {
+        return @is.Wrap(new DelegateAssertCondition<TActual, TActual, TAnd, TOr>(@is.AssertionBuilder.AppendCallerMethod(doNotPopulateThisValue), default, (value, _, _, self) =>
+            {
+                if (value is null)
+                {
+                    self.WithMessage((_, _) => $"{@is.AssertionBuilder.RawActualExpression ?? typeof(TActual).Name} is null");
+                    return false;
+                }
+                
+                return value >= expected;
+            },
+            (value, _) => $"{value} was not greater than or equal to {expected}"));
+    }
+    
+    public static BaseAssertCondition<TActual, TAnd, TOr> LessThan<TActual, TAnd, TOr>(this Is<TActual, TAnd, TOr> @is, TActual expected, [CallerArgumentExpression("expected")] string doNotPopulateThisValue = "") 
+        where TActual : INumber<TActual>
+        where TAnd : And<TActual, TAnd, TOr>, IAnd<TAnd, TActual, TAnd, TOr>
+        where TOr : Or<TActual, TAnd, TOr>, IOr<TOr, TActual, TAnd, TOr>
+    {
+        return @is.Wrap(new DelegateAssertCondition<TActual, TActual, TAnd, TOr>(@is.AssertionBuilder.AppendCallerMethod(doNotPopulateThisValue), default, (value, _, _, self) =>
+            {
+                if (value is null)
+                {
+                    self.WithMessage((_, _) => $"{@is.AssertionBuilder.RawActualExpression ?? typeof(TActual).Name} is null");
+                    return false;
+                }
+
+                return value < expected;
+            },
+            (value, _) => $"{value} was not less than {expected}"));
+    }
+    
+    public static BaseAssertCondition<TActual, TAnd, TOr> LessThanOrEqualTo<TActual, TAnd, TOr>(this Is<TActual, TAnd, TOr> @is, TActual expected, [CallerArgumentExpression("expected")] string doNotPopulateThisValue = "") 
+        where TActual : INumber<TActual>
+        where TAnd : And<TActual, TAnd, TOr>, IAnd<TAnd, TActual, TAnd, TOr>
+        where TOr : Or<TActual, TAnd, TOr>, IOr<TOr, TActual, TAnd, TOr>
+    {
+        return @is.Wrap(new DelegateAssertCondition<TActual, TActual, TAnd, TOr>(@is.AssertionBuilder.AppendCallerMethod(doNotPopulateThisValue), default, (value, _, _, self) =>
+            {
+                if (value is null)
+                {
+                    self.WithMessage((_, _) => $"{@is.AssertionBuilder.RawActualExpression ?? typeof(TActual).Name} is null");
+                    return false;
+                }
+
+                return value <= expected;
+            },
+            (value, _) => $"{value} was not less than or equal to {expected}"));
+    }
+    
+    public static BaseAssertCondition<TActual, TAnd, TOr> Even<TActual, TAnd, TOr>(this Is<TActual, TAnd, TOr> @is) 
+        where TActual : INumber<TActual>, IModulusOperators<TActual, int, int>
+        where TAnd : And<TActual, TAnd, TOr>, IAnd<TAnd, TActual, TAnd, TOr>
+        where TOr : Or<TActual, TAnd, TOr>, IOr<TOr, TActual, TAnd, TOr>
+    {
+        return @is.Wrap(new DelegateAssertCondition<TActual, TActual, TAnd, TOr>(@is.AssertionBuilder.AppendCallerMethod(null), default, (value, _, _, self) =>
+            {
+                if (value is null)
+                {
+                    self.WithMessage((_, _) => $"{@is.AssertionBuilder.RawActualExpression ?? typeof(TActual).Name} is null");
+                    return false;
+                }
+
+                return value % 2 == 0;
+            },
+            (value, _) => $"{value} was not even"));
+    }
+    
+    public static BaseAssertCondition<TActual, TAnd, TOr> Odd<TActual, TAnd, TOr>(this Is<TActual, TAnd, TOr> @is) 
+        where TActual : INumber<TActual>, IModulusOperators<TActual, int, int>
+        where TAnd : And<TActual, TAnd, TOr>, IAnd<TAnd, TActual, TAnd, TOr>
+        where TOr : Or<TActual, TAnd, TOr>, IOr<TOr, TActual, TAnd, TOr>
+    {
+        return @is.Wrap(new DelegateAssertCondition<TActual, TActual, TAnd, TOr>(@is.AssertionBuilder.AppendCallerMethod(null), default, (value, _, _, self) =>
+            {
+                if (value is null)
+                {
+                    self.WithMessage((_, _) => $"{@is.AssertionBuilder.RawActualExpression ?? typeof(TActual).Name} is null");
+                    return false;
+                }
+
+                return value % 2 != 0;
+            },
+            (value, _) => $"{value} was not odd"));
+    }
+    
+    public static BaseAssertCondition<TActual, TAnd, TOr> Negative<TActual, TAnd, TOr>(this Is<TActual, TAnd, TOr> @is) 
+        where TActual : INumber<TActual>
+        where TAnd : And<TActual, TAnd, TOr>, IAnd<TAnd, TActual, TAnd, TOr>
+        where TOr : Or<TActual, TAnd, TOr>, IOr<TOr, TActual, TAnd, TOr>
+    {
+        return @is.Wrap(new DelegateAssertCondition<TActual, TActual, TAnd, TOr>(@is.AssertionBuilder.AppendCallerMethod(null), default, (value, _, _, self) =>
+            {
+                if (value is null)
+                {
+                    self.WithMessage((_, _) => $"{@is.AssertionBuilder.RawActualExpression ?? typeof(TActual).Name} is null");
+                    return false;
+                }
+
+                return value < TActual.Zero;
+            },
+            (value, _) => $"{value} was not negative"));
+    }
+    
+    public static BaseAssertCondition<TActual, TAnd, TOr> Positive<TActual, TAnd, TOr>(this Is<TActual, TAnd, TOr> @is) 
+        where TActual : INumber<TActual>
+        where TAnd : And<TActual, TAnd, TOr>, IAnd<TAnd, TActual, TAnd, TOr>
+        where TOr : Or<TActual, TAnd, TOr>, IOr<TOr, TActual, TAnd, TOr>
+    {
+        return @is.Wrap(new DelegateAssertCondition<TActual, TActual, TAnd, TOr>(@is.AssertionBuilder.AppendCallerMethod(null), default, (value, _, _, self) =>
+            {
+                if (value is null)
+                {
+                    self.WithMessage((_, _) => $"{@is.AssertionBuilder.RawActualExpression ?? typeof(TActual).Name} is null");
+                    return false;
+                }
+
+                return value > TActual.Zero;
+            },
+            (value, _) => $"{value} was not positive"));
+    }
+}
---
+++ TUnit.TestProject/MyTestClass.cs
@@ -0,0 +1,25 @@
+﻿using TUnit.Assertions;
+using TUnit.Assertions.Extensions;
+using TUnit.Assertions.Extensions.Is;
+using TUnit.Core;
+
+namespace MyTestProject;
+
+public class MyTestClass
+{
+    [CombinativeTest]
+    public async Task MyTest(
+        [CombinativeValues(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] int value1,
+        [CombinativeValues(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] int value2
+        )
+    {
+        var result = Add(value1, value2);
+
+        await Assert.That(result).Is.Positive();
+    }
+
+    private int Add(int x, int y)
+    {
+        return x + y;
+    }
+}
