9754df881409ba86ca0dbbdb85a8e234b092ca27
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-02-24 12:03:34 +0000

    Tidy usings

--- TUnit.Analyzers/TUnit.Analyzers/CombinativeTestArgumentsAnalyzer.cs
+++ TUnit.Analyzers/TUnit.Analyzers/CombinativeTestArgumentsAnalyzer.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Immutable;
 using System.Linq;
-using System.Threading;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
