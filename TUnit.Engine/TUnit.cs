653856373de88afe28a506afeba4e801545a9dbd
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-02-27 19:50:34 +0000

    OutputDirectory

---
+++ TUnit.Engine/TUnit.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+namespace TUnit.Engine;
+
+public static class TUnit
+{
+    public static string OutputDirectory 
+        => Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!;
+}
