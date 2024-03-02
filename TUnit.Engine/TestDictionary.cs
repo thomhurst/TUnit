01bdd955ea8dde8f3e6651ac08dc9709504a97f8
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-03-02 14:08:00 +0000

    Hook up source generator

---
+++ TUnit.Engine/TestDictionary.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Testing.Platform.Extensions.Messages;
+using TUnit.Core;
+
+namespace TUnit.Engine;
+
+public class TestDictionary
+{
+    private static readonly Dictionary<string, Func<Task>> Tests = new();
+
+    public static void AddTest(string testId, Func<Task> action)
+    {
+        Tests[testId] = action;
+    }
+    
+    public static void AddTest(string testId, Action action)
+    {
+        Tests[testId] = () =>
+        {
+            action();
+            return Task.CompletedTask;
+        };
+    }
+}
