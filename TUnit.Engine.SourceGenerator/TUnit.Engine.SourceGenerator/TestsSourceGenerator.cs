a26b3f1d72964ae6398bc11ed443067eca180a52
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-04-05 19:10:05 +0100

    Fix

--- TUnit.Engine.SourceGenerator/TUnit.Engine.SourceGenerator/TestsSourceGenerator.cs
+++ TUnit.Engine.SourceGenerator/TUnit.Engine.SourceGenerator/TestsSourceGenerator.cs
@@ -96,6 +96,7 @@ public class TestsSourceGenerator : IIncrementalGenerator
                    {
                         {{methodCode}}
                    }
+               } 
                """;
     }
 
