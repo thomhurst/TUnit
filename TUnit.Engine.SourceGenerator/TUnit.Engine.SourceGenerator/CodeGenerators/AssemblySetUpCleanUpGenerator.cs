d4cb162acf048edf03492b89d7f602b539de83a9
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-04-08 19:22:12 +0100

    Fix

--- TUnit.Engine.SourceGenerator/TUnit.Engine.SourceGenerator/CodeGenerators/AssemblySetUpCleanUpGenerator.cs
+++ TUnit.Engine.SourceGenerator/TUnit.Engine.SourceGenerator/CodeGenerators/AssemblySetUpCleanUpGenerator.cs
@@ -81,7 +81,7 @@ public class AssemblySetUpCleanUpGenerator : IIncrementalGenerator
             {
                 var className = $"AssemblyCleanUp_{method.ContainingType.Name}_{Guid.NewGuid():N}";
 
-                var code = $"global::TUnit.Engine.AssemblyHookOrchestrators.RegisterCleanUp(global::TUnit.Engine.RunHelpers(() => {method.ContainingType.ToDisplayString(DisplayFormats.FullyQualifiedGenericWithGlobalPrefix)}.{method.Name}()))";
+                var code = $"global::TUnit.Engine.AssemblyHookOrchestrators.RegisterCleanUp(() => global::TUnit.Engine.RunHelpers(() => {method.ContainingType.ToDisplayString(DisplayFormats.FullyQualifiedGenericWithGlobalPrefix)}.{method.Name}()))";
                 
                 context.AddSource($"{className}.g.cs", WrapInClass(className, code));
             }
