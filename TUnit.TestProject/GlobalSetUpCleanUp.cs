42ae3a10deaced1476de2239a504b8b19810b0be
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-04-08 19:18:22 +0100

    Fix

---
+++ TUnit.TestProject/GlobalSetUpCleanUp.cs
@@ -0,0 +1,22 @@
+﻿using TUnit.Core;
+
+namespace TUnit.TestProject;
+
+public static class GlobalSetUpCleanUp
+{
+    [AssemblySetUp]
+    public static void BlahSetUp()
+    {
+    }
+    
+    [AssemblySetUp]
+    public static void BlahSetUp2()
+    {
+    }
+
+    [AssemblyCleanUp]
+    public static Task BlahCleanUp()
+    {
+        return Task.CompletedTask;
+    }
+}
