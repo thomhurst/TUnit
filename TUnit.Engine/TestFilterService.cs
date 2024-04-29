ea6dc2324cf7ca8f93ffc874e11eec86715999d2
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-04-29 18:55:23 +0100

    Fix filter service

--- TUnit.Engine/TestFilterService.cs
+++ TUnit.Engine/TestFilterService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Testing.Platform.Extensions.Messages;
+using Microsoft.Testing.Platform.Logging;
 using Microsoft.Testing.Platform.Requests;
 using TUnit.Core;
 
@@ -6,6 +7,13 @@ namespace TUnit.Engine;
 
 public class TestFilterService
 {
+    private readonly ILogger<TestFilterService> _logger;
+
+    public TestFilterService(ILoggerFactory loggerFactory)
+    {
+        _logger = loggerFactory.CreateLogger<TestFilterService>();
+    }
+    
     public IEnumerable<TestInformation> FilterTests(ITestExecutionFilter? testExecutionFilter, List<TestInformation> testNodes)
     {
         if (testExecutionFilter is null)
@@ -22,8 +30,13 @@ public class TestFilterService
         {
             null => true,
             TestNodeUidListFilter testNodeUidListFilter => testNodeUidListFilter.TestNodeUids.Contains(new TestNodeUid(testInformation.TestId)),
-            BasicFilter basicFilter => true,
-            _ => throw new ArgumentOutOfRangeException(nameof(testExecutionFilter))
+            _ => UnhandledFilter(testExecutionFilter)
         };
+    }
+
+    private bool UnhandledFilter(ITestExecutionFilter testExecutionFilter)
+    {
+        _logger.LogWarning($"Filter is Unhandled Type: {testExecutionFilter.GetType().FullName}");
+        return true;
     }
 }
