0eb7ac8930d60fb58ad112a4ded5695405c64bcd
Tom Longhurst <130671542+msm-tomlonghurst@users.noreply.github.com>
2024-05-02 12:29:38 +0100

    Fix test

--- TUnit.Engine.SourceGenerator/TUnit.Engine.SourceGenerator.Tests/SetupTests.cs
+++ TUnit.Engine.SourceGenerator/TUnit.Engine.SourceGenerator.Tests/SetupTests.cs
@@ -13,9 +13,9 @@ internal class SetupTests : TestsBase<TestsGenerator>
             Assert.That(generatedFiles.Length, Is.EqualTo(2));
             
             Assert.That(generatedFiles[0], Does.Contain("TestName = \"TestServerResponse1\","));
-            Assert.That(generatedFiles[0], Does.Contain("\t\t\t\tBeforeEachTestSetUps = [classInstance => global::TUnit.Core.Helpers.RunHelpers.RunAsync(() => classInstance.BeforeEach1()),classInstance => global::TUnit.Core.Helpers.RunHelpers.RunAsync(() => classInstance.BeforeEach3()),classInstance => global::TUnit.Core.Helpers.RunHelpers.RunAsync(() => classInstance.BeforeEach3()),classInstance => global::TUnit.Core.Helpers.RunHelpers.RunAsync(() => classInstance.Setup()),],"));
+            Assert.That(generatedFiles[0], Does.Contain("\t\t\t\tBeforeEachTestSetUps = [classInstance => global::TUnit.Core.Helpers.RunHelpers.RunAsync(() => classInstance.BeforeEach1()),classInstance => global::TUnit.Core.Helpers.RunHelpers.RunAsync(() => classInstance.BeforeEach2()),classInstance => global::TUnit.Core.Helpers.RunHelpers.RunAsync(() => classInstance.BeforeEach3()),classInstance => global::TUnit.Core.Helpers.RunHelpers.RunAsync(() => classInstance.Setup()),],"));
             
             Assert.That(generatedFiles[1], Does.Contain("TestName = \"TestServerResponse2\","));
-            Assert.That(generatedFiles[1], Does.Contain("\t\t\t\tBeforeEachTestSetUps = [classInstance => global::TUnit.Core.Helpers.RunHelpers.RunAsync(() => classInstance.BeforeEach1()),classInstance => global::TUnit.Core.Helpers.RunHelpers.RunAsync(() => classInstance.BeforeEach3()),classInstance => global::TUnit.Core.Helpers.RunHelpers.RunAsync(() => classInstance.BeforeEach3()),classInstance => global::TUnit.Core.Helpers.RunHelpers.RunAsync(() => classInstance.Setup()),],"));
+            Assert.That(generatedFiles[1], Does.Contain("\t\t\t\tBeforeEachTestSetUps = [classInstance => global::TUnit.Core.Helpers.RunHelpers.RunAsync(() => classInstance.BeforeEach1()),classInstance => global::TUnit.Core.Helpers.RunHelpers.RunAsync(() => classInstance.BeforeEach2()),classInstance => global::TUnit.Core.Helpers.RunHelpers.RunAsync(() => classInstance.BeforeEach3()),classInstance => global::TUnit.Core.Helpers.RunHelpers.RunAsync(() => classInstance.Setup()),],"));
         });
 }
