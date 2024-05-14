5e0999d9f6c28fc1d3932ea92f25e59ae79b512b
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-05-14 20:33:10 +0100

    Generator logic

--- TUnit.Pipeline/Modules/PackTUnitFilesModule.cs
+++ TUnit.Pipeline/Modules/PackTUnitFilesModule.cs
@@ -22,25 +22,38 @@ public class GenerateVersionModule : Module<GitVersionInformation>
 [DependsOn<GenerateVersionModule>]
 public class PackTUnitFilesModule : Module<List<PackedProject>>
 {
-    protected override async Task<List<PackedProject>?> ExecuteAsync(IPipelineContext context, CancellationToken cancellationToken)
+    protected override async Task<List<PackedProject>?> ExecuteAsync(IPipelineContext context,
+        CancellationToken cancellationToken)
     {
         var projects = await GetModule<GetPackageProjectsModule>();
         var versionResult = await GetModule<GenerateVersionModule>();
-        
+
         var version = versionResult.Value!;
-        
+
         // TODO: Full version
         var packageVersion = version.NuGetVersionV2;
-        
+
         if (context.Git().Information.BranchName == "main")
         {
             packageVersion += "-alpha01";
         }
 
-        await projects.Value!.SelectAsync(async project =>
-        {
-            return await context.DotNet().Pack(new DotNetPackOptions(project) { Properties = new[] { new KeyValue("Version", version.SemVer!), new KeyValue("PackageVersion", packageVersion!) }, IncludeSource = true, }, cancellationToken);
-        }, cancellationToken: cancellationToken).ProcessOneAtATime();
+        await projects.Value!.SelectAsync(
+            async project =>
+            {
+                return await context.DotNet()
+                    .Pack(
+                        new DotNetPackOptions(project)
+                        {
+                            Properties = new[]
+                            {
+                                new KeyValue("Version", version.SemVer!),
+                                new KeyValue("PackageVersion", packageVersion!)
+                            },
+                            IncludeSource = !project.Name.Contains("Generator"),
+                        }, cancellationToken);
+            }, cancellationToken: cancellationToken).ProcessOneAtATime();
+        
         return projects.Value!.Select(x => new PackedProject(x.NameWithoutExtension, version.SemVer!)).ToList();
     }
 }
