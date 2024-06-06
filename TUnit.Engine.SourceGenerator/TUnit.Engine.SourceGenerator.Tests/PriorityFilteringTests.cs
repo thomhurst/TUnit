af47bcf7f531cc60ea2c6847b165c03c7b27ef3b
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-06-07 00:23:20 +0100

    Fix assertion

--- TUnit.Engine.SourceGenerator/TUnit.Engine.SourceGenerator.Tests/PriorityFilteringTests.cs
+++ TUnit.Engine.SourceGenerator/TUnit.Engine.SourceGenerator.Tests/PriorityFilteringTests.cs
@@ -22,15 +22,5 @@ internal class PriorityFilteringTests : TestsBase<TestsGenerator>
         generatedFiles =>
         {
             Assert.That(generatedFiles.Length, Is.EqualTo(6));
-            
-            Assert.That(generatedFiles[0], Does.Contain("RepeatIndex = 1,"));
-            Assert.That(generatedFiles[1], Does.Contain("RepeatIndex = 1,"));
-            Assert.That(generatedFiles[2], Does.Contain("RepeatIndex = 2,"));
-            Assert.That(generatedFiles[3], Does.Contain("RepeatIndex = 2,"));
-            Assert.That(generatedFiles[4], Does.Contain("RepeatIndex = 2,"));
-            Assert.That(generatedFiles[5], Does.Contain("RepeatIndex = 3,"));
-            Assert.That(generatedFiles[6], Does.Contain("RepeatIndex = 3,"));
-            Assert.That(generatedFiles[7], Does.Contain("RepeatIndex = 3,"));
-            Assert.That(generatedFiles[8], Does.Contain("RepeatIndex = 3,"));
         });
 }
