e3e8d0aa0ebfdbfac124fc40142f1b80f005b9e7
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-06-07 18:16:34 +0100

    --list-tests

---
+++ TUnit.Pipeline/Modules/Tests/ListTestsModule.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+using ModularPipelines.Attributes;
+using ModularPipelines.Context;
+using ModularPipelines.DotNet.Extensions;
+using ModularPipelines.DotNet.Options;
+using ModularPipelines.Extensions;
+using ModularPipelines.Git.Extensions;
+using ModularPipelines.Models;
+using ModularPipelines.Modules;
+
+namespace TUnit.Pipeline.Modules.Tests;
+
+[DependsOn<BuildTestProjectModule>]
+public class ListTestsModule : Module<CommandResult>
+{
+    protected override async Task<CommandResult?> ExecuteAsync(IPipelineContext context, CancellationToken cancellationToken)
+    {
+        var project = context.Git().RootDirectory.FindFile(x => x.Name == "TUnit.TestProject.csproj").AssertExists();
+        
+        return await context.DotNet().Run(new DotNetRunOptions
+        {
+            Project = project,
+            NoBuild = true,
+            Arguments = [ "--list-tests" ]
+        }, cancellationToken);
+
+    }
+}
