b9d685ea997300d8db43baf40404538bf3836fca
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-06-07 18:30:20 +0100

    Build all projects

--- TUnit.Pipeline/Modules/Tests/BuildTestProjectModule.cs
+++ TUnit.Pipeline/Modules/Tests/BuildTestProjectModule.cs
@@ -10,10 +10,17 @@ namespace TUnit.Pipeline.Modules.Tests;
 
 public class BuildTestProjectModule : Module<CommandResult>
 {
-    protected override async Task<CommandResult?> ExecuteAsync(IPipelineContext context, CancellationToken cancellationToken)
+    protected override async Task<CommandResult?> ExecuteAsync(IPipelineContext context,
+        CancellationToken cancellationToken)
     {
-        var project = context.Git().RootDirectory.FindFile(x => x.Name == "TUnit.TestProject.csproj").AssertExists();
+        var projects = context.Git().RootDirectory.GetFiles(x =>
+            x.Extension == ".csproj" && !x.Path.Contains("Pipeline", StringComparison.OrdinalIgnoreCase));
 
-        return await context.DotNet().Build(new DotNetBuildOptions(project), cancellationToken);
+        foreach (var project in projects)
+        {
+            await context.DotNet().Build(new DotNetBuildOptions(project), cancellationToken);
+        }
+
+        return null!;
     }
 }
