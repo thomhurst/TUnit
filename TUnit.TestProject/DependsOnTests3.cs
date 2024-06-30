a9b88c2af91b8b19bdd5a6c4b81ed962424c1289
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-06-30 19:08:46 +0100

    DependsOnTests3.cs

---
+++ TUnit.TestProject/DependsOnTests3.cs
@@ -0,0 +1,42 @@
+﻿using TUnit.Assertions;
+using TUnit.Assertions.Extensions;
+using TUnit.Core;
+
+namespace TUnit.TestProject;
+
+public class DependsOnTests3
+{
+    private static DateTime test1Start;
+    private static DateTime test2Start;
+    private static DateTime test3Start;
+
+    [Test]
+    public async Task Test1()
+    {
+        test1Start = DateTime.Now;
+        await Task.Delay(TimeSpan.FromSeconds(1));
+    }
+    
+    [Test]
+    public async Task Test2()
+    {
+        test2Start = DateTime.Now;
+        await Task.Delay(TimeSpan.FromSeconds(1));
+    }
+    
+    [Test]
+    [DependsOn(nameof(Test1))]
+    [DependsOn(nameof(Test2))]
+    public async Task Test3()
+    {
+        test3Start = DateTime.Now;
+        await Task.Delay(TimeSpan.FromSeconds(1));
+    }
+
+    [AfterAllTestsInClass]
+    public static async Task AssertStartTimes()
+    {
+        await Assert.That(test3Start).Is.GreaterThanOrEqualTo(test1Start.AddSeconds(1));
+        await Assert.That(test3Start).Is.GreaterThanOrEqualTo(test2Start.AddSeconds(1));
+    }
+}
