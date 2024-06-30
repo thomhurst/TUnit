44103d689d21379bc2238c04c3eff9ae56b4de8b
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-06-30 14:23:48 +0100

    Fix

--- TUnit.Pipeline/Modules/Tests/DependsOnTests.cs
+++ TUnit.Pipeline/Modules/Tests/DependsOnTests.cs
@@ -18,7 +18,7 @@ public class DependsOnTests : TestModule
         await RunTestsWithFilter(context, 
             "/*/*/DependsOnTests/*",
             [
-                result => result.Successful.Should().BeFalse(),
+                result => result.Successful.Should().BeTrue(),
                 result => result.Total.Should().Be(2),
                 result => result.Passed.Should().Be(0),
                 result => result.Failed.Should().Be(3),
