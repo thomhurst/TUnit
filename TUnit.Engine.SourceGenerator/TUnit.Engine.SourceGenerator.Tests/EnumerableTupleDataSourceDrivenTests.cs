d77302108f1f6a15014072a44645d53e06969d63
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-06-30 23:00:45 +0100

    NamedTupleMethod

--- TUnit.Engine.SourceGenerator/TUnit.Engine.SourceGenerator.Tests/EnumerableTupleDataSourceDrivenTests.cs
+++ TUnit.Engine.SourceGenerator/TUnit.Engine.SourceGenerator.Tests/EnumerableTupleDataSourceDrivenTests.cs
@@ -10,7 +10,7 @@ internal class EnumerableTupleDataSourceDrivenTests : TestsBase<TestsGenerator>
             "EnumerableTupleDataSourceDrivenTests.cs"),
         generatedFiles =>
         {
-            Assert.That(generatedFiles.Length, Is.EqualTo(1));
+            Assert.That(generatedFiles, Has.Length.EqualTo(2));
             
             Assert.That(generatedFiles[0], Does.Contain("foreach (var methodData in global::TUnit.TestProject.EnumerableTupleDataSourceDrivenTests.TupleMethod())"));
             Assert.That(generatedFiles[0], Does.Contain("var methodArg0 = methodData;"));
@@ -18,5 +18,12 @@ internal class EnumerableTupleDataSourceDrivenTests : TestsBase<TestsGenerator>
             Assert.That(generatedFiles[0], Does.Contain("TestMethodArguments = [methodArg1, methodArg2, methodArg3],"));
             Assert.That(generatedFiles[0], Does.Contain("DisplayName = $\"DataSource_TupleMethod({methodArg1}, {methodArg2}, {methodArg3})\","));
             Assert.That(generatedFiles[0], Does.Contain("TestBody = classInstance => global::TUnit.Core.Helpers.RunHelpers.RunAsync(() => classInstance.DataSource_TupleMethod(methodArg1, methodArg2, methodArg3)),"));
+            
+            Assert.That(generatedFiles[1], Does.Contain("foreach (var methodData in global::TUnit.TestProject.EnumerableTupleDataSourceDrivenTests.NamedTupleMethod())"));
+            Assert.That(generatedFiles[1], Does.Contain("var methodArg0 = methodData;"));
+            Assert.That(generatedFiles[1], Does.Contain("var (methodArg1, methodArg2, methodArg3) = methodArg0;"));
+            Assert.That(generatedFiles[1], Does.Contain("TestMethodArguments = [methodArg1, methodArg2, methodArg3],"));
+            Assert.That(generatedFiles[1], Does.Contain("DisplayName = $\"DataSource_TupleMethod({methodArg1}, {methodArg2}, {methodArg3})\","));
+            Assert.That(generatedFiles[1], Does.Contain("TestBody = classInstance => global::TUnit.Core.Helpers.RunHelpers.RunAsync(() => classInstance.DataSource_TupleMethod(methodArg1, methodArg2, methodArg3)),"));
         });
 }
--- TUnit.TestProject/EnumerableTupleDataSourceDrivenTests.cs
+++ TUnit.TestProject/EnumerableTupleDataSourceDrivenTests.cs
@@ -6,12 +6,20 @@ public class EnumerableTupleDataSourceDrivenTests
 {
     [DataSourceDrivenTest]
     [EnumerableMethodDataSource(nameof(TupleMethod), UnfoldTuple = true)]
+    [EnumerableMethodDataSource(nameof(NamedTupleMethod), UnfoldTuple = true)]
     public void DataSource_TupleMethod(int value, string value2, bool value3)
     {
         // Dummy method
     }
     
     public static IEnumerable<(int, string, bool)> TupleMethod()
+    {
+        yield return (1, "String", true);
+        yield return (2, "String2", false);
+        yield return (3, "String3", true);
+    }
+    
+    public static IEnumerable<(int value1, string value2, bool value3)> NamedTupleMethod()
     {
         yield return (1, "String", true);
         yield return (2, "String2", false);
