09a5ed5efa35cf72b4a0a584d858c8aca05f7bcd
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-06-30 21:51:22 +0100

    Fix

--- TUnit.Pipeline/Modules/Tests/DependsOnAndNotInParallelTests.cs
+++ TUnit.Pipeline/Modules/Tests/DependsOnAndNotInParallelTests.cs
@@ -18,7 +18,7 @@ public class DependsOnAndNotInParallelTests : TestModule
                 result => result.Passed.Should().Be(1),
                 result => result.Failed.Should().Be(1),
                 result => result.Skipped.Should().Be(0),
-                result => result.TrxReport.UnitTestResults.First(x => x.TestName!.StartsWith("Test2")).Output!.ErrorInfo!.Message.Should().Contain("DependsOnNotInParallelException"),
+                result => result.TrxReport.UnitTestResults.First(x => x.TestName!.StartsWith("Test2")).Output!.ErrorInfo!.Message.Should().Contain("Test2 cannot use DependsOn with a test that has a NotInParallel attribute"),
 
             ], cancellationToken);
     }
