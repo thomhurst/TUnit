f1e08eea906d9a7692e1ffa7e7226c1f74fdc6bb
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-06-30 19:44:31 +0100

    DependsOnAndNotInParallelTests.cs

--- TUnit.Core/Exceptions/DependsOnNotInParallelException.cs
+++ TUnit.Core/Exceptions/DependsOnNotInParallelException.cs
@@ -2,7 +2,7 @@
 
 public class DependsOnNotInParallelException : TUnitException
 {
-    internal DependsOnNotInParallelException(string testName) : base($"{testName} cannot have both DependsOn and NotInParallel attributes")
+    internal DependsOnNotInParallelException(string testName) : base($"{testName} cannot use DependsOn with a test that has a NotInParallel attributes")
     {
     }
 }
---
+++ TUnit.TestProject/DependsOnAndNotInParallelTests.cs
@@ -0,0 +1,18 @@
+﻿using TUnit.Core;
+
+namespace TUnit.TestProject;
+
+public class DependsOnAndNotInParallelTests
+{
+    [Test, NotInParallel]
+    public async Task Test1()
+    {
+        await Task.Delay(TimeSpan.FromSeconds(5));
+    }
+    
+    [Test, DependsOn(nameof(Test1))]
+    public async Task Test2()
+    {
+        await Task.CompletedTask;
+    }
+}
