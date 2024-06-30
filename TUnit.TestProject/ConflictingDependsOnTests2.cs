03db8f8c682b6d9fbf80fc5c150637de30f2b7ce
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-06-30 16:41:55 +0100

    ConflictingDependsOnTests2

---
+++ TUnit.TestProject/ConflictingDependsOnTests2.cs
@@ -0,0 +1,24 @@
+﻿using TUnit.Core;
+
+namespace TUnit.TestProject;
+
+public class ConflictingDependsOnTests2
+{
+    [Test, DependsOn(nameof(Test3))]
+    public async Task Test1()
+    {
+        await Task.Delay(TimeSpan.FromSeconds(5));
+    }
+    
+    [Test, DependsOn(nameof(Test1))]
+    public async Task Test2()
+    {
+        await Task.CompletedTask;
+    }
+    
+    [Test, DependsOn(nameof(Test2))]
+    public async Task Test3()
+    {
+        await Task.CompletedTask;
+    }
+}
