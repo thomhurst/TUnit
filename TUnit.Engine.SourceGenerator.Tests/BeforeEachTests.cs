91987b78de2efa6d15bc713741474d9cfc2f83d4
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-07-07 21:26:05 +0100

    Fix tests

--- TUnit.Engine.SourceGenerator.Tests/BeforeEachTests.cs
+++ TUnit.Engine.SourceGenerator.Tests/BeforeEachTests.cs
@@ -12,11 +12,57 @@ internal class BeforeEachTests : TestsBase<TestsGenerator>
         generatedFiles =>
         {
             Assert.That(generatedFiles.Length, Is.EqualTo(2));
-            
+
             Assert.That(generatedFiles[0], Does.Contain("TestName = \"Test1\","));
-            Assert.That(generatedFiles[0], Does.Contain("\t\t\t\tBeforeEachTestSetUps = [classInstance => RunHelpers.RunAsync(() => classInstance.BeforeEach1()),classInstance => RunHelpers.RunAsync(() => classInstance.BeforeEach2()),classInstance => RunHelpers.RunAsync(() => classInstance.BeforeEach3()),classInstance => RunHelpers.RunAsync(() => classInstance.Setup()),],"));
-            
+            Assert.That(generatedFiles[0], Does.Contain(
+                """
+                				BeforeEachTestSetUps = [
+                new InstanceMethod<global::TUnit.TestProject.BeforeEachTests.SetupTests>
+                {
+                    MethodInfo = typeof(global::TUnit.TestProject.BeforeEachTests.SetupTests).GetMethod("BeforeEach1", 0, []),
+                    Body = (classInstance, cancellationToken) => RunHelpers.RunAsync(() => classInstance.BeforeEach1()),
+                },
+                new InstanceMethod<global::TUnit.TestProject.BeforeEachTests.SetupTests>
+                {
+                    MethodInfo = typeof(global::TUnit.TestProject.BeforeEachTests.SetupTests).GetMethod("BeforeEach2", 0, []),
+                    Body = (classInstance, cancellationToken) => RunHelpers.RunAsync(() => classInstance.BeforeEach2()),
+                },
+                new InstanceMethod<global::TUnit.TestProject.BeforeEachTests.SetupTests>
+                {
+                    MethodInfo = typeof(global::TUnit.TestProject.BeforeEachTests.SetupTests).GetMethod("BeforeEach3", 0, []),
+                    Body = (classInstance, cancellationToken) => RunHelpers.RunAsync(() => classInstance.BeforeEach3()),
+                },
+                new InstanceMethod<global::TUnit.TestProject.BeforeEachTests.SetupTests>
+                {
+                    MethodInfo = typeof(global::TUnit.TestProject.BeforeEachTests.SetupTests).GetMethod("Setup", 0, []),
+                    Body = (classInstance, cancellationToken) => RunHelpers.RunAsync(() => classInstance.Setup()),
+                },],
+                """));
+
             Assert.That(generatedFiles[1], Does.Contain("TestName = \"Test2\","));
-            Assert.That(generatedFiles[1], Does.Contain("\t\t\t\tBeforeEachTestSetUps = [classInstance => RunHelpers.RunAsync(() => classInstance.BeforeEach1()),classInstance => RunHelpers.RunAsync(() => classInstance.BeforeEach2()),classInstance => RunHelpers.RunAsync(() => classInstance.BeforeEach3()),classInstance => RunHelpers.RunAsync(() => classInstance.Setup()),],"));
+            Assert.That(generatedFiles[1], Does.Contain(
+                """
+                BeforeEachTestSetUps = [
+                new InstanceMethod<global::TUnit.TestProject.BeforeEachTests.SetupTests>
+                {
+                    MethodInfo = typeof(global::TUnit.TestProject.BeforeEachTests.SetupTests).GetMethod("BeforeEach1", 0, []),
+                    Body = (classInstance, cancellationToken) => RunHelpers.RunAsync(() => classInstance.BeforeEach1()),
+                },
+                new InstanceMethod<global::TUnit.TestProject.BeforeEachTests.SetupTests>
+                {
+                    MethodInfo = typeof(global::TUnit.TestProject.BeforeEachTests.SetupTests).GetMethod("BeforeEach2", 0, []),
+                    Body = (classInstance, cancellationToken) => RunHelpers.RunAsync(() => classInstance.BeforeEach2()),
+                },
+                new InstanceMethod<global::TUnit.TestProject.BeforeEachTests.SetupTests>
+                {
+                    MethodInfo = typeof(global::TUnit.TestProject.BeforeEachTests.SetupTests).GetMethod("BeforeEach3", 0, []),
+                    Body = (classInstance, cancellationToken) => RunHelpers.RunAsync(() => classInstance.BeforeEach3()),
+                },
+                new InstanceMethod<global::TUnit.TestProject.BeforeEachTests.SetupTests>
+                {
+                    MethodInfo = typeof(global::TUnit.TestProject.BeforeEachTests.SetupTests).GetMethod("Setup", 0, []),
+                    Body = (classInstance, cancellationToken) => RunHelpers.RunAsync(() => classInstance.Setup()),
+                },],
+                """));
         });
 }
