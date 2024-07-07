383b23a3bc5e2394ba9dc41f19e4028544ec9d22
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-07-07 16:31:25 +0100

    Build

---
+++ TUnit.Pipeline/Modules/BuildSolutionModule.cs
@@ -0,0 +1,20 @@
+﻿using ModularPipelines.Attributes;
+using ModularPipelines.Context;
+using ModularPipelines.DotNet.Extensions;
+using ModularPipelines.DotNet.Options;
+using ModularPipelines.Extensions;
+using ModularPipelines.Git.Extensions;
+using ModularPipelines.Models;
+using ModularPipelines.Modules;
+
+namespace TUnit.Pipeline.Modules;
+
+public class BuildSolutionModule : Module<CommandResult>
+{
+    protected override async Task<CommandResult?> ExecuteAsync(IPipelineContext context, CancellationToken cancellationToken)
+    {
+        var project = context.Git().RootDirectory.FindFile(x => x.Name == "TUnit.sln").AssertExists();
+        
+        return await context.DotNet().Build(new DotNetBuildOptions(project), cancellationToken);
+    }
+}
