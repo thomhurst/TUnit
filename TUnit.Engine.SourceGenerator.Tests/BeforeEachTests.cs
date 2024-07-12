041eeb93f855a0721b0f91be9bd82c9a624e93e9
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-07-12 18:34:26 +0100

    Fix source generated tests

--- TUnit.Engine.SourceGenerator.Tests/BeforeEachTests.cs
+++ TUnit.Engine.SourceGenerator.Tests/BeforeEachTests.cs
@@ -2,7 +2,7 @@ using TUnit.Engine.SourceGenerator.CodeGenerators;
 
 namespace TUnit.Engine.SourceGenerator.Tests;
 
-internal class BeforeTests : TestsBase<TestsGenerator>
+internal class BeforeTests : TestsBase<TestHooksGenerator>
 {
     [Test]
     public Task Test() => RunTest(Path.Combine(Git.RootDirectory.FullName,
@@ -11,68 +11,51 @@ internal class BeforeTests : TestsBase<TestsGenerator>
             "BeforeTests.cs"),
         generatedFiles =>
         {
-            Assert.That(generatedFiles.Length, Is.EqualTo(2));
+            Assert.That(generatedFiles.Length, Is.EqualTo(5));
 
-            Assert.That(generatedFiles[0], Does.Contain("TestName = \"Test1\","));
             Assert.That(generatedFiles[0], Does.Contain(
-                """
-                				BeforeEachTestSetUps = [
-                				    new InstanceMethod<global::TUnit.TestProject.BeforeTests.SetupTests>
-                				    {
-                    				    MethodInfo = typeof(global::TUnit.TestProject.BeforeTests.SetupTests).GetMethod("BeforeEach1", 0, []),
-                    				    Body = (classInstance, testContext, cancellationToken) => AsyncConvert.Convert(() => classInstance.BeforeEach1()),
-                				    },
-                				    new InstanceMethod<global::TUnit.TestProject.BeforeTests.SetupTests>
-                				    {
-                    				    MethodInfo = typeof(global::TUnit.TestProject.BeforeTests.SetupTests).GetMethod("BeforeEach2", 0, []),
-                    				    Body = (classInstance, testContext, cancellationToken) => AsyncConvert.Convert(() => classInstance.BeforeEach2()),
-                				    },
-                				    new InstanceMethod<global::TUnit.TestProject.BeforeTests.SetupTests>
-                				    {
-                    				    MethodInfo = typeof(global::TUnit.TestProject.BeforeTests.SetupTests).GetMethod("BeforeEach3", 0, []),
-                    				    Body = (classInstance, testContext, cancellationToken) => AsyncConvert.Convert(() => classInstance.BeforeEach3()),
-                				    },
-                				    new InstanceMethod<global::TUnit.TestProject.BeforeTests.SetupTests>
-                				    {
-                    				    MethodInfo = typeof(global::TUnit.TestProject.BeforeTests.SetupTests).GetMethod("Setup", 0, []),
-                    				    Body = (classInstance, testContext, cancellationToken) => AsyncConvert.Convert(() => classInstance.Setup()),
-                				    },
-                				    new InstanceMethod<global::TUnit.TestProject.BeforeTests.SetupTests>
-                				    {
-                    				    MethodInfo = typeof(global::TUnit.TestProject.BeforeTests.SetupTests).GetMethod("SetupWithContext", 0, [typeof(global::TUnit.Core.TestContext)]),
-                    				    Body = (classInstance, testContext, cancellationToken) => AsyncConvert.Convert(() => classInstance.SetupWithContext(testContext)),
-                				    },],
-                """));
-
-            Assert.That(generatedFiles[1], Does.Contain("TestName = \"Test2\","));
+	            """
+	            		TestHookOrchestrator.RegisterSetUp<global::TUnit.TestProject.BeforeTests.Base1>(new InstanceMethod<global::TUnit.TestProject.BeforeTests.Base1>
+	            		{ 
+	               		MethodInfo = typeof(global::TUnit.TestProject.BeforeTests.Base1).GetMethod("BeforeEach1", 0, []),
+	               		Body = (classInstance, testContext, cancellationToken) => AsyncConvert.Convert(() => classInstance.BeforeEach1())
+	            		});
+	            """));
+            
             Assert.That(generatedFiles[1], Does.Contain(
-                """
-                BeforeEachTestSetUps = [
-                				    new InstanceMethod<global::TUnit.TestProject.BeforeTests.SetupTests>
-                				    {
-                    				    MethodInfo = typeof(global::TUnit.TestProject.BeforeTests.SetupTests).GetMethod("BeforeEach1", 0, []),
-                    				    Body = (classInstance, testContext, cancellationToken) => AsyncConvert.Convert(() => classInstance.BeforeEach1()),
-                				    },
-                				    new InstanceMethod<global::TUnit.TestProject.BeforeTests.SetupTests>
-                				    {
-                    				    MethodInfo = typeof(global::TUnit.TestProject.BeforeTests.SetupTests).GetMethod("BeforeEach2", 0, []),
-                    				    Body = (classInstance, testContext, cancellationToken) => AsyncConvert.Convert(() => classInstance.BeforeEach2()),
-                				    },
-                				    new InstanceMethod<global::TUnit.TestProject.BeforeTests.SetupTests>
-                				    {
-                    				    MethodInfo = typeof(global::TUnit.TestProject.BeforeTests.SetupTests).GetMethod("BeforeEach3", 0, []),
-                    				    Body = (classInstance, testContext, cancellationToken) => AsyncConvert.Convert(() => classInstance.BeforeEach3()),
-                				    },
-                				    new InstanceMethod<global::TUnit.TestProject.BeforeTests.SetupTests>
-                				    {
-                    				    MethodInfo = typeof(global::TUnit.TestProject.BeforeTests.SetupTests).GetMethod("Setup", 0, []),
-                    				    Body = (classInstance, testContext, cancellationToken) => AsyncConvert.Convert(() => classInstance.Setup()),
-                				    },
-                				    new InstanceMethod<global::TUnit.TestProject.BeforeTests.SetupTests>
-                				    {
-                    				    MethodInfo = typeof(global::TUnit.TestProject.BeforeTests.SetupTests).GetMethod("SetupWithContext", 0, [typeof(global::TUnit.Core.TestContext)]),
-                    				    Body = (classInstance, testContext, cancellationToken) => AsyncConvert.Convert(() => classInstance.SetupWithContext(testContext)),
-                				    },],
-                """));
+	            """
+	            		TestHookOrchestrator.RegisterSetUp<global::TUnit.TestProject.BeforeTests.Base2>(new InstanceMethod<global::TUnit.TestProject.BeforeTests.Base2>
+	            		{ 
+	               		MethodInfo = typeof(global::TUnit.TestProject.BeforeTests.Base2).GetMethod("BeforeEach2", 0, []),
+	               		Body = (classInstance, testContext, cancellationToken) => AsyncConvert.Convert(() => classInstance.BeforeEach2())
+	            		});
+	            """));
+            
+            Assert.That(generatedFiles[2], Does.Contain(
+	            """
+	            		TestHookOrchestrator.RegisterSetUp<global::TUnit.TestProject.BeforeTests.Base3>(new InstanceMethod<global::TUnit.TestProject.BeforeTests.Base3>
+	            		{ 
+	               		MethodInfo = typeof(global::TUnit.TestProject.BeforeTests.Base3).GetMethod("BeforeEach3", 0, []),
+	               		Body = (classInstance, testContext, cancellationToken) => AsyncConvert.Convert(() => classInstance.BeforeEach3())
+	            		});
+	            """));
+            
+            Assert.That(generatedFiles[3], Does.Contain(
+	            """
+	            		TestHookOrchestrator.RegisterSetUp<global::TUnit.TestProject.BeforeTests.SetupTests>(new InstanceMethod<global::TUnit.TestProject.BeforeTests.SetupTests>
+	            		{ 
+	               		MethodInfo = typeof(global::TUnit.TestProject.BeforeTests.SetupTests).GetMethod("Setup", 0, []),
+	               		Body = (classInstance, testContext, cancellationToken) => AsyncConvert.Convert(() => classInstance.Setup())
+	            		});
+	            """));
+            
+            Assert.That(generatedFiles[4], Does.Contain(
+	            """
+	            		TestHookOrchestrator.RegisterSetUp<global::TUnit.TestProject.BeforeTests.SetupTests>(new InstanceMethod<global::TUnit.TestProject.BeforeTests.SetupTests>
+	            		{ 
+	               		MethodInfo = typeof(global::TUnit.TestProject.BeforeTests.SetupTests).GetMethod("SetupWithContext", 0, [typeof(global::TUnit.Core.TestContext)]),
+	               		Body = (classInstance, testContext, cancellationToken) => AsyncConvert.Convert(() => classInstance.SetupWithContext(testContext))
+	            		});
+	            """));
         });
 }
