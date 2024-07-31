4d6d0f57ebce035a35b9437e62fe26cf4b32a0b1
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-07-31 13:18:27 +0100

    Remove ObjectBag

--- TUnit.Core/DiscoveredTestContext.cs
+++ TUnit.Core/DiscoveredTestContext.cs
@@ -3,11 +3,10 @@
 public class DiscoveredTestContext(TestDetails TestDetails)
 {
     internal Dictionary<string, string>? Properties;
-    internal Dictionary<string, object?>? ObjectBag;
     
-    public void AddProperty(string key, string value) =>
-        (Properties ??= new Dictionary<string, string>()).Add(key, value);
-
-    public void AddToObjectBag(string key, object? value) =>
-        (ObjectBag ??= new Dictionary<string, object?>()).Add(key, value);
+    public void AddProperty(string key, string value)
+    {
+        var properties = Properties ??= new Dictionary<string, string>();
+        properties.Add(key, value);
+    }
 }
