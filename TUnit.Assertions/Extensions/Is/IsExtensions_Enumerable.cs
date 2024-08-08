36fbeec118c60fb9ef2c3af6e753e1b48db88cda
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-08-08 20:10:49 +0100

    Fix

--- TUnit.Assertions/Extensions/Is/IsExtensions_Enumerable.cs
+++ TUnit.Assertions/Extensions/Is/IsExtensions_Enumerable.cs
@@ -10,7 +10,7 @@ namespace TUnit.Assertions.Extensions;
 
 public static partial class IsExtensions
 {
-    public static BaseAssertCondition<TActual, TAnd, TOr> EquivalentTo<TActual, TInner, TAnd, TOr>(this Is<TActual, TAnd, TOr> @is, IEnumerable<TInner> expected, IEqualityComparer<TInner> equalityComparer, [CallerArgumentExpression("expected")] string doNotPopulateThisValue = "")
+    public static BaseAssertCondition<TActual, TAnd, TOr> EquivalentTo<TActual, TInner, TAnd, TOr>(this Is<TActual, TAnd, TOr> @is, IEnumerable<TInner> expected, IEqualityComparer<TInner> equalityComparer = null, [CallerArgumentExpression("expected")] string doNotPopulateThisValue = "")
         where TActual : IEnumerable<TInner>
         where TAnd : And<TActual, TAnd, TOr>, IAnd<TAnd, TActual, TAnd, TOr>
         where TOr : Or<TActual, TAnd, TOr>, IOr<TOr, TActual, TAnd, TOr>
