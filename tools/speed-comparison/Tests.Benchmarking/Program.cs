9308fc816e15630a28c9d0d30c0adbf37c5a20fe
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-09-01 22:48:38 +0100

    Tidy

--- tools/speed-comparison/Tests.Benchmarking/Program.cs
+++ tools/speed-comparison/Tests.Benchmarking/Program.cs
@@ -1,10 +1,10 @@
 ﻿using BenchmarkDotNet.Running;
 using Tests.Benchmarking;
 
-var summary = BenchmarkRunner.Run<Benchmarks>();
+BenchmarkRunner.Run<Benchmarks>();
 
 var output = new DirectoryInfo(Environment.CurrentDirectory)
     .GetFiles("*.md", SearchOption.AllDirectories)
     .First();
     
-    Environment.SetEnvironmentVariable("GITHUB_STEP_SUMMARY", await File.ReadAllTextAsync(output.FullName));
+Environment.SetEnvironmentVariable("GITHUB_STEP_SUMMARY", await File.ReadAllTextAsync(output.FullName));
