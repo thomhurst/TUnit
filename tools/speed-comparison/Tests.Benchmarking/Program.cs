b682ced0d0ea69d088a245e5c137dcab9bb15e05
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-09-01 22:28:06 +0100

    MarkdownExporter.GitHub

--- tools/speed-comparison/Tests.Benchmarking/Program.cs
+++ tools/speed-comparison/Tests.Benchmarking/Program.cs
@@ -1,4 +1,6 @@
-﻿using BenchmarkDotNet.Running;
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Running;
 using Tests.Benchmarking;
 
-var summary = BenchmarkRunner.Run<Benchmarks>();
+var summary = BenchmarkRunner.Run<Benchmarks>(ManualConfig.CreateMinimumViable().AddExporter(MarkdownExporter.GitHub));
