4556fbd4609adc3280987f1bf87dfbb653aa41bd
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-09-01 20:24:36 +0100

    Benchmarking Project

---
+++ tools/speed-comparison/Tests.Benchmarking/Benchmarks.cs
@@ -0,0 +1,38 @@
+﻿using BenchmarkDotNet.Attributes;
+using TUnit.Engine.Services;
+using Xunit.Runners;
+
+public class Benchmarks
+{
+    [Benchmark]
+    public async Task TUnit()
+    {
+        await TUnitRunner.RunTests();
+    }
+    
+    [Benchmark]
+    public void NUnit()
+    {
+        new NUnitLite.AutoRun().Execute([]);
+    }
+    
+    [Benchmark]
+    public void xUnit()
+    {
+        using var runner = AssemblyRunner.WithoutAppDomain(typeof(xUnitTimer.Tests).Assembly.FullName);
+        
+        using var finished = new ManualResetEvent(false);
+
+        runner.OnExecutionComplete += _ =>  finished.Set();
+        
+        runner.Start(new AssemblyRunnerStartOptions());
+
+        finished.WaitOne();
+    }
+    
+    [Benchmark]
+    public void MSTest()
+    {
+        Microsoft.VisualStudio.TestPlatform.TestHost.Program.Main([]);
+    }
+}
