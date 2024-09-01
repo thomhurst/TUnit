a14637a5cf16cb50b7b7f528b460b2ba10d5511a
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-09-01 23:57:52 +0100

    Benchmarks

---
+++ tools/speed-comparison/Tests.Benchmark/Benchmarks.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using BenchmarkDotNet.Attributes;
+using Process = System.Diagnostics.Process;
+
+[MarkdownExporterAttribute.GitHub]
+public class Benchmarks
+{
+    [Benchmark]
+    public async Task TUnit()
+    {
+        await Process.Start(new ProcessStartInfo("dotnet", "run")
+        {
+            WorkingDirectory = "../../../../TUnitTimer",
+        })!.WaitForExitAsync();
+    }
+
+    [Benchmark]
+    public async Task NUnit()
+    {
+        await Process.Start(new ProcessStartInfo("dotnet", "test")
+        {
+            WorkingDirectory = "../../../../NUnitTimer",
+        })!.WaitForExitAsync();
+    }
+
+    [Benchmark]
+    public async Task xUnit()
+    {
+        await Process.Start(new ProcessStartInfo("dotnet", "test")
+        {
+            WorkingDirectory = "../../../../xUnitTimer",
+        })!.WaitForExitAsync();
+    }
+    
+    [Benchmark]
+    public async Task MSTest()
+    {
+        await Process.Start(new ProcessStartInfo("dotnet", "test")
+        {
+            WorkingDirectory = "../../../../MSTestTimer",
+        })!.WaitForExitAsync();
+    }
+}
