4f5601637fb497310935d7d81c87d63bea512970
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-09-02 20:07:19 +0100

    WorkingDirectory = readme.Folder!

--- TUnit.Pipeline/Modules/GenerateReadMeModule.cs
+++ TUnit.Pipeline/Modules/GenerateReadMeModule.cs
@@ -76,7 +76,7 @@ public class GenerateReadMeModule : Module<File>
         await context.Git().Commands.Add(new GitAddOptions
         {
             Arguments = ["README.md"],
-            WorkingDirectory = context.Git().RootDirectory.AssertExists()
+            WorkingDirectory = readme.Folder!
         }, cancellationToken);
 
         await context.Git().Commands.Commit(new GitCommitOptions
