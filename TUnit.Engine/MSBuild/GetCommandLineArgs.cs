581131bc9f37e526f33709bb3ba46310ada31cee
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-09-15 17:56:52 +0100

    GetCommandLineArgs

---
+++ TUnit.Engine/MSBuild/GetCommandLineArgs.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using Task = Microsoft.Build.Utilities.Task;
+
+namespace TUnit.Engine.MSBuild;
+
+public sealed class GetCommandLineArgs : Task
+{
+    [Output] public ITaskItem2[] CommandLineArgs { get; private set; } = [];
+
+    public override bool Execute() {
+        CommandLineArgs = Environment.GetCommandLineArgs().Select(a => new TaskItem(a)).Cast<ITaskItem2>().ToArray();
+        return true;
+    }
+}
