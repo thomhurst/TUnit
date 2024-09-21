b146bf4bf3e669b931fa240af8322135f99cb1f2
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-09-21 11:44:20 +0100

    Fix

--- TUnit.Engine.SourceGenerator/Models/Arguments/Argument.cs
+++ TUnit.Engine.SourceGenerator/Models/Arguments/Argument.cs
@@ -31,7 +31,7 @@ internal record Argument
         
         if (type == "global::System.Char")
         {
-            return $"'{value.Replace(@"\", @"\\")}'";
+            return $"'{value}'";
         }
         
         if (type == "global::System.Boolean")
@@ -41,7 +41,7 @@ internal record Argument
         
         if (type == "global::System.String")
         {
-            return $"\"{value.Replace(@"\", @"\\")}\"";
+            return $"\"{value}\"";
         }
 
         return value;
