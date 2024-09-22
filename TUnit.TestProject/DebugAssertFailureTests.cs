16f2c521be0db078df09d7f54bbce5af704ccee2
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-09-22 21:06:29 +0100

    Fix terminating process using Debug.Assert or Trace.Assert (#598)
    * Fix terminating process using Debug.Assert or Trace.Assert
    
    * Fix test
    
    * Fix test
    
    * Fix
    
    * Fix

---
+++ TUnit.TestProject/DebugAssertFailureTests.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+
+namespace TUnit.TestProject;
+
+public class DebugAssertFailureTests
+{
+    [Test]
+    public void Test()
+    {
+        var @true = "true";
+        Trace.Assert(@true is "false", "Some message");
+    }
+}
