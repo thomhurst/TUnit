e5c56901c9924828c1d2fcff617742e439fe1e23
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-09-23 23:03:17 +0100

    Fix nullable values in [Arguments]

---
+++ TUnit.Engine.SourceGenerator.Tests/NullableByteArgumentTests.cs
@@ -0,0 +1,38 @@
+using TUnit.Engine.SourceGenerator.CodeGenerators;
+using TUnit.Engine.SourceGenerator.Tests.Extensions;
+
+namespace TUnit.Engine.SourceGenerator.Tests;
+
+internal class NullableByteArgumentTests : TestsBase<TestsGenerator>
+{
+    [Test]
+    public Task Test() => RunTest(Path.Combine(Git.RootDirectory.FullName,
+            "TUnit.TestProject",
+            "NullableByteArgumentTests.cs"),
+        generatedFiles =>
+        {
+            Assert.That(generatedFiles.Length, Is.EqualTo(4));
+
+            Assert.That(generatedFiles[0], Does.Contain(
+                """
+                global::System.Byte? methodArg0 = 1;
+                """));
+            
+            Assert.That(generatedFiles[1], Does.Contain(
+                """
+                global::System.Byte? methodArg0 = null;
+                """));
+            
+            Assert.That(generatedFiles[2].IgnoreWhitespaceFormatting(), Does.Contain(
+                """
+                global::System.Byte methodArg0 = 1;
+                global::System.Byte? methodArg1 = 1;
+                """.IgnoreWhitespaceFormatting()));
+            
+            Assert.That(generatedFiles[3].IgnoreWhitespaceFormatting(), Does.Contain(
+                """
+                global::System.Byte methodArg0 = 1;
+                global::System.Byte? methodArg1 = null;
+                """.IgnoreWhitespaceFormatting()));
+        });
+}
