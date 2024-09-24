60201e3d858c57dc606b35b8c795dfb01407a5df
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-09-24 21:52:39 +0100

    Fix

--- TUnit.TestProject/ParallelLimiterTests.cs
+++ TUnit.TestProject/ParallelLimiterTests.cs
@@ -1,21 +1,10 @@
-﻿using System.Collections.Concurrent;
-using TUnit.Assertions;
-using TUnit.Assertions.Extensions;
-using TUnit.TestProject.Dummy;
+﻿using TUnit.TestProject.Dummy;
 
 namespace TUnit.TestProject;
 
 [ParallelLimiter<ParallelLimit3>]
 public class ParallelLimiterTests
 {
-    private static readonly ConcurrentBag<DateTimeRange> TestDateTimeRanges = [];
-    
-    [After(Test)]
-    public void After()
-    {
-        TestDateTimeRanges.Add(new DateTimeRange(TestContext.Current!.TestStart!.Value.DateTime, TestContext.Current.Result!.End.DateTime));
-    }
-
     [Test, Repeat(3)]
     public async Task Parallel_Test1()
     {
@@ -32,26 +21,5 @@ public class ParallelLimiterTests
     public async Task Parallel_Test3()
     {
         await Task.Delay(500);
-    }
-    
-    [After(Class)]
-    public static async Task AssertOverlaps()
-    {
-        var start = TestDateTimeRanges.MinBy(x => x.Start)!.Start;
-        var end = TestDateTimeRanges.MaxBy(x => x.End)!.End;
-
-        await Assert.That(end - start).IsGreaterThan(TimeSpan.FromMilliseconds(1500));
-    }
-
-    private class DateTimeRange
-    {
-        public DateTime Start { get; }
-        public DateTime End { get; }
-        
-        public DateTimeRange(DateTime start, DateTime end)
-        {
-            Start = start;
-            End = end;
-        }
     }
 }
