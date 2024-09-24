bb69673f5526c1648571f4d74861f291bcf79453
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-09-24 17:39:58 +0100

    Library NuGet Tester

---
+++ tools/tunit-nuget-tester/TUnit.NugetTester/TUnit.NugetTester.Library/TestBase.cs
@@ -0,0 +1,16 @@
+﻿namespace TUnit.NugetTester.Library;
+
+public class TestBase
+{
+    [Before(Test)]
+    public void Setup(TestContext testContext)
+    {
+        Console.WriteLine($"Starting test: {testContext}");
+    }
+    
+    [After(Test)]
+    public void Teardown(TestContext testContext)
+    {
+        Console.WriteLine($"Finishing test: {testContext}");
+    }
+}
