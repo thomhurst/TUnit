fe1915d120a12d8ab877f71d61aa9deae7c185c9
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-09-25 16:15:30 +0100

    Test NET 9.0 (#636)
    * Test NET 9.0
    
    * Remove azure pipeline

---
+++ TUnit.Pipeline/Modules/TestNugetPackageModule.cs
@@ -0,0 +1,40 @@
+﻿using EnumerableAsyncProcessor.Extensions;
+using ModularPipelines.Attributes;
+using ModularPipelines.Context;
+using ModularPipelines.DotNet.Extensions;
+using ModularPipelines.DotNet.Options;
+using ModularPipelines.Extensions;
+using ModularPipelines.Git.Extensions;
+using ModularPipelines.Models;
+using ModularPipelines.Modules;
+
+namespace TUnit.Pipeline.Modules;
+
+[DependsOn<GenerateVersionModule>]
+[DependsOn<CopyToLocalNuGetModule>]
+public class TestNugetPackageModule : Module<CommandResult[]>
+{
+    private string[] _frameworks = ["net8.0", "net9.0"];
+    protected override async Task<CommandResult[]?> ExecuteAsync(IPipelineContext context, CancellationToken cancellationToken)
+    {
+        var version = await GetModule<GenerateVersionModule>();
+
+        var project = context.Git()
+            .RootDirectory
+            .AssertExists()
+            .FindFile(x => x.Name == "TUnit.NugetTester.csproj")
+            .AssertExists();
+
+        return await _frameworks.SelectAsync(framework =>
+            context.DotNet().Run(new DotNetRunOptions
+            {
+                Project = project,
+                Framework = framework,
+                Properties =
+                [
+                    new KeyValue("TUnitVersion", version.Value!.SemVer!)
+                ]
+            }, cancellationToken)
+, cancellationToken: cancellationToken).ProcessOneAtATime();
+    }
+}
