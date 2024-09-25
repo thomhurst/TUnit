355804915d56cbbc1d15ba773b43af2093fc4765
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-09-25 16:03:54 +0100

    Test NET 9.0

--- TUnit.Pipeline/Modules/CopyToLocalNuGetModule.cs
+++ TUnit.Pipeline/Modules/CopyToLocalNuGetModule.cs
@@ -1,10 +1,6 @@
 ﻿using ModularPipelines.Attributes;
 using ModularPipelines.Context;
-using ModularPipelines.DotNet.Extensions;
-using ModularPipelines.DotNet.Options;
-using ModularPipelines.Extensions;
 using ModularPipelines.Git.Extensions;
-using ModularPipelines.Models;
 using ModularPipelines.Modules;
 using File = ModularPipelines.FileSystem.File;
 
@@ -17,30 +13,5 @@ public class CopyToLocalNuGetModule : Module<List<File>>
     {
         var folder = await GetModule<AddLocalNuGetRepositoryModule>();
         return context.Git().RootDirectory.GetFiles(x => x.Extension.EndsWith("nupkg")).Select(x => x.CopyTo(folder.Value!)).ToList();
-    }
-}
-
-[DependsOn<GenerateVersionModule>]
-[DependsOn<CopyToLocalNuGetModule>]
-public class TestNugetPackageModule : Module<CommandResult>
-{
-    protected override async Task<CommandResult?> ExecuteAsync(IPipelineContext context, CancellationToken cancellationToken)
-    {
-        var version = await GetModule<GenerateVersionModule>();
-
-        var project = context.Git()
-            .RootDirectory
-            .AssertExists()
-            .FindFile(x => x.Name == "TUnit.NugetTester.csproj")
-            .AssertExists();
-
-        return await context.DotNet().Run(new DotNetRunOptions
-        {
-            Project = project,
-            Properties =
-            [
-                new KeyValue("TUnitVersion", version.Value!.SemVer!)
-            ]
-        }, cancellationToken);
     }
 }
