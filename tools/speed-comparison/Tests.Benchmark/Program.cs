5b616461bf1661f0a1da0028e3302a2da072f749
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-09-28 13:29:09 +0100

    Tweak benchmarks

--- tools/speed-comparison/Tests.Benchmark/Program.cs
+++ tools/speed-comparison/Tests.Benchmark/Program.cs
@@ -1,7 +1,7 @@
 ﻿using BenchmarkDotNet.Running;
 using Tests.Benchmark;
 
-BenchmarkRunner.Run<Benchmarks>(args: args);
+BenchmarkRunner.Run<RuntimeBenchmarks>(args: args);
 
 var output = new DirectoryInfo(Environment.CurrentDirectory)
     .GetFiles("*.md", SearchOption.AllDirectories)
