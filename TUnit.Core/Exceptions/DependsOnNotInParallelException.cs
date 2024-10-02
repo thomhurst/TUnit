152c6519879f8a6a02f11c5036ae4462dff1bb9a
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-10-02 21:22:24 +0100

    Fix

--- TUnit.Core/Exceptions/DependsOnNotInParallelException.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TUnit.Core.Exceptions;
-
-public class DependsOnNotInParallelException : TUnitException
-{
-    internal DependsOnNotInParallelException(string testName) : base($"{testName} cannot use DependsOn with a test that has a NotInParallel attribute")
-    {
-    }
-}
