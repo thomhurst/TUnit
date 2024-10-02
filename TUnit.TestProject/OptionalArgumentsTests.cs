6f55fdddcbed3912263df5b618423910a1a81b6a
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-10-02 20:31:01 +0100

    Allow optional args with [Arguments] (#704)

---
+++ TUnit.TestProject/OptionalArgumentsTests.cs
@@ -0,0 +1,11 @@
+﻿namespace TUnit.TestProject;
+
+public class OptionalArgumentsTests
+{
+    [Test]
+    [Arguments(1)]
+    public void Test(int value, bool flag = true)
+    {
+        // Dummy Method
+    }
+}
