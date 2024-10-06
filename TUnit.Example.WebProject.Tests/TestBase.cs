cbd94e69b99bdc6b6315b5945684ea40831c43a2
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-10-06 10:41:09 +0100

    Web project tests

---
+++ TUnit.Example.WebProject.Tests/TestBase.cs
@@ -0,0 +1,24 @@
+﻿using TUnit.Core;
+
+namespace TUnit.Example.WebProject.Tests;
+
+public class TestBase
+{
+    [ClassDataSource<MyFactory>(Shared = SharedType.Globally)]
+    public static MyFactory Factory { get; set; } = null!;
+    
+    protected HttpClient Client { get; private set; } = null!;
+
+    [Before(HookType.Test)]
+    public void BeforeTest()
+    {
+        Client = Factory.CreateClient();
+    }
+
+    [Before(HookType.TestSession)]
+    public static async Task PrepareSession()
+    {
+        Factory.CreateClient();
+        await Task.CompletedTask;
+    }
+}
---
+++ TUnit.Example.WebProject.Tests/Tests.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using FluentAssertions;
+using TUnit.Core;
+
+namespace TUnit.Example.WebProject.Tests;
+
+public class Tests : TestBase
+{
+    [Test]
+    public async Task Test()
+    {
+        var response = await Client.GetAsync("/weatherforecast");
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+}
