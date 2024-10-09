d367d9966b9ae6103a7829481561260c00da8b0c
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-10-09 23:29:01 +0100

    Fix

--- TUnit.Engine.SourceGenerator/CodeGenerators/TestsGenerator.cs
+++ TUnit.Engine.SourceGenerator/CodeGenerators/TestsGenerator.cs
@@ -71,10 +71,10 @@ internal class TestsGenerator : IIncrementalGenerator
             sourceBuilder.WriteLine();
             sourceBuilder.WriteLine("namespace TUnit.SourceGenerated;");
             sourceBuilder.WriteLine();
-            sourceBuilder.WriteLine("[global::System.Diagnostics.DebuggerHidden]");
             sourceBuilder.WriteLine("[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]");
             sourceBuilder.WriteLine($"file partial class {className}");
             sourceBuilder.WriteLine("{");
+            sourceBuilder.WriteLine("[global::System.Diagnostics.DebuggerHidden]");
             sourceBuilder.WriteLine("[global::System.Runtime.CompilerServices.ModuleInitializer]");
             sourceBuilder.WriteLine("public static void Initialise()");
             sourceBuilder.WriteLine("{");
