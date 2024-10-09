5feaedc4286c42e73bb890f00491aec92b175ead
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-10-09 22:05:15 +0100

    Remove runtime check

--- TUnit.Core/Attributes/TestData/ClassDataSourceAttribute.cs
+++ TUnit.Core/Attributes/TestData/ClassDataSourceAttribute.cs
@@ -17,14 +17,6 @@ public sealed class ClassDataSourceAttribute<[DynamicallyAccessedMembers(Dynamic
     private static readonly GetOnlyDictionary<Assembly, Task> AssemblyInitializers = new();
     private static readonly GetOnlyDictionary<string, Task> KeyedInitializers = new();
     
-    public ClassDataSourceAttribute()
-    {
-        if (!typeof(T).GetConstructors().Any(x => x.IsPublic && x.GetParameters().Length == 0))
-        {
-            throw new ArgumentException($"{typeof(T).FullName} cannot be used within [ClassData] as it does not have a public constructor.");
-        }
-    }
-    
     public SharedType Shared { get; set; } = SharedType.None;
     public string Key { get; set; } = string.Empty;
     public override IEnumerable<T> GenerateDataSources(DataGeneratorMetadata dataGeneratorMetadata)
