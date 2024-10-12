22280b657d6e8322983930b6547cdf8ad5e6fd3f
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-10-12 13:15:47 +0100

    Undo

--- TUnit.TestProject/BasicTests.cs
+++ TUnit.TestProject/BasicTests.cs
@@ -11,16 +11,6 @@ public class BasicTests
     [Test]
     public async Task AsynchronousTest()
     {
-        Console.WriteLine("Foo Bar!");
         await Task.CompletedTask;
-    }
-
-    public class Inner
-    {
-        [Test]
-        public void SynchronousTest()
-        {
-            // Dummy method
-        }
     }
 }
