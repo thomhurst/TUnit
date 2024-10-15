6fe5366bdee88dc370aac5d56c64331a9d882676
Valentin Breuß <valentin.breuss@baur.eu>
2024-10-15 11:15:16 +0200

    Move `Assert.Fail` to separate class, similar to `Skip` (#905)

---
+++ TUnit.Assertions.Tests/FailTests.cs
@@ -0,0 +1,56 @@
+﻿namespace TUnit.Assertions.Tests;
+
+public class FailTests
+{
+    [Test]
+    public async Task Fail_Test_Throws_AssertionException()
+    {
+        string reason = "foo";
+        var action = () => Fail.Test(reason);
+
+        var exception = await Assert.That(action).ThrowsExactly<AssertionException>();
+        await Assert.That(exception!.Message).IsEqualTo(reason);
+    }
+
+    [Test]
+    public async Task Fail_Unless_Throws_AssertionException_When_Condition_Is_False()
+    {
+        bool condition = false;
+        string reason = "foo";
+        var action = () => Fail.Unless(condition, reason);
+
+        var exception = await Assert.That(action).ThrowsExactly<AssertionException>();
+        await Assert.That(exception!.Message).IsEqualTo(reason);
+    }
+
+    [Test]
+    public async Task Fail_Unless_Does_Not_Throw_When_Condition_Is_True()
+    {
+        bool condition = true;
+        string reason = "foo";
+        var action = () => Fail.Unless(condition, reason);
+
+        var exception = await Assert.That(action).ThrowsNothing();
+    }
+
+    [Test]
+    public async Task Fail_When_Throws_AssertionException_When_Condition_Is_True()
+    {
+        bool condition = true;
+        string reason = "foo";
+        var action = () => Fail.When(condition, reason);
+
+        var exception = await Assert.That(action).ThrowsExactly<AssertionException>();
+        await Assert.That(exception!.Message).IsEqualTo(reason);
+    }
+
+    [Test]
+    public async Task Fail_When_Does_Not_Throw_When_Condition_Is_False()
+    {
+        bool condition = false;
+        string reason = "foo";
+        var action = () => Fail.When(condition, reason);
+
+        var exception = await Assert.That(action).ThrowsNothing();
+    }
+}
---
+++ TUnit.Assertions/Fail.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using TUnit.Assertions.Exceptions;
+
+namespace TUnit.Assertions;
+
+public static class Fail
+{
+    /// <summary>
+    /// Fails the current test.
+    /// </summary>
+    /// <param name="reason">The reason why the test failed</param>
+    [DoesNotReturn]
+    public static void Test(string reason)
+    {
+        throw new AssertionException(reason);
+    }
+
+    /// <summary>
+    /// Fails the current test when the <paramref name="condition"/> is <c>false</c>.
+    /// </summary>
+    /// <param name="condition">When <c>false</c>, the test will be failed; otherwise it will continue to run</param>
+    /// <param name="reason">The reason why the test was failed</param>
+    public static void Unless([DoesNotReturnIf(false)] bool condition, string reason)
+    {
+        if (!condition)
+        {
+            Test(reason);
+        }
+    }
+
+    /// <summary>
+    /// Fails the current test when the <paramref name="condition"/> is <c>true</c>.
+    /// </summary>
+    /// <param name="condition">When <c>true</c>, the test will be failed; otherwise it will continue to run</param>
+    /// <param name="reason">The reason why the test was failed</param>
+    public static void When([DoesNotReturnIf(true)] bool condition, string reason)
+    {
+        if (condition)
+        {
+            Test(reason);
+        }
+    }
+}
