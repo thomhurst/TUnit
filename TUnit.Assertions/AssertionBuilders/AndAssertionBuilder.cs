ce8625154c977336d75f15579ab5a19ce860cd62
Valentin Breuß <vbreuss@gmail.com>
2024-10-16 00:46:15 +0200

    Throw an exception when mixing AND and OR assertions (#911)
    * Throw an exception when mixing AND and OR assertions
    
    * Implement review comments
    
    ---------
    
    Co-authored-by: Tom Longhurst <30480171+thomhurst@users.noreply.github.com>

--- TUnit.Assertions/AssertionBuilders/AndAssertionBuilder.cs
+++ TUnit.Assertions/AssertionBuilders/AndAssertionBuilder.cs
@@ -1,8 +1,15 @@
+using TUnit.Assertions.AssertConditions.Connectors;
+using TUnit.Assertions.Exceptions;
+
 namespace TUnit.Assertions.AssertionBuilders;
 
 public class AndAssertionBuilder<TActual> : AssertionBuilder<TActual>, IAndAssertionBuilder
 {
     internal AndAssertionBuilder(AssertionBuilder<TActual> assertionBuilder) : base(assertionBuilder.AssertionDataDelegate, assertionBuilder.ActualExpression!, assertionBuilder.ExpressionBuilder, assertionBuilder.Assertions)
     {
+        if (assertionBuilder.Assertions.Any(a => a is OrAssertCondition<TActual>))
+        {
+            throw new MixedAndOrAssertionsException();
+        }
     }
 }
