49171a0c20caaf40a10790c89b628fee991b8342
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-10-16 08:53:21 +0100

    Fix null warning

--- TUnit.Assertions/AssertionBuilders/Groups/UnknownAssertionGroup.cs
+++ TUnit.Assertions/AssertionBuilders/Groups/UnknownAssertionGroup.cs
@@ -3,9 +3,9 @@ namespace TUnit.Assertions.AssertionBuilders.Groups;
 public class UnknownAssertionGroup<TActual, TAssertionBuilder> where TAssertionBuilder : AssertionBuilder<TActual>
 {
     private readonly TAssertionBuilder _assertionBuilder;
-    private readonly Func<TAssertionBuilder, InvokableAssertionBuilder<TActual>>? _initialAssert;
+    private readonly Func<TAssertionBuilder, InvokableAssertionBuilder<TActual>> _initialAssert;
 
-    internal UnknownAssertionGroup(TAssertionBuilder assertionBuilder, Func<TAssertionBuilder, InvokableAssertionBuilder<TActual>>? initialAssert)
+    internal UnknownAssertionGroup(TAssertionBuilder assertionBuilder, Func<TAssertionBuilder, InvokableAssertionBuilder<TActual>> initialAssert)
     {
         _assertionBuilder = assertionBuilder;
         _initialAssert = initialAssert;
