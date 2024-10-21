e267de076814d5b8572e07fa9be03695a44583bf
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-10-21 20:14:43 +0100

    Run DisposedRepro tests on pipeline

---
+++ TUnit.Pipeline/Modules/Tests/DisposedReproTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using ModularPipelines.Context;
+
+namespace TUnit.Pipeline.Modules.Tests;
+
+public class DisposedReproTests : TestModule
+{
+    protected override async Task<TestResult?> ExecuteAsync(IPipelineContext context, CancellationToken cancellationToken)
+    {
+        return await RunTestsWithFilter(context, 
+            "/*/*/DisposedRepro/*",
+            [
+                result => result.Successful.Should().BeTrue(),
+                result => result.Total.Should().Be(3),
+                result => result.Passed.Should().Be(3),
+                result => result.Failed.Should().Be(0),
+                result => result.Skipped.Should().Be(0)
+            ], cancellationToken);
+    }
+}
