9a09241a54ec4fe9c8100baaa385169c93124bec
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-10-21 20:01:03 +0100

    Fix re-using test class instances in some edge cases

---
+++ TUnit.TestProject/DisposedRepro.cs
@@ -0,0 +1,35 @@
+﻿namespace TUnit.TestProject;
+
+public abstract class DisposedReproTestBase : IDisposable
+{
+    private bool _disposed;
+
+    public void CheckDisposed()
+    {
+        if (_disposed)
+        {
+            throw new InvalidOperationException("Already disposed");
+        }
+    }
+
+    public void Dispose()
+    {
+        _disposed = true;
+    }
+}
+
+[ClassDataSource<Dummy2>]
+[NotInParallel]
+public sealed class DisposedRepro(Dummy2 dummy) : DisposedReproTestBase
+{
+    [Test]
+    [MethodDataSource(nameof(GetValues))]
+    public void DoTest(int value)
+    {
+        CheckDisposed();
+    }
+
+    public static IEnumerable<int> GetValues() => [1, 2, 3];
+}
+
+public sealed record Dummy2;
