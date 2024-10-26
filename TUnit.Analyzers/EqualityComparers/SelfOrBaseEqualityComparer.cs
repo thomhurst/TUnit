cf0b8a7ee84f9fbeca34249215457efb124f8c00
Stuart Turner <stuart@turner-isageek.com>
2024-10-26 18:30:09 -0500

    More `IEnumerable<>` improvements (#1021)

--- TUnit.Analyzers/EqualityComparers/SelfOrBaseEqualityComparer.cs
+++ TUnit.Analyzers/EqualityComparers/SelfOrBaseEqualityComparer.cs
@@ -6,7 +6,26 @@ public class SelfOrBaseEqualityComparer(Compilation compilation) : EqualityCompa
 {
     public override bool Equals(ITypeSymbol? superType, ITypeSymbol? subType)
     {
-        return compilation.HasImplicitConversion(subType, superType);
+        if (compilation.HasImplicitConversion(subType, superType))
+            return true;
+
+        // cannot be valid subType if superType is not named generic type like `IEnumerable<>`
+        if (superType is not INamedTypeSymbol { IsGenericType: true } namedType)
+            return false;
+
+        if (subType is IArrayTypeSymbol { ElementType: { } elementType })
+        {
+            var specializedSuper = namedType.OriginalDefinition.Construct(elementType);
+            return compilation.HasImplicitConversion(subType, specializedSuper);
+        }
+
+        if (subType is INamedTypeSymbol { IsGenericType: true, TypeArguments: [{ } genericArgument] })
+        {
+            var specializedSuper = namedType.OriginalDefinition.Construct(genericArgument);
+            return compilation.HasImplicitConversion(subType, specializedSuper);
+        }
+
+        return false;
     }
 
     public override int GetHashCode(ITypeSymbol? obj)
