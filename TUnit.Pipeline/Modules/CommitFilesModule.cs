1e28ddcd33bda983b30bc72be8b15751d1fe27db
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-10-27 23:40:44 +0000

    CommitFilesModule fix

--- TUnit.Pipeline/Modules/CommitFilesModule.cs
+++ TUnit.Pipeline/Modules/CommitFilesModule.cs
@@ -17,14 +17,22 @@ namespace TUnit.Pipeline.Modules;
 [RunOnLinuxOnly]
 [DependsOn<MergeNuGetFilesModule>]
 [DependsOn<TestNugetPackageModule>]
-[DependsOn<GenerateReadMeModule>]
+[DependsOn<GenerateReadMeModule>(IgnoreIfNotRegistered = true)]
 [SkipIfDependabot]
 public class CommitFilesModule : Module<CommandResult>
 {
     protected override async Task<SkipDecision> ShouldSkip(IPipelineContext context)
     {
-        var generateReadMeModule = await GetModule<GenerateReadMeModule>();
-        return generateReadMeModule.Value == null;
+        var generateReadMeModule = GetModuleIfRegistered<GenerateReadMeModule>();
+
+        if (generateReadMeModule is null)
+        {
+            return "Nothing to commit";
+        }
+
+        var result = await generateReadMeModule;
+        
+        return result.SkipDecision.ShouldSkip || !result.HasValue;
     }
 
     protected override async Task<CommandResult?> ExecuteAsync(IPipelineContext context, CancellationToken cancellationToken)
