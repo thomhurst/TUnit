436a770237feffc755656372932587c23d11dc4d
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-10-30 10:23:16 +0000

    Fix

--- tools/tunit-nuget-tester/TUnit.NugetTester/TUnit.NugetTester.Library/TestBase.cs
+++ tools/tunit-nuget-tester/TUnit.NugetTester/TUnit.NugetTester.Library/TestBase.cs
@@ -7,12 +7,12 @@ public class TestBase
     [Before(Test)]
     public void Setup(TestContext testContext)
     {
-        Console.WriteLine($"Starting test: {testContext.TestDetails.GetTestDisplayName()}");
+        Console.WriteLine($"Starting test: {testContext.GetTestDisplayName()}");
     }
     
     [After(Test)]
     public void Teardown(TestContext testContext)
     {
-        Console.WriteLine($"Finishing test: {testContext.TestDetails.GetTestDisplayName()}");
+        Console.WriteLine($"Finishing test: {testContext.GetTestDisplayName()}");
     }
 }
