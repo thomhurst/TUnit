18ace40b2d853cff59863131704ea224fc86d6e6
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-11-13 19:15:09 +0000

    WIP

--- TUnit.Core.SourceGenerator.Tests/EnumerableDataSourceDrivenTests.cs
+++ TUnit.Core.SourceGenerator.Tests/EnumerableDataSourceDrivenTests.cs
@@ -13,10 +13,10 @@ internal class EnumerableDataSourceDrivenTests : TestsBase<TestsGenerator>
         {
             await Assert.That(generatedFiles.Length).IsEqualTo(2);
             
-            await AssertFileContains(generatedFiles[0], "foreach (var methodData in global::TUnit.TestProject.EnumerableDataSourceDrivenTests.SomeMethod())");
+            await AssertFileContains(generatedFiles[0], "foreach (var methodDataAccessor in global::TUnit.TestProject.EnumerableDataSourceDrivenTests.SomeMethod().ToUniqueElementsEnumerable())");
             await AssertFileContains(generatedFiles[0], "TestMethodArguments = [methodData],");
             
-            await AssertFileContains(generatedFiles[1], "foreach (var methodData in global::TUnit.TestProject.EnumerableDataSourceDrivenTests.SomeMethod())");
+            await AssertFileContains(generatedFiles[1], "foreach (var methodDataAccessor in global::TUnit.TestProject.EnumerableDataSourceDrivenTests.SomeMethod().ToUniqueElementsEnumerable())");
             await AssertFileContains(generatedFiles[1], "TestMethodArguments = [methodData],");
         });
 }
