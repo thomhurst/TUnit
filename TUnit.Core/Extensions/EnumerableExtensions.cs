edac3e41c40c563ca0beb2afb546f5ef6a6772c6
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-11-18 15:18:34 +0000

    DefaultParallelLimit

--- TUnit.Core/Extensions/EnumerableExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TUnit.Core.Extensions;
-
-public static class EnumerableExtensions
-{
-    public static UniqueElementsEnumerable<T> ToUniqueElementsEnumerable<T>(this IEnumerable<T> enumerable)
-    {
-        return new UniqueElementsEnumerable<T>(enumerable);
-    }
-}
--- TUnit.Core/NotInParallelConstraint.cs
+++ TUnit.Core/NotInParallelConstraint.cs
@@ -4,5 +4,5 @@ namespace TUnit.Core;
 
 public record NotInParallelConstraint(IReadOnlyList<string> NotInParallelConstraintKeys) : IParallelConstraint
 {
-    public int Order { get; internal set; } = int.MaxValue / 2;
+    public int Order { get; set; } = int.MaxValue / 2;
 }
