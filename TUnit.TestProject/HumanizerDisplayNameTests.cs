c4c152365078f9b8635a756632cbbad53d5dd4b7
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-11-24 21:48:37 +0000

    HumanizerDisplayNameTests

---
+++ TUnit.TestProject/HumanizerDisplayNameTests.cs
@@ -0,0 +1,23 @@
+﻿using Humanizer;
+using TUnit.Core.Extensions;
+
+namespace TUnit.TestProject;
+
+[HumanizerDisplayName]
+public class HumanizerDisplayNameTests
+{
+    [Test]
+    public void This_test_name_is_formatted_nicely()
+    {
+        // Dummy method
+        Console.WriteLine(TestContext.Current!.GetTestDisplayName());
+    }
+    
+    public class HumanizerDisplayNameAttribute : DisplayNameFormatterAttribute
+    {
+        protected override string FormatDisplayName(TestContext testContext)
+        {
+            return testContext.TestDetails.TestName.Humanize();
+        }
+    }
+}
