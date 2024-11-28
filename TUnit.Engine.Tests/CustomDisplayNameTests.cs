a2947c2a6db083b250158d61c647b42eb3e252fb
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-11-28 15:01:21 +0000

    Fix

--- TUnit.Engine.Tests/CustomDisplayNameTests.cs
+++ TUnit.Engine.Tests/CustomDisplayNameTests.cs
@@ -11,8 +11,8 @@ public class CustomDisplayNameTests : InvokableTestBase
             "/*/*/CustomDisplayNameTests/*",
             [
                 result => result.ResultSummary.Outcome.Should().Be("Completed"),
-                result => result.ResultSummary.Counters.Total.Should().Be(5),
-                result => result.ResultSummary.Counters.Passed.Should().Be(5),
+                result => result.ResultSummary.Counters.Total.Should().Be(6),
+                result => result.ResultSummary.Counters.Passed.Should().Be(6),
                 result => result.ResultSummary.Counters.Failed.Should().Be(0),
                 result => result.ResultSummary.Counters.NotExecuted.Should().Be(0)
             ]);
