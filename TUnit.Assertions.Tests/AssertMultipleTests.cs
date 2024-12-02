1d48367bc4271c2990fc53867b2fa0b84f521c49
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-12-02 20:05:58 +0000

    Capture and Surface Exceptions thrown within AssertionScope

---
+++ TUnit.Assertions.Tests/AssertMultipleTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace TUnit.Assertions.Tests;
+
+[SuppressMessage("Usage", "TUnitAssertions0005:Assert.That(...) should not be used with a constant value")]
+public class AssertMultipleTests
+{
+    [Test]
+    public async Task Exception_In_Scope_Is_Captured()
+    {
+        using (Assert.Multiple())
+        {
+            await Assert.That(1).IsEqualTo(2);
+            await Assert.That(2).IsEqualTo(4);
+            
+            if(1.ToString() == "1")
+            {
+                throw new Exception("Hello World");
+            }
+
+            await Assert.That(3).IsEqualTo(6);
+        }
+    }
+}
