7d6013a9e0dbe6e83ff748527e8891cc6f8ee2f4
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-12-07 10:33:01 +0000

    Fix

--- TUnit.Engine.Tests/InheritedTestsFromDifferentProjectTests.cs
+++ TUnit.Engine.Tests/InheritedTestsFromDifferentProjectTests.cs
@@ -11,8 +11,8 @@ public class InheritedTestsFromDifferentProjectTests : InvokableTestBase
             "/*/*/InheritedTestsFromDifferentProjectTests/*",
             [
                 result => result.ResultSummary.Outcome.Should().Be("Completed"),
-                result => result.ResultSummary.Counters.Total.Should().Be(2),
-                result => result.ResultSummary.Counters.Passed.Should().Be(2),
+                result => result.ResultSummary.Counters.Total.Should().Be(4),
+                result => result.ResultSummary.Counters.Passed.Should().Be(4),
                 result => result.ResultSummary.Counters.Failed.Should().Be(0),
                 result => result.ResultSummary.Counters.NotExecuted.Should().Be(0)
             ]);
