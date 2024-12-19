6351d6cc307cfa6cd3a2010d3bf57207c4a23f0c
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-12-19 20:13:35 +0000

    EnumMemberNamesTests

---
+++ TUnit.TestProject/Bugs/1432/EnumMemberNamesTests.cs
@@ -0,0 +1,20 @@
+﻿using TUnit.Assertions;
+using TUnit.Assertions.Extensions;
+
+namespace TUnit.TestProject.Bugs._1432;
+
+public class EnumMemberNamesTests
+{
+    [Test]
+    [Arguments(nameof(SomeEnum.A))]
+    [Arguments(nameof(SomeEnum.B))]
+    [Arguments(nameof(SomeEnum.C))]
+    public void SomeTest(string value)
+    {
+    }
+
+    public enum SomeEnum
+    {
+        A, B, C
+    }
+}
