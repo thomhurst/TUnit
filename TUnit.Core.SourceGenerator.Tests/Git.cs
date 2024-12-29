ecc5df60a16270f88e8c21a60e33165f36df01cc
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-12-29 15:55:52 +0000

    Polyfill tests (#1476)

--- TUnit.Core.SourceGenerator.Tests/Git.cs
+++ TUnit.Core.SourceGenerator.Tests/Git.cs
@@ -1,19 +1,6 @@
-﻿using System.Diagnostics;
-
-namespace TUnit.Core.SourceGenerator.Tests;
+﻿namespace TUnit.Core.SourceGenerator.Tests;
 
 internal class Git
 {
-    private static readonly Lazy<DirectoryInfo> LazyRootDirectory = new(() =>
-    {
-        var processStartInfo = new ProcessStartInfo("git", "rev-parse --show-toplevel")
-        {
-            RedirectStandardOutput = true
-        };
-        var process = Process.Start(processStartInfo)!;
-        process.WaitForExit();
-        return new DirectoryInfo(process.StandardOutput.ReadToEnd().Trim());
-    });
-    
-    public static DirectoryInfo RootDirectory => LazyRootDirectory.Value;
+    public static DirectoryInfo RootDirectory => Sourcy.Git.RootDirectory;
 }
