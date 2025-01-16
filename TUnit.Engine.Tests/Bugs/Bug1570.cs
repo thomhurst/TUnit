b809ecf772cd4f6dc4df967a6a4f43cbe806511b
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-01-16 19:56:25 +0000

    Fix

--- TUnit.Engine.Tests/Bugs/Bug1570.cs
+++ TUnit.Engine.Tests/Bugs/Bug1570.cs
@@ -8,11 +8,11 @@ public class Bug1570 : InvokableTestBase
     public async Task Test()
     {
         await RunTestsWithFilter(
-            "/*/TUnit.TestProject.Bugs._1570/*/Tests",
+            "/*/TUnit.TestProject.Bugs._1570/*/*",
             [
                 result => result.ResultSummary.Outcome.ShouldBe("Completed"),
-                result => result.ResultSummary.Counters.Total.ShouldBe(2),
-                result => result.ResultSummary.Counters.Passed.ShouldBe(2),
+                result => result.ResultSummary.Counters.Total.ShouldBe(6),
+                result => result.ResultSummary.Counters.Passed.ShouldBe(6),
                 result => result.ResultSummary.Counters.Failed.ShouldBe(0),
                 result => result.ResultSummary.Counters.NotExecuted.ShouldBe(0)
             ]);
