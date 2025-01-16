ace80d3c72d0e16ce28d6a882effb4693e603460
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-01-16 18:30:31 +0000

    Enum Assertions

---
+++ TUnit.Assertions/Assertions/Enums/Conditions/EnumIsNotDefinedAssertCondition.cs
@@ -0,0 +1,17 @@
+﻿using TUnit.Assertions.AssertConditions;
+
+namespace TUnit.Assertions.Assertions.Enums.Conditions;
+
+public class EnumIsNotDefinedAssertCondition<TEnum> : BaseAssertCondition<TEnum> where TEnum : Enum
+{
+    protected override string GetExpectation()
+    {
+        return "to not be defined";
+    }
+
+    protected override Task<AssertionResult> GetResult(TEnum? actualValue, Exception? exception)
+    {
+        return AssertionResult.FailIf(() => actualValue is null, () => "the source enum is null")
+            .OrFailIf(() => Enum.IsDefined(typeof(TEnum), actualValue!), () => "it was");
+    }
+}
