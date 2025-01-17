54505f607381718cd0df7243bb5f28f3d9a05359
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-01-17 16:02:38 +0000

    Snapshots

---
+++ TUnit.Core.SourceGenerator.Tests/Bugs/1589/Tests1539.cs
@@ -0,0 +1,31 @@
+using TUnit.Core.SourceGenerator.CodeGenerators;
+
+namespace TUnit.Core.SourceGenerator.Tests.Bugs._1539;
+
+internal class Tests1589 : TestsBase<TestsGenerator>
+{
+    [Test]
+    public Task Test() => RunTest(Path.Combine(Git.RootDirectory.FullName,
+            "TUnit.TestProject",
+            "Bugs",
+            "1589",
+            "MyTests.cs"),
+        async generatedFiles =>
+        {
+            await Assert.That(generatedFiles.Length).IsEqualTo(3);
+        });
+}
+
+internal class Hooks1589 : TestsBase<TestHooksGenerator>
+{
+    [Test]
+    public Task Test() => RunTest(Path.Combine(Git.RootDirectory.FullName,
+            "TUnit.TestProject",
+            "Bugs",
+            "1589",
+            "MyTests.cs"),
+        async generatedFiles =>
+        {
+            await Assert.That(generatedFiles.Length).IsEqualTo(3);
+        });
+}
