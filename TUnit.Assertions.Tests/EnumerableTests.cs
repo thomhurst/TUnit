cdb04116a65519671995943222211a740a8bf2ac
Genmutant <bitbucket@jurirobl.de>
2025-01-18 15:11:56 +0100

    Add Assertions on Enumerables taking a Func matcher

--- TUnit.Assertions.Tests/EnumerableTests.cs
+++ TUnit.Assertions.Tests/EnumerableTests.cs
@@ -3,10 +3,56 @@
 public class EnumerableTests
 {
     [Test]
-    public async Task Test1()
+    public async Task Enumerable_Contains_Item_Good()
     {
         int[] array = [1, 2, 3];
 
         await Assert.That(array).Contains(1);
+    }
+    
+    [Test]
+    public async Task Enumerable_Contains_Item_Bad()
+    {
+        int[] array = [1, 2, 3];
+
+        await Assert.That(
+                async () => await Assert.That(array).Contains(4)
+        ).Throws<AssertionException>();
+    }
+    
+    [Test]
+    public async Task Enumerable_Contains_Matcher_Good()
+    {
+        int[] array = [1, 2, 3];
+
+        await Assert.That(array).Contains((int x) => x == 1);
+    }
+    
+    [Test]
+    public async Task Enumerable_Contains_Matcher_Bad()
+    {
+        int[] array = [1, 2, 3];
+
+        await Assert.That(
+            async () => await Assert.That(array).Contains((int x) => x == 4)
+        ).Throws<AssertionException>();
+    }
+    
+    [Test]
+    public async Task Enumerable_ContainsOnly_Matcher_Good()
+    {
+        int[] array = [1, 2, 3];
+
+        await Assert.That(array).ContainsOnly((int x) => x < 10);
+    }
+    
+    [Test]
+    public async Task Enumerable_ContainsOnly_Matcher_Bad()
+    {
+        int[] array = [1, 2, 3];
+
+        await Assert.That(
+            async () => await Assert.That(array).ContainsOnly((int x) => x < 3)
+        ).Throws<AssertionException>();
     }
 }
---
+++ TUnit.Assertions/Assertions/Collections/Conditions/EnumerableContainsExpectedFuncAssertCondition.cs
@@ -0,0 +1,20 @@
+namespace TUnit.Assertions.AssertConditions.Collections;
+
+public class EnumerableContainsExpectedFuncAssertCondition<TActual, TInner>(
+    Func<TInner, bool> matcher, string matcherString)
+    : BaseAssertCondition<TActual>
+    where TActual : IEnumerable<TInner>
+{
+    protected override string GetExpectation() => $"to contain an entry matching {matcherString}";
+    
+    protected override Task<AssertionResult> GetResult(TActual? actualValue, Exception? exception)
+    {
+        return AssertionResult
+            .FailIf(
+                () => actualValue is null,
+                () => $"{ActualExpression ?? typeof(TActual).Name} is null")
+            .OrFailIf(
+                () => !actualValue!.Any(matcher),
+                () => "there was no match found in the collection");
+    }
+}
