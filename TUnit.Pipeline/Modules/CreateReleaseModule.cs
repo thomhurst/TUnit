69551059cad8145786ad74686a4feda29222fd84
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-01-18 21:59:37 +0000

    Automatically create release notes on a new release (#1612)

---
+++ TUnit.Pipeline/Modules/CreateReleaseModule.cs
@@ -0,0 +1,43 @@
+﻿using ModularPipelines.Attributes;
+using ModularPipelines.Context;
+using ModularPipelines.Git.Attributes;
+using ModularPipelines.GitHub.Extensions;
+using ModularPipelines.Models;
+using ModularPipelines.Modules;
+using Octokit;
+
+namespace TUnit.Pipeline.Modules;
+
+[RunOnLinuxOnly]
+[RunOnlyOnBranch("main")]
+[DependsOn<UploadToNuGetModule>(IgnoreIfNotRegistered = true)]
+[DependsOn<GenerateVersionModule>]
+public class CreateReleaseModule : Module<Release>
+{
+    protected override async Task<SkipDecision> ShouldSkip(IPipelineContext context)
+    {
+        if (GetModuleIfRegistered<UploadToNuGetModule>() is not { } uploadToNuGetModule)
+        {
+            return true;
+        }
+
+        var result = await uploadToNuGetModule;
+
+        return result.SkipDecision.ShouldSkip;
+    }
+
+    protected override async Task<Release?> ExecuteAsync(IPipelineContext context, CancellationToken cancellationToken)
+    {
+        var versionModule = await GetModule<GenerateVersionModule>();
+
+        var version = versionModule.Value!.SemVer;
+
+        return await context.GitHub().Client.Repository.Release.Create(
+            long.Parse(context.GitHub().EnvironmentVariables.RepositoryId!),
+            new NewRelease($"v{version}")
+            {
+                Name = version,
+                GenerateReleaseNotes = true,
+            });
+    }
+}
