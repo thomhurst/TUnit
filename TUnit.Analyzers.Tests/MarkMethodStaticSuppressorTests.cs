c5f7e59f75ff773909b6df362a5d03b28d9024d9
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-01-20 17:26:02 +0000

    MarkMethodStaticSuppressor

---
+++ TUnit.Analyzers.Tests/MarkMethodStaticSuppressorTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Testing;
+using NUnit.Framework;
+
+namespace TUnit.Analyzers.Tests;
+
+public class MarkMethodStaticSuppressorTests
+{
+    private static readonly DiagnosticResult CA1822 = new("CA1822", DiagnosticSeverity.Info);
+    
+    [TestCase("Test")]
+    [TestCase("Before(Test)")]
+    [TestCase("After(Test)")]
+    public async Task WarningsInTUnitAreSuppressed(string attribute) =>
+        await AnalyzerTestHelpers
+            .CreateSuppressorTest<MarkMethodStaticSuppressor>(
+                $$"""
+                using TUnit.Core;
+                using static TUnit.Core.HookType;
+                
+                public class MyTests
+                {
+                    [{{attribute}}]
+                    public void {|#0:Test1|}() 
+                    {
+                    }
+                }
+                """
+            )
+            .WithSpecificDiagnostics(CA1822)
+            .WithExpectedDiagnosticsResults(CA1822.WithLocation(0).WithIsSuppressed(true))
+            .RunAsync();
+
+    [Test]
+    public async Task WarningsAllowedElsewhere() =>
+        await AnalyzerTestHelpers
+            .CreateSuppressorTest<MarkMethodStaticSuppressor>(
+                """
+                public class MyTests
+                {
+                    public void {|#0:DoSomething|}() 
+                    {
+                    }
+                }
+                """
+            )
+            .WithSpecificDiagnostics(CA1822)
+            .WithExpectedDiagnosticsResults(CA1822.WithLocation(0).WithIsSuppressed(false))
+            .RunAsync();
+}
