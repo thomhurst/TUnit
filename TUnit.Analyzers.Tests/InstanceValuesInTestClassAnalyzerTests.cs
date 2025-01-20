7054957e45770cef4ed6d1ee266124884c419e05
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-01-20 12:56:10 +0000

    Fix incorrect analyzer warning around tests assigning instance data (#1634)

--- TUnit.Analyzers.Tests/InstanceValuesInTestClassAnalyzerTests.cs
+++ TUnit.Analyzers.Tests/InstanceValuesInTestClassAnalyzerTests.cs
@@ -54,4 +54,51 @@ public class InstanceValuesInTestClassAnalyzerTests
                 """
             );
     }
+    
+    [Test]
+    public async Task Do_Not_Flag_When_Not_Assigning_To_New_Class()
+    {
+        await Verifier
+            .VerifyAnalyzerAsync(
+                """
+                using System;
+                using TUnit.Core;
+
+                public record SomeDataClass
+                {
+                    public required Guid SomeGuid { get; set; }
+                    public required Guid SomeGuid2 { get; set; }
+                    public required Guid MyCoolGuid { get; set; }
+                }
+                
+                public class TestTest
+                {
+                
+                    private readonly Guid SomeGuid;
+                    private readonly Guid SomeGuid2;
+                    private Guid SomeGuid3; // IDE0052 => for this context, ignored
+                    private readonly Guid SomeVeryCoolGuid;
+                
+                    public TestTest()
+                    {
+                        SomeGuid = Guid.NewGuid();
+                        SomeGuid2 = Guid.NewGuid();
+                        SomeGuid3 = Guid.NewGuid();
+                        SomeVeryCoolGuid = Guid.NewGuid();
+                    }
+                
+                    [Test]
+                    public void SomeTest()
+                    {
+                        var _ = new SomeDataClass()
+                        {
+                            SomeGuid = SomeGuid, // 2 => Warning: TUnit0018
+                            MyCoolGuid = SomeGuid2, // 3 => nothing
+                            SomeGuid2 = SomeVeryCoolGuid // 4 => Warning: TUnit0018
+                        };
+                    }
+                }
+                """
+            );
+    }
 }
