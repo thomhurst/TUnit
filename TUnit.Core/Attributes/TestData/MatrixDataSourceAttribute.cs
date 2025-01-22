f9bede94963f989443b72be16eb82517fe5fd5eb
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-01-22 18:29:01 +0000

    MatrixDataSourceAttribute

---
+++ TUnit.Core/Attributes/TestData/MatrixDataSourceAttribute.cs
@@ -0,0 +1,39 @@
+﻿namespace TUnit.Core;
+
+public class MatrixDataSourceAttribute : NonTypedDataSourceGeneratorAttribute
+{
+    public override IEnumerable<Func<object?[]?>> GenerateDataSources(DataGeneratorMetadata dataGeneratorMetadata)
+    {
+        var parameterInformation = dataGeneratorMetadata
+            .MembersToGenerate
+            .OfType<SourceGeneratedParameterInformation>()
+            .ToArray();
+
+        if (parameterInformation.Length != dataGeneratorMetadata.MembersToGenerate.Length
+            || parameterInformation.Length is 0)
+        {
+            throw new Exception("[MatrixDataSource] only supports parameterised tests");
+        }
+
+        foreach (var row in GetMatrixValues(parameterInformation.Select(GetAllArguments)))
+        {
+            yield return () => row.ToArray();
+        }
+    }
+
+    private IReadOnlyList<object?> GetAllArguments(SourceGeneratedParameterInformation sourceGeneratedParameterInformation)
+    {
+        var matrixAttribute = sourceGeneratedParameterInformation.Attributes.OfType<MatrixAttribute>().FirstOrDefault() 
+                              ?? throw new ArgumentNullException($"No MatrixAttribute found for parameter {sourceGeneratedParameterInformation.Name}");
+
+        return matrixAttribute.Objects;
+    }
+    
+    private IEnumerable<IEnumerable<object?>> GetMatrixValues(IEnumerable<IReadOnlyList<object?>> elements)
+    {
+        IEnumerable<IEnumerable<object?>> seed = [[]];
+        
+        return elements.Aggregate(seed, (accumulator, enumerable)
+            => accumulator.SelectMany(x => enumerable.Select(x.Append)));
+    }
+}
