e6eff19f7bf8f205b977688479c239237f70b764
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-01-23 17:34:16 +0000

    Add ASP.NET `dotnet new` Template (#1656)

---
+++ TUnit.Templates/content/TUnit.AspNet/TestProject/WebApplicationFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using TUnit.Core.Interfaces;
+
+namespace TestProject;
+
+public class WebApplicationFactory : WebApplicationFactory<Program>, IAsyncInitializer
+{
+    public Task InitializeAsync()
+    {
+        _ = Server;
+        
+        return Task.CompletedTask;
+    }
+}
