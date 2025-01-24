ec9605d0c74c594496b32c2d900cb704de2ee822
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-01-24 15:40:26 +0000

    Fix Templates

---
+++ TUnit.Pipeline/Modules/TestTemplatePackageModule.cs
@@ -0,0 +1,39 @@
+﻿using EnumerableAsyncProcessor.Extensions;
+using ModularPipelines.Attributes;
+using ModularPipelines.Context;
+using ModularPipelines.DotNet.Extensions;
+using ModularPipelines.DotNet.Options;
+using ModularPipelines.Extensions;
+using ModularPipelines.Git.Extensions;
+using ModularPipelines.Models;
+using ModularPipelines.Modules;
+
+namespace TUnit.Pipeline.Modules;
+
+[DependsOn<CopyToLocalNuGetModule>]
+public class TestTemplatePackageModule : Module<CommandResult>
+{
+    protected override async Task<CommandResult?> ExecuteAsync(IPipelineContext context,
+        CancellationToken cancellationToken)
+    {
+        await context.DotNet().New(new DotNetNewOptions("install")
+        {
+            Arguments = ["TUnit.Templates"]
+        }, cancellationToken);
+        
+        await context.DotNet().New(new DotNetNewOptions("TUnit")
+        {
+            Name = "MyTestProject"
+        }, cancellationToken);
+        
+        await context.DotNet().New(new DotNetNewOptions("TUnit.AspNet")
+        {
+            Name = "MyTestProject"
+        }, cancellationToken);
+        
+        return await context.DotNet().New(new DotNetNewOptions("TUnit.Playwright")
+        {
+            Name = "MyTestProject"
+        }, cancellationToken);
+    }
+}
