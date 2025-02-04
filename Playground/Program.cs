44c1ada86c2f13f54be9b0652d4194347464a97c
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-02-04 10:45:38 +0000

    Fix Lock contention when combined with `[Before(Assembly)]` attributes

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -1,3 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Testcontainers.PostgreSql;
+using Testcontainers.Redis;
 
-Console.WriteLine("Hello, World!");
+public class Tests
+{
+    [Test, Repeat(1000)]
+    public void Test()
+    {
+    }
+}
+
+public class Hooks
+{
+    public static PostgreSqlContainer PostgreSqlContainer { get; } = new PostgreSqlBuilder().Build();
+    public static RedisContainer RedisContainer { get; } = new RedisBuilder().Build();
+        
+    [Before(Assembly)]
+    public static async Task Before()
+    {
+        await PostgreSqlContainer.StartAsync();
+        await RedisContainer.StartAsync();
+    }
+    
+    [After(Assembly)]
+    public static async Task After()
+    {
+        await PostgreSqlContainer.StopAsync();
+        await PostgreSqlContainer.DisposeAsync();
+        await RedisContainer.StopAsync();
+        await RedisContainer.DisposeAsync();
+    }
+}
