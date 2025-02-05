f04d84bbaa7433ef3e3c61b99a4248d88d020c9b
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-02-05 22:21:12 +0000

    Fix `.IsTypeOf<T>()` not returning `T` if wanting to assign the result to a variable (#1776)

---
+++ TUnit.Assertions.Tests/Bugs/Tests1774.cs
@@ -0,0 +1,29 @@
+﻿using TUnit.Assertions.AssertConditions.Throws;
+
+namespace TUnit.Assertions.Tests.Bugs;
+
+public class Tests1774
+{
+    [Test]
+    public async Task Test()
+    {
+        Type1 type1 = new Type2();
+        
+        var type2 = await Assert.That(() => type1)
+            .ThrowsNothing()
+            .And
+            .IsTypeOf<Type2>()
+            .And
+            .Satisfies(res => res?.Property2, assert => assert.IsNotNullOrEmpty()!);
+    }
+
+    public record Type1
+    {
+        public string Property1 => "Value1";
+    }
+
+    public record Type2 : Type1
+    {
+        public string Property2 => "Value2";
+    }
+}
