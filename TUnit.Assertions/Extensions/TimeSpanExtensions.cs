b2e2f100a347b539dc06e7f103ecffcaf632d8ed
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-02-08 17:01:50 +0000

    +semver:minor - TimeSpanExtensions for easily converting ints to TimeSpans (#1798)

---
+++ TUnit.Assertions/Extensions/TimeSpanExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace TUnit.Assertions.Extensions;
+
+public static class TimeSpanExtensions
+{
+    public static TimeSpan Milliseconds(this int milliseconds) => TimeSpan.FromMilliseconds(milliseconds);
+    public static TimeSpan Seconds(this int seconds) => TimeSpan.FromSeconds(seconds);
+    public static TimeSpan Minutes(this int minutes) => TimeSpan.FromMinutes(minutes);
+    public static TimeSpan Hours(this int hours) => TimeSpan.FromHours(hours);
+    public static TimeSpan Days(this int days) => TimeSpan.FromDays(days);
+}
