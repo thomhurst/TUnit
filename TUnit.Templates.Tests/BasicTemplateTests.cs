493356530eb002bdf05521d0fea66914c34e2d2f
Matt Kotsenas <matt.kotsenas@gmail.com>
2025-02-14 17:00:22 -0800

    Add Basic template test

---
+++ TUnit.Templates.Tests/BasicTemplateTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+using NUnit.Framework;
+
+namespace TUnit.Templates.Tests;
+
+public class BasicTemplateTests
+{
+    private static readonly ILoggerFactory _loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
+    private const string templateShortName = "TUnit";
+
+    [Test]
+    public async Task InstantiationTest()
+    {
+        TemplateVerifierOptions options = new(templateName: templateShortName)
+        {
+            TemplatePath = Path.Combine(TestContext.CurrentContext.TestDirectory, "content", "TUnit"),
+        };
+
+        VerificationEngine engine = new(_loggerFactory);
+        await engine.Execute(options).ConfigureAwait(false);
+    }
+
+    [OneTimeTearDown]
+    public void Cleanup()
+    {
+        _loggerFactory.Dispose();
+    }
+}
---
+++ TUnit.Templates.Tests/GlobalUsings.cs
@@ -0,0 +1,1 @@
+﻿global using Microsoft.TemplateEngine.Authoring.TemplateVerifier;
