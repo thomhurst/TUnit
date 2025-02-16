e0879ed73f984ed5f1627d70b203a313add55fc5
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-02-16 14:03:10 +0000

    Speed up dependency tests

--- TUnit.TestProject/ComplexDependsOn2/Tests.cs
+++ TUnit.TestProject/ComplexDependsOn2/Tests.cs
@@ -6,14 +6,14 @@ public abstract class BaseClass
     [Test]
     public async Task Test1()
     {
-        await Task.Delay(TimeSpan.FromSeconds(1));
+        await Task.Delay(TimeSpan.FromMilliseconds(50));
     }
 
     [Test]
     [DependsOn(nameof(Test1))]
     public async Task Test2()
     {
-        await Task.Delay(TimeSpan.FromSeconds(1));
+        await Task.Delay(TimeSpan.FromMilliseconds(50));
     }
 }
 public class TestsDataSource
