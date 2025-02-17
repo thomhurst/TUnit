fc406f8b6895c0f3442503b6851288057cacd836
James Alickolli <jalickolli@gmail.com>
2025-02-17 23:39:24 +1100

    Added ApiTest to Aspire template

--- TUnit.Templates/content/TUnit.Aspire/TestProject/Tests/ApiTests.cs
+++ TUnit.Templates/content/TUnit.Aspire/TestProject/Tests/ApiTests.cs
@@ -1,1 +1,38 @@
-﻿
+﻿using System.Text.Json;
+using TestProject.Models;
+
+namespace TestProject.Tests;
+
+public class ApiTests
+{
+    [Test]
+    public async Task GetWeatherForecastReturnsOkStatusCode()
+    {
+        // Act
+        var httpClient = (GlobalHooks.App ?? throw new NullReferenceException()).CreateHttpClient("apiservice");
+        if (GlobalHooks.ResourceNotificationService != null)
+            await GlobalHooks.ResourceNotificationService
+                .WaitForResourceAsync("apiservice", KnownResourceStates.Running).WaitAsync(TimeSpan.FromSeconds(30));
+        var response = await httpClient.GetAsync("/weatherforecast");
+        // Assert
+        await Assert.That(response.StatusCode).IsEqualTo(HttpStatusCode.OK);
+    } 
+
+    [Test]
+    [MatrixDataSource]
+    public async Task GetWeatherForecastReturnsCorrectData(
+        [Matrix("Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching")] string summary
+    )
+    {
+        // Act
+        var httpClient = (GlobalHooks.App ?? throw new NullReferenceException()).CreateHttpClient("apiservice");
+        if (GlobalHooks.ResourceNotificationService != null)
+            await GlobalHooks.ResourceNotificationService
+                .WaitForResourceAsync("apiservice", KnownResourceStates.Running).WaitAsync(TimeSpan.FromSeconds(30));
+        var response = await httpClient.GetAsync("/weatherforecast");
+        var content = await response.Content.ReadAsStringAsync();
+        var data = JsonSerializer.Deserialize<IEnumerable<WeatherForecast>>(content, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        // Assert
+        await Assert.That(data!.FirstOrDefault(w => w.Summary == summary)).IsNotNull();
+    }
+}
