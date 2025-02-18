d81e0235defd0962aff2cf3c3d4474a363375462
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-02-18 09:22:41 +0000

    Fix

---
+++ TUnit.TestProject/DynamicCodeOnlyAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace TUnit.TestProject;
+
+public class DynamicCodeOnlyAttribute() : SkipAttribute("This test is only supported when dynamic code is available")
+{
+    public override Task<bool> ShouldSkip(BeforeTestContext context)
+    {
+#if NET
+        return Task.FromResult(!RuntimeFeature.IsDynamicCodeSupported);
+#else
+        return Task.FromResult(false);
+#endif
+    }
+}
