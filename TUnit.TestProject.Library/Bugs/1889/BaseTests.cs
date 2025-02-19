cac85ff5eff8806c12813a5db544b7d478f0b7c4
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-02-19 11:40:00 +0000

    Fix [Arguments] attribute in a separate assembly

--- TUnit.TestProject.Library/Bugs/1889/BaseTests.cs
+++ TUnit.TestProject.Library/Bugs/1889/BaseTests.cs
@@ -2,7 +2,8 @@
 
 namespace TUnit.TestProject.Library.Bugs._1889;
 
-public abstract class BaseTest<T> {
+public abstract class BaseTest<T>
+{
     [Test]
     public void Test1()
     {
@@ -11,6 +12,13 @@ public abstract class BaseTest<T> {
     [Test]
     [MatrixDataSource]
     public void Test2(bool condition)
+    {
+    }
+
+    [Test]
+    [Arguments(false)]
+    [Arguments(true)]
+    public void Test3(bool condition)
     {
     }
 }
