245684b829d36823e4571e536ef926181e79c5e4
James Alickolli <32421608+licon4812@users.noreply.github.com>
2025-02-25 09:26:47 +1100

    Created New TUnit Aspire templates (#1880)
    * Created Aspire templates
    
    * moved aspire templates
    
    * Created UI web test for aspire template
    
    * Added ApiTest to Aspire template
    
    * Reorganized aspire starter templates
    
    * Created aspire template tests
    
    * Fixed major code quality errors
    
    * Apply suggestions from code review
    
    Co-authored-by: Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
    
    * Removed Playwright from Aspire Starter template
    
    * Renamed Aspire Templates
    
    * Implemented HttpClientData class into aspire starter
    
    * updated Aspire Starter template tests
    
    * Updated Template TUnit.Aspire.Test
    
    * Commented out Apphost in TUnit.Aspire.Test
    
    * Commented out GlobalSetup in Aspire.Test template
    
    * undid formatting issues in TUnit.Basic tests.
    
    ---------
    
    Co-authored-by: Tom Longhurst <30480171+thomhurst@users.noreply.github.com>

---
+++ TUnit.Templates/content/TUnit.Aspire.Starter/ExampleNamespace.TestProject/Tests/ApiTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json;
+using ExampleNamespace.TestProject.Data;
+using ExampleNamespace.TestProject.Models;
+
+namespace ExampleNamespace.TestProject.Tests;
+
+[ClassDataSource<HttpClientDataClass>]
+public class ApiTests(HttpClientDataClass httpClientData)
+{
+    [Test]
+    public async Task GetWeatherForecastReturnsOkStatusCode()
+    {
+        // Arrange
+        var httpClient = httpClientData.HttpClient;
+        // Act
+        var response = await httpClient.GetAsync("/weatherforecast");
+        // Assert
+        await Assert.That(response.StatusCode).IsEqualTo(HttpStatusCode.OK);
+    } 
+
+    [Test]
+    [MatrixDataSource]
+    public async Task GetWeatherForecastReturnsCorrectData(
+        [Matrix("Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching")] string summary
+    )
+    {
+        // Arrange
+        var httpClient = httpClientData.HttpClient;
+        // Act
+        var response = await httpClient.GetAsync("/weatherforecast");
+        var content = await response.Content.ReadAsStringAsync();
+        var data = JsonSerializer.Deserialize<IEnumerable<WeatherForecast>>(content, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        // Assert
+        await Assert.That(data!.FirstOrDefault(w => w.Summary == summary)).IsNotNull();
+    }
+}
