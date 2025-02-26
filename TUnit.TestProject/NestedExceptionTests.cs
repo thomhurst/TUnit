566f9a31e3c38afff583763bfc6c61e570c5e7e6
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-02-26 13:05:30 +0000

    Shorten Stack Traces to user defined code for easier reading (#1942)
    * Shorten Stack Traces to user defined code for easier reading
    
    * Tidy comment

---
+++ TUnit.TestProject/NestedExceptionTests.cs
@@ -0,0 +1,39 @@
+﻿namespace TUnit.TestProject;
+
+public class NestedExceptionTests
+{
+    [Test]
+    public void Test()
+    {
+        Method1();
+    }
+
+    private void Method1()
+    {
+        try
+        {
+            Method2();
+        }
+        catch (Exception e)
+        {
+            throw new Exception("Thrown from Method1", e);
+        }
+    }
+    
+    private void Method2()
+    {
+        try
+        {
+            Method3();
+        }
+        catch (Exception e)
+        {
+            throw new ArgumentException("Thrown from Method2", e);
+        }
+    }
+    
+    private void Method3()
+    {
+        throw new InvalidOperationException("Thrown from Method3");
+    }
+}
