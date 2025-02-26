ff9c983104b11c2deb96a21e1985ffacb4184540
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-02-26 15:25:23 +0000

    Add tests to verify class data sources aren't disposed of too early

---
+++ TUnit.Engine.Tests/Bugs/Bug1924.cs
@@ -0,0 +1,20 @@
+﻿using Shouldly;
+
+namespace TUnit.Engine.Tests.Bugs;
+
+public class Bug1924 : InvokableTestBase
+{
+    [Test]
+    public async Task Test()
+    {
+        await RunTestsWithFilter(
+            "/*/TUnit.TestProject.Bugs._1924.*/*/*",
+            [
+                result => result.ResultSummary.Outcome.ShouldBe("Completed"),
+                result => result.ResultSummary.Counters.Total.ShouldBe(660),
+                result => result.ResultSummary.Counters.Passed.ShouldBe(660),
+                result => result.ResultSummary.Counters.Failed.ShouldBe(0),
+                result => result.ResultSummary.Counters.NotExecuted.ShouldBe(0)
+            ]);
+    }
+}
