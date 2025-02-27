f83bfee8ead305d062892319e8b64e7f9e6bf127
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-02-27 20:22:40 +0000

    SkipNetFramework

---
+++ TUnit.Engine.Tests/Attributes/SkipNetFrameworkAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.InteropServices;
+
+namespace TUnit.Engine.Tests.Attributes;
+
+public class SkipNetFrameworkAttribute(string reason) : SkipAttribute(reason)
+{
+    public override Task<bool> ShouldSkip(BeforeTestContext context)
+    {
+        return Task.FromResult(RuntimeInformation.FrameworkDescription.StartsWith(".NET Framework"));
+    }
+}
