0fe4b3e9e204baf287a60b4cb97f3a1c2cf357f0
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-02-27 21:21:25 +0000

    SkipNetFrameworkAttribute

--- TUnit.Engine.Tests/Attributes/SkipNetFrameworkAttribute.cs
+++ TUnit.Engine.Tests/Attributes/SkipNetFrameworkAttribute.cs
@@ -1,11 +1,11 @@
-﻿using System.Runtime.InteropServices;
-
-namespace TUnit.Engine.Tests.Attributes;
+﻿namespace TUnit.Engine.Tests.Attributes;
 
 public class SkipNetFrameworkAttribute(string reason) : SkipAttribute(reason)
 {
+    private static readonly string NetVersion = Environment.GetEnvironmentVariable("NET_VERSION") ?? "net9.0";
+
     public override Task<bool> ShouldSkip(BeforeTestContext context)
     {
-        return Task.FromResult(RuntimeInformation.FrameworkDescription.StartsWith(".NET Framework"));
+        return Task.FromResult(NetVersion == "net472");
     }
 }
