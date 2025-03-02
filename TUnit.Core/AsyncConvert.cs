02b9c41f698a7889f90ec00ba9eb9f35a86a30a7
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-03-02 15:17:21 +0000

    IsCompletedSuccessfully

--- TUnit.Core/AsyncConvert.cs
+++ TUnit.Core/AsyncConvert.cs
@@ -34,7 +34,7 @@ public static class AsyncConvert
     {
         var task = action();
 
-        if (task.IsCompleted)
+        if (task.IsCompletedSuccessfully)
         {
             return default;
         }
