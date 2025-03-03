b2b274d99d215c95e10655008f58fbdb6b7c680a
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-03-03 12:07:33 +0000

    Fix message not being initialized in `SkipTestException`

--- TUnit.Core/Exceptions/SkipTestException.cs
+++ TUnit.Core/Exceptions/SkipTestException.cs
@@ -1,6 +1,3 @@
 ﻿namespace TUnit.Core.Exceptions;
 
-public class SkipTestException(string reason) : TUnitException
-{
-    public string Reason { get; } = reason;
-}
+public class SkipTestException(string reason) : TUnitException(reason);
