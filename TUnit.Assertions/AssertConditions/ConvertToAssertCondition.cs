b59b6bfdc2d7fd17763dd5f448bf0eadb9e18e56
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-03-11 16:36:25 +0000

    +semver:minor - Add types to help converting from one type to another during an assertion chain (#2058)

---
+++ TUnit.Assertions/AssertConditions/ConvertToAssertCondition.cs
@@ -0,0 +1,22 @@
+﻿namespace TUnit.Assertions.AssertConditions;
+
+public abstract class ConvertToAssertCondition<TFromType, TToType> : BaseAssertCondition<TFromType>
+{
+    public abstract ValueTask<(AssertionResult, TToType?)> ConvertValue(TFromType value);
+    
+    public TToType? ConvertedValue { get; private set; }
+
+    protected override async ValueTask<AssertionResult> GetResult(TFromType? actualValue, Exception? exception, AssertionMetadata assertionMetadata)
+    {
+        if (actualValue is null)
+        {
+            return AssertionResult.Fail("The value was null.");
+        }
+
+        var (result, convertedValue) = await ConvertValue(actualValue);
+        
+        ConvertedValue = convertedValue;
+        
+        return result;
+    }
+}
---
+++ TUnit.TestProject/CustomAssertions/ProblemDetailsSourceGenerationContext.cs
@@ -0,0 +1,9 @@
+﻿#if NET
+using System.Text.Json.Serialization;
+
+namespace TUnit.TestProject.CustomAssertions;
+
+[JsonSourceGenerationOptions(WriteIndented = true)]
+[JsonSerializable(typeof(ProblemDetails))]
+public partial class ProblemDetailsSourceGenerationContext : JsonSerializerContext;
+#endif
