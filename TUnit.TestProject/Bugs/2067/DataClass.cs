1ab69632e8e2e826c1fb5840213075338503225b
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-03-11 21:28:25 +0000

    Fix `ITestStartEventReceiver` and `ITestEndEventReceiver` not firing for custom classes when injected in via `[ClassDataSource<>]`

---
+++ TUnit.TestProject/Bugs/2067/DataClass.cs
@@ -0,0 +1,51 @@
+﻿using TUnit.Core.Interfaces;
+
+namespace TUnit.TestProject.Bugs._2067;
+
+public class DataClass : 
+    IAsyncInitializer,
+    ITestRegisteredEventReceiver,
+    ITestStartEventReceiver,
+    ITestEndEventReceiver,
+    IAsyncDisposable
+{
+    public bool IsRegistered { get; set; }
+    public bool IsInitialized { get; set; }
+    public bool IsStarted { get; set; }
+    public bool IsEnded { get; set; }
+    public bool IsDisposed { get; private set; }
+
+    public Task InitializeAsync()
+    {
+        IsInitialized = true;
+        return Task.CompletedTask;
+    }
+
+    public ValueTask OnTestRegistered(TestRegisteredContext context)
+    {
+        IsRegistered = true;
+        return default;
+    }
+
+
+    public ValueTask OnTestStart(BeforeTestContext beforeTestContext)
+    {
+        IsStarted = true;
+        return default;
+    }
+
+
+    public ValueTask OnTestEnd(TestContext testContext)
+    {
+        IsEnded = true;
+        return default;
+    }
+
+    public ValueTask DisposeAsync()
+    {
+        IsDisposed = true;
+        return default;
+    }
+
+    public int Order => 0;
+}
