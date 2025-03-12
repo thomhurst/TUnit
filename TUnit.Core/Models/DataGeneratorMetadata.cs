a8c4e8a082ce16fe85a8528ca9dd60cc7a351869
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-03-12 19:54:29 +0000

    `DataGeneratorMetadata` exposes `ClassInstanceArguments` if the data source is for the test level and requires access to the class instance data

--- TUnit.Core/Models/DataGeneratorMetadata.cs
+++ TUnit.Core/Models/DataGeneratorMetadata.cs
@@ -11,4 +11,5 @@ public record DataGeneratorMetadata
     public required DataGeneratorType Type { get; init; }
     public required string TestSessionId { get; init; }
     public required object? TestClassInstance { get; init; }
+    public required object?[]? ClassInstanceArguments { get; init; }
 }
