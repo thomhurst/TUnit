a2ee6755ae018fb487e24541ffa23e475da60ef3
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-03-17 16:17:18 +0000

    Fix

--- TUnit.TestProject/Attributes/ClassDisplayNameAttribute.cs
+++ TUnit.TestProject/Attributes/ClassDisplayNameAttribute.cs
@@ -9,7 +9,7 @@ public class ClassDisplayNameAttribute : Attribute, ITestDiscoveryEventReceiver
 {
     public void OnTestDiscovery(DiscoveredTestContext discoveredTestContext)
     {
-        discoveredTestContext.SetDisplayName($"{discoveredTestContext.TestDetails.TestClass.Name}.{discoveredTestContext.TestContext.GetTestDisplayName()}");
+        // discoveredTestContext.SetDisplayName($"{discoveredTestContext.TestDetails.TestClass.Name}.{discoveredTestContext.TestContext.GetTestDisplayName()}");
     }
 
     public int Order { get; } = 0;
