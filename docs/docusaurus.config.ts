bb9a7fe7dcba224671a4263084bea4e0f920bce7
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-03-27 23:31:15 +0000

    Change baseUrl

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: 'https://thomhurst.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/TUnit/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
