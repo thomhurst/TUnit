cbff11a9563e4ee4c5460d17063983163e62bd92
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-03-28 19:08:42 +0000

    Asp.NET Tests (#2176)

---
+++ TUnit.Example.Asp.Net.TestProject/Tests.cs
@@ -0,0 +1,29 @@
+﻿using TUnit.Assertions;
+using TUnit.Assertions.Extensions;
+
+namespace TUnit.Example.Asp.Net.TestProject;
+
+[ClassDataSource<WebApplicationFactory>(Shared = SharedType.None)]
+[ClassDataSource<WebApplicationFactory>(Shared = SharedType.PerClass)]
+[ClassDataSource<WebApplicationFactory>(Shared = SharedType.PerAssembly)]
+[ClassDataSource<WebApplicationFactory>(Shared = SharedType.PerTestSession)]
+[ClassDataSource<WebApplicationFactory>(Shared = SharedType.Keyed, Key = "🔑")]
+public class Tests(WebApplicationFactory webApplicationFactory)
+{
+    [Repeat(5)]
+    [Test]
+    public async Task Test()
+    {
+        var client = webApplicationFactory.CreateClient();
+
+        var response = await client.GetAsync("/ping");
+
+        var stringContent = await response.Content.ReadAsStringAsync();
+
+        await Assert.That(stringContent).IsEqualTo("Hello, World!");
+        await Assert.That(webApplicationFactory.ConfiguredWebHostCalled).IsEqualTo(1);
+    }
+    
+    
+
+}
---
+++ TUnit.Example.Asp.Net.TestProject/WebApplicationFactory.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+using TUnit.Core.Interfaces;
+
+namespace TUnit.Example.Asp.Net.TestProject;
+
+public class WebApplicationFactory : WebApplicationFactory<Program>, IAsyncInitializer
+{
+    private int _configuredWebHostCalled;
+
+    public int ConfiguredWebHostCalled => _configuredWebHostCalled;
+
+    public Task InitializeAsync()
+    {
+        _ = Server;
+
+        return Task.CompletedTask;
+    }
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        Interlocked.Increment(ref _configuredWebHostCalled);
+        
+        builder.ConfigureAppConfiguration((context, configBuilder) =>
+        {
+            var testName = TestContext.Current!.TestDetails.TestName;
+            
+            var connectionString = testName + "-DummyConnectionString";
+            
+            configBuilder.AddInMemoryCollection(new Dictionary<string, string?>
+            {
+                { "ConnectionStrings:DefaultConnection", connectionString }
+            });
+        });
+    }
+}
