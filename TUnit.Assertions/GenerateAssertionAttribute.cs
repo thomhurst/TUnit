22aeb0e07e6f76d7f4b2751c2c177a176db133b4
Anna Sas <74367457+AnnaSasDev@users.noreply.github.com>
2025-05-02 18:04:01 +0200

    Early idea on generator

---
+++ TUnit.Assertions/GenerateAssertionAttribute.cs
@@ -0,0 +1,20 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+namespace TUnit.Assertions;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+// ReSharper disable once UnusedTypeParameter
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+public class GenerateIsAssertionAttribute<TBase>(string methodName) : Attribute {
+    public string MethodName { get; } = methodName;
+}
+
+// ReSharper disable once UnusedTypeParameter
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+public class GenerateIsNotAssertionAttribute<TBase>(string methodName) : Attribute {
+    public string MethodName { get; } = methodName;
+
+}
