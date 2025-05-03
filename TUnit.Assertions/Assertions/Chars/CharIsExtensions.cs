02eb5e6e06f1c19c8c5940728f608142181678e5
Anna Sas <74367457+AnnaSasDev@users.noreply.github.com>
2025-05-03 12:48:50 +0200

    Early work on actual Assertion generation

--- TUnit.Assertions/Assertions/Chars/CharIsExtensions.cs
+++ TUnit.Assertions/Assertions/Chars/CharIsExtensions.cs
@@ -6,7 +6,29 @@ namespace TUnit.Assertions.Assertions.Chars;
 // ---------------------------------------------------------------------------------------------------------------------
 // Code
 // ---------------------------------------------------------------------------------------------------------------------
+[GenerateIsAssertion<char>(nameof(char.IsAscii))]
+[GenerateIsAssertion<char>(nameof(char.IsAsciiDigit))]
+[GenerateIsAssertion<char>(nameof(char.IsAsciiLetterLower))]
+[GenerateIsAssertion<char>(nameof(char.IsAsciiLetterUpper))]
+[GenerateIsAssertion<char>(nameof(char.IsAsciiLetterOrDigit))]
+[GenerateIsAssertion<char>(nameof(char.IsAsciiHexDigit))]
+[GenerateIsAssertion<char>(nameof(char.IsAsciiHexDigitUpper))]
+[GenerateIsAssertion<char>(nameof(char.IsAsciiHexDigitLower))]
 [GenerateIsAssertion<char>(nameof(char.IsDigit))]
-public partial class CharIsExtensions {
+// [GenerateIsAssertion<char>(nameof(char.IsBetween))]
+[GenerateIsAssertion<char>(nameof(char.IsLetter))]
+[GenerateIsAssertion<char>(nameof(char.IsWhiteSpace))]
+[GenerateIsAssertion<char>(nameof(char.IsUpper))]
+[GenerateIsAssertion<char>(nameof(char.IsLower))]
+[GenerateIsAssertion<char>(nameof(char.IsPunctuation))]
+[GenerateIsAssertion<char>(nameof(char.IsLetterOrDigit))]
+[GenerateIsAssertion<char>(nameof(char.IsControl))]
+[GenerateIsAssertion<char>(nameof(char.IsNumber))]
+[GenerateIsAssertion<char>(nameof(char.IsSeparator))]
+[GenerateIsAssertion<char>(nameof(char.IsSurrogate))]
+[GenerateIsAssertion<char>(nameof(char.IsSymbol))]
+[GenerateIsAssertion<char>(nameof(char.IsHighSurrogate))]
+[GenerateIsAssertion<char>(nameof(char.IsLowSurrogate))]
+public static partial class CharIsExtensions {
     
 }
