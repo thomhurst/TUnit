e9db604033e4996717f8ba7ced20c4c5d0eb0cac
Anna Sas <anna.sas.dev@gmail.com>
2025-05-04 14:35:52 +0200

    Add `UriIsNotExtensions` for negated `Uri` assertions

---
+++ TUnit.Assertions/Assertions/Uris/UriIsNotExtensions.cs
@@ -0,0 +1,19 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+namespace TUnit.Assertions.Assertions.Uris;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+[GenerateAssertion<Uri>(AssertionType.IsNot, nameof(Uri.IsAbsoluteUri))]
+// [GenerateAssertion<Uri>(AssertionType.IsNot, nameof(Uri.IsBaseOf))] // TODO requires extra options
+[GenerateAssertion<Uri>(AssertionType.IsNot, nameof(Uri.IsDefaultPort))]
+[GenerateAssertion<Uri>(AssertionType.IsNot, nameof(Uri.IsFile))]
+[GenerateAssertion<Uri>(AssertionType.IsNot, nameof(Uri.IsLoopback))]
+[GenerateAssertion<Uri>(AssertionType.IsNot, nameof(Uri.IsUnc))]
+[GenerateAssertion<Uri>(AssertionType.IsNot, nameof(Uri.IsWellFormedOriginalString))]
+// [GenerateAssertion<Uri>(AssertionType.IsNot, nameof(Uri.IsWellFormedUriString))] //  TODO requires extra options
+public static partial class UriIsNotExtensions {
+    
+}
