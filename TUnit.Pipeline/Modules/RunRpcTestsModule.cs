e7d99dacf55cab35ac37ef9fb2f557985bc2e43d
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-05-04 17:09:24 +0100

    framework

--- TUnit.Pipeline/Modules/RunRpcTestsModule.cs
+++ TUnit.Pipeline/Modules/RunRpcTestsModule.cs
@@ -22,7 +22,7 @@ public class RunRpcTestsModule : TestBaseModule
             WorkingDirectory = project.Folder!,
             NoBuild = true,
             Configuration = Configuration.Release,
-            Framework = "net8.0",
+            Framework = framework,
             EnvironmentVariables = new Dictionary<string, string?>
             {
                 ["DISABLE_GITHUB_REPORTER"] = "true",
