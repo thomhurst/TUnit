974596020479e98a46e7f83585f5976b988657e8
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-05-10 22:34:12 +0100

    Update snaps

--- TUnit.Core.SourceGenerator.Tests/AssemblyLoaderTests.cs
+++ TUnit.Core.SourceGenerator.Tests/AssemblyLoaderTests.cs
@@ -9,7 +9,7 @@ internal class AssemblyLoaderTests : TestsBase<AssemblyLoaderGenerator>
     [Test]
     public Task Test() => RunTest(Path.Combine(Git.RootDirectory.FullName,
             "TUnit.TestProject",
-            "AssemblyLoaderTest.cs"),
+            "BasicTests.cs"),
         new RunTestOptions
         {
             VerifyConfigurator = verify =>
@@ -24,20 +24,10 @@ internal class AssemblyLoaderTests : TestsBase<AssemblyLoaderGenerator>
 
                         return line;
                     });
-            },
-            AdditionalPackages = 
-            [
-                new PackageIdentity("Confluent.SchemaRegistry.Serdes.Protobuf", "2.10.0"),
-                new PackageIdentity("WireMock.Net", "1.7.4"),
-            ],
+            }
         },
         async generatedFiles =>
         {
-            await Assert.That(generatedFiles)
-                .HasSingleItem()
-                .And
-                .DoesNotContain("typeof(global::Google.Protobuf.Reflection.FileDescriptorSet).Assembly")
-                .And
-                .DoesNotContain("global::Google.Protobuf.WellKnownTypes.Any).Assembly");
+            await Assert.That(generatedFiles).HasSingleItem();
         });
 }
