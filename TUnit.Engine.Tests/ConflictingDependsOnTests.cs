298a49df1bc81782c355ac126792e79667754190
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-05-12 22:31:19 +0100

    Fix assertions

--- TUnit.Engine.Tests/ConflictingDependsOnTests.cs
+++ TUnit.Engine.Tests/ConflictingDependsOnTests.cs
@@ -16,8 +16,8 @@ public class ConflictingDependsOnTests(TestMode testMode) : InvokableTestBase(te
                 result => result.ResultSummary.Counters.Passed.ShouldBe(0),
                 result => result.ResultSummary.Counters.Failed.ShouldBe(2),
                 result => result.ResultSummary.Counters.NotExecuted.ShouldBe(0),
-                result => result.Results.First(x => x.TestName == "Test1").Output?.ErrorInfo?.Message.ShouldContain("DependsOn Conflict: Test1 > Test2 > Test1"),
-                result => result.Results.First(x => x.TestName == "Test2").Output?.ErrorInfo?.Message.ShouldContain("DependsOn Conflict: Test2 > Test1 > Test2"),
+                result => result.Results.First(x => x.TestName == "Test1").Output?.ErrorInfo?.Message.ShouldContain("DependsOn Conflict: ConflictingDependsOnTests.Test1 > ConflictingDependsOnTests.Test2 > ConflictingDependsOnTests.Test1"),
+                result => result.Results.First(x => x.TestName == "Test2").Output?.ErrorInfo?.Message.ShouldContain("DependsOn Conflict: ConflictingDependsOnTests.Test2 > ConflictingDependsOnTests.Test1 > ConflictingDependsOnTests.Test2"),
 
             ]);
     }
