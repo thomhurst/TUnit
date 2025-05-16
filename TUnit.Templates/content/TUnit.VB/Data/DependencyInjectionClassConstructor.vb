97e425d92c4f0cbe05c1e9e022f7bef7c72c370c
James Alickolli <jalickolli@gmail.com>
2025-05-16 14:37:57 +1000

    Created basic VB template

---
+++ TUnit.Templates/content/TUnit.VB/Data/DependencyInjectionClassConstructor.vb
@@ -0,0 +1,31 @@
+﻿Imports System
+Imports TUnit.Core
+Imports TUnit.Core.Interfaces
+
+Namespace TestProject
+
+    'Public Class DependencyInjectionClassConstructor
+    '    Implements IClassConstructor
+
+    '    Public Function Create(Of T As Class)(classConstructorMetadata As ClassConstructorMetadata) As T
+    '        Console.WriteLine("You can also control how your test classes are new'd up, giving you lots of power and the ability to utilise tools such as dependency injection")
+
+    '        If GetType(T) Is GetType(AndEvenMoreTests) Then
+    '            Return CType(DirectCast(New AndEvenMoreTests(New DataClass()), Object), T)
+    '        End If
+
+    '        Throw New NotImplementedException()
+    '    End Function
+
+    '    ' Explicit interface implementation for IClassConstructor.Create(Type, ClassConstructorMetadata)
+    '    Private Function IClassConstructor_Create(type As Type, classConstructorMetadata As ClassConstructorMetadata) As Object Implements IClassConstructor.Create
+    '        If type Is GetType(AndEvenMoreTests) Then
+    '            Return New AndEvenMoreTests(New DataClass())
+    '        End If
+
+    '        Throw New NotImplementedException()
+    '    End Function
+
+    'End Class
+
+End Namespace
