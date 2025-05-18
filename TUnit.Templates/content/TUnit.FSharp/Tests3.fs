3d4391931b0d2ff3dc88941d6cb652eaf9808494
James Alickolli <jalickolli@gmail.com>
2025-05-19 09:44:56 +1000

    updated vb and f# tests 3

--- TUnit.Templates/content/TUnit.FSharp/Tests3.fs
+++ TUnit.Templates/content/TUnit.FSharp/Tests3.fs
@@ -3,13 +3,13 @@
 open System
 open TUnit.Core
 
-//[<ClassDataSource(typeof<DataClass>)>]
-//[<ClassConstructor(typeof<DependencyInjectionClassConstructor>)>]
-//type AndEvenMoreTests(dataClass: DataClass) =
+[<ClassDataSource(typeof<DataClass>)>]
+[<ClassConstructor(typeof<DependencyInjectionClassConstructor>)>]
+type AndEvenMoreTests(dataClass: DataClass) =
 
-//    [<Test>]
-//    member _.HaveFun() =
-//        Console.WriteLine(dataClass)
-//        Console.WriteLine("For more information, check out the documentation")
-//        Console.WriteLine("https://tunit.dev/")
+    [<Test>]
+    member _.HaveFun() =
+        Console.WriteLine(dataClass)
+        Console.WriteLine("For more information, check out the documentation")
+        Console.WriteLine("https://tunit.dev/")
 
