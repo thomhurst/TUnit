0aa8743dbfbcb22e022e2b47bbbe93b2e979fcc0
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-05-19 01:22:49 +0100

    chore(deps): update tunit to 0.20.16 (#2384)
    * chore(deps): update tunit to 0.20.16
    
    * Fix
    
    * Update snaps
    
    ---------
    
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- TUnit.Templates/content/TUnit/Data/DependencyInjectionClassConstructor.cs
+++ TUnit.Templates/content/TUnit/Data/DependencyInjectionClassConstructor.cs
@@ -4,13 +4,13 @@ namespace TestProject;
 
 public class DependencyInjectionClassConstructor : IClassConstructor
 {
-    public T Create<T>(ClassConstructorMetadata classConstructorMetadata) where T : class
+    public object Create(Type type, ClassConstructorMetadata classConstructorMetadata)
     {
         Console.WriteLine(@"You can also control how your test classes are new'd up, giving you lots of power and the ability to utilise tools such as dependency injection");
 
-        if (typeof(T) == typeof(AndEvenMoreTests))
+        if (type == typeof(AndEvenMoreTests))
         {
-            return (new AndEvenMoreTests(new DataClass()) as T)!;
+            return new AndEvenMoreTests(new DataClass());
         }
 
         throw new NotImplementedException();
