7f19f92b29d69b306aa1fd7f33668c08da6b6834
James Alickolli <jalickolli@gmail.com>
2025-05-20 08:44:04 +1000

    Finished f# template

--- TUnit.Templates/content/TUnit.FSharp/Data/DependencyInjectionClassConstructor.fs
+++ TUnit.Templates/content/TUnit.FSharp/Data/DependencyInjectionClassConstructor.fs
@@ -1,14 +1,18 @@
 ﻿namespace TestProject
 
 open System
+open Microsoft.Extensions.DependencyInjection
 open TUnit.Core.Interfaces
 open TUnit.Core
 
 type DependencyInjectionClassConstructor() =
+    let serviceProvider =
+        ServiceCollection()
+            // Register your dependencies here, e.g.:
+            .AddTransient<DataClass>()
+            .BuildServiceProvider()
+
     interface IClassConstructor with
-        member _.Create(typ: System.Type, classConstructorMetadata: ClassConstructorMetadata) : obj =
+        member _.Create(typ: Type, _: ClassConstructorMetadata) : obj =
             Console.WriteLine("You can also control how your test classes are new'd up, giving you lots of power and the ability to utilise tools such as dependency injection")
-            //if typ = typeof<AndEvenMoreTests> then
-            //    AndEvenMoreTests(DataClass()) :> obj
-            //else
-            //    raise (NotImplementedException())
+            ActivatorUtilities.GetServiceOrCreateInstance(serviceProvider, typ)
