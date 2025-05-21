36bc40c1720cd306d9db408e2ff08cea291b301a
James Alickolli <32421608+licon4812@users.noreply.github.com>
2025-05-21 21:00:55 +1000

    F# ASP.NET templates (#2429)
    * created f# template
    
    * completed f# ASP template tests
    
    * updated FSharp.AspNet template config
    
    * Added FSharp.Asp template test
    
    * removed reference to TUnit.Assertions in FSharp ASP templates

---
+++ TUnit.Templates.Tests/Snapshots/InstantiationTestWithFSharp.TUnit.AspNet.FSharp._.verified/TUnit.AspNet.FSharp/TUnit.AspNet.FSharp/GlobalSetup.fs
@@ -0,0 +1,7 @@
+﻿namespace TUnit.AspNet.FSharp
+
+// Here you could define global logic that would affect all tests
+
+// You can use attributes at the assembly level to apply to all tests in the assembly
+[<assembly: System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage>]
+do ()
