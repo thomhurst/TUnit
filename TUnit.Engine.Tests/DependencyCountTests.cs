7051f5fa5d7a5847c2f3fd9336ea9da3ed1e7ef6
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-05-22 19:35:38 +0100

    Fix multiple data-driven tests not all being picked up in a [DependsOn] (#2436)
    * Fix multiple data-driven tests not all being picked up in a [DependsOn]
    
    * Engine test

---
+++ TUnit.Engine.Tests/DependencyCountTests.cs
@@ -0,0 +1,20 @@
+﻿using Shouldly;
+using TUnit.Engine.Tests.Enums;
+
+namespace TUnit.Engine.Tests;
+
+public class DependencyCountTests(TestMode testMode) : InvokableTestBase(testMode)
+{
+    public async Task Test()
+    {
+        await RunTestsWithFilter(
+            "/*/*/DependencyCountTests/*",
+            [
+                result => result.ResultSummary.Outcome.ShouldBe("Completed"),
+                result => result.ResultSummary.Counters.Total.ShouldBe(8),
+                result => result.ResultSummary.Counters.Passed.ShouldBe(8),
+                result => result.ResultSummary.Counters.Failed.ShouldBe(0),
+                result => result.ResultSummary.Counters.NotExecuted.ShouldBe(0),
+            ]);
+    }
+}
