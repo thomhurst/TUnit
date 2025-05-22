fb00c0aa3704b3e7d265c4ea9ed9b434066c9a25
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-05-22 20:04:59 +0100

    +semver:minor - Don't overwrite LangVersion if set and check raise diagnostic error if level not high enough (#2437)

---
+++ TUnit.Core.SourceGenerator/CodeGenerators/LanguageVersionCheckGenerator.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace TUnit.Core.SourceGenerator;
+
+[Generator]
+public class LanguageVersionCheckGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        var settings = context.CompilationProvider
+            .Select((c, _)  => 
+            {
+                LanguageVersion? csharpVersion = c is CSharpCompilation comp
+                    ? comp.LanguageVersion
+                    : null;
+                
+                return csharpVersion;
+            });
+
+        context.RegisterSourceOutput(settings, static (sourceProductionContext, languageVersion) =>
+        {
+            if (languageVersion is null)
+            {
+                return;
+            }
+            
+            if((int)languageVersion.Value < 1200)
+            {
+                sourceProductionContext.ReportDiagnostic(Diagnostic.Create(
+                    new DiagnosticDescriptor(
+                        "TUNIT_LANG_001",
+                        "Language Version Check",
+                        "TUnit requires C# 12 or higher when using Source Generation.",
+                        "Usage",
+                        DiagnosticSeverity.Error,
+                        true),
+                    Location.None));
+            }
+        });
+    }
+}
