21cb45aa155ae716f621c4fd9e39a7106668c2ce
James Alickolli <32421608+licon4812@users.noreply.github.com>
2025-05-22 16:52:52 +1000

    Updated TUnit.Assertions.FSharp.Operations check function to support assertions with Throw Type (#2431)
    * Updated Check to support assertions with Throw Type
    
    * removed unused open declarations in Extensions.fs
    
    * updated fsharp assertions readme

--- TUnit.Assertions.FSharp/Extensions.fs
+++ TUnit.Assertions.FSharp/Extensions.fs
@@ -1,18 +1,34 @@
 ﻿namespace TUnit.Assertions.FSharp
 
 open TUnit.Assertions.AssertionBuilders
+open TUnit.Assertions.AssertConditions.Throws
 
 module Operations =
     [<CustomOperation(MaintainsVariableSpaceUsingBind = true)>]
-    let check (assertion: IInvokableAssertionBuilder) =
+    let check (assertion: 'T) =
         Async.FromContinuations(fun (cont, econt, ccont) ->
-            let awaiter = assertion.GetAwaiter()
-
-            awaiter.OnCompleted(fun () ->
-                try
-                    if awaiter.IsCompleted then
-                        cont(awaiter.GetResult())
-                    else
-                        ccont (System.OperationCanceledException())
-                with ex ->
-                    econt ex))
+            match box assertion with
+            | :? IInvokableAssertionBuilder as invokable ->
+                let awaiter = invokable.GetAwaiter()
+                awaiter.OnCompleted(fun () ->
+                    try
+                        if awaiter.IsCompleted then
+                            cont(awaiter.GetResult())
+                        else
+                            ccont (System.OperationCanceledException())
+                    with ex ->
+                        econt ex)
+            | :? ThrowsException<obj, exn> as throwsExn ->
+                let awaiter = throwsExn.GetAwaiter()
+                awaiter.OnCompleted(fun () ->
+                    try
+                        if awaiter.IsCompleted then
+                            let _ = awaiter.GetResult() // ignore the exn result
+                            cont ()
+                        else
+                            ccont (System.OperationCanceledException())
+                    with ex ->
+                        econt ex)
+            | _ ->
+                invalidOp $"Unsupported assertion type: We currently don't support Assertion Type {assertion.GetType()}"
+        )
