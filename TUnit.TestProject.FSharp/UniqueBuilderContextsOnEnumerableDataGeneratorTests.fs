2ece1e00c4a696d80a54469d405e8a58e2590d8a
James Alickolli <32421608+licon4812@users.noreply.github.com>
2025-05-22 11:54:23 +0000

    Refactor UniqueBuilderContextsOnEnumerableDataGeneratorTests for clarity and consistency

--- TUnit.TestProject.FSharp/UniqueBuilderContextsOnEnumerableDataGeneratorTests.fs
+++ TUnit.TestProject.FSharp/UniqueBuilderContextsOnEnumerableDataGeneratorTests.fs
@@ -1,23 +1,29 @@
 namespace TUnit.TestProject.FSharp
 
+open System
 open System.Collections.Generic
 open TUnit.Core
+open Shouldly
 
-// Equivalent of UniqueBuilderContextsOnEnumerableDataGeneratorTests.cs
+// F# equivalent of UniqueBuilderContextsOnEnumerableDataGeneratorTests.cs
 
 type UniqueBuilderContextsOnEnumerableDataGeneratorTests() =
-    [<Test; UniqueBuilderContextsOnEnumerableDataGeneratorTestsGenerator>]
+    [<Test>]
+    [<UniqueBuilderContextsOnEnumerableDataGeneratorTestsGenerator>]
     member _.Test(value: int) = ()
 
-and UniqueBuilderContextsOnEnumerableDataGeneratorTestsGenerator() =
+type UniqueBuilderContextsOnEnumerableDataGeneratorTestsGenerator() =
     inherit DataSourceGeneratorAttribute<int>()
     override _.GenerateDataSources(dataGeneratorMetadata) =
+        let id1 = dataGeneratorMetadata.TestBuilderContext.Current.Id
+        let id2 = dataGeneratorMetadata.TestBuilderContext.Current.Id
         seq {
-            let id1 = dataGeneratorMetadata.TestBuilderContext.Current.Id
-            let id2 = dataGeneratorMetadata.TestBuilderContext.Current.Id
             yield (fun () -> 1)
             let id3 = dataGeneratorMetadata.TestBuilderContext.Current.Id
             yield (fun () -> 2)
             let id4 = dataGeneratorMetadata.TestBuilderContext.Current.Id
-            // ShouldBe/ShouldNotBe logic can be implemented with F# assertions or left as comments
+            id1.ShouldBe(id2)
+            id3.ShouldNotBe(id1)
+            id4.ShouldNotBe(id1)
+            id4.ShouldNotBe(id3)
         }
