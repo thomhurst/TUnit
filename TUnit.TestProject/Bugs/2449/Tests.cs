e16c1ce30c2405d243e50c60152072b4d543159f
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-05-25 12:59:29 +0100

    Fix DependsOn not working when a test takes a reference type argument

---
+++ TUnit.TestProject/Bugs/2449/Tests.cs
@@ -0,0 +1,32 @@
+﻿using TUnit.Core.Interfaces;
+
+namespace TUnit.TestProject.Bugs._2449;
+
+[ClassDataSource<SampleDataClass>]
+public sealed class Tests(SampleDataClass arg)
+{
+    [Test]
+    public async Task Test1()
+    {
+        var value = arg.Value;
+
+        await Assert.That(value).IsTrue();
+    }
+
+    [Test]
+    [DependsOn(nameof(Test1))]
+    public async Task Test2()
+    {
+        var value = arg.Value;
+
+        await Assert.That(value).IsTrue();
+    }
+}
+
+public sealed class SampleDataClass : IAsyncInitializer, IAsyncDisposable
+{
+    public bool Value { get; } = true;
+
+    public Task InitializeAsync() => Task.CompletedTask;
+    public ValueTask DisposeAsync() => default;
+}
