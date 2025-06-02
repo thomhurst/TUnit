a38c903efd1a390683bb37d4122aed17151368ac
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-06-02 22:01:24 +0100

    Fixes

--- TUnit.TestProject/InheritedPropertySetterTests.cs
+++ TUnit.TestProject/InheritedPropertySetterTests.cs
@@ -1,7 +1,4 @@
-﻿using TUnit.TestProject.Attributes;
+﻿namespace TUnit.TestProject;
 
-namespace TUnit.TestProject;
-
-[EngineTest(ExpectedResult.Pass)]
 [InheritsTests]
 public class InheritedPropertySetterTests : PropertySetterTests;
