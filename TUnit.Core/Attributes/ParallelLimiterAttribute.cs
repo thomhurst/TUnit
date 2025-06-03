94bec29a1bc7ecd9ba257a0caee890985aed7981
Martin Stühmer <me@samtrion.net>
2025-06-03 13:43:19 +0200

    docs: Further XML Summaries for `TUnit.Core/Attributes`

--- TUnit.Core/Attributes/ParallelLimiterAttribute.cs
+++ TUnit.Core/Attributes/ParallelLimiterAttribute.cs
@@ -2,12 +2,61 @@
 
 namespace TUnit.Core;
 
+/// <summary>
+/// Limits the number of tests that can run in parallel for a test assembly, class, or method.
+/// </summary>
+/// <typeparam name="TParallelLimit">
+/// The type that implements <see cref="IParallelLimit"/> and defines the maximum number 
+/// of tests that can execute concurrently.
+/// </typeparam>
+/// <remarks>
+/// <para>
+/// This attribute controls the degree of parallelism for test execution. When applied to a test assembly,
+/// class, or method, it limits how many tests from that scope can run simultaneously.
+/// </para>
+/// <para>
+/// The parallelism limit is defined by the <see cref="IParallelLimit.Limit"/> property of the 
+/// <typeparamref name="TParallelLimit"/> instance. This value is used to create a semaphore that
+/// controls concurrent test execution.
+/// </para>
+/// <para>
+/// Common implementations include:
+/// <list type="bullet">
+/// <item><description><c>DefaultParallelLimit</c> - Uses <see cref="Environment.ProcessorCount"/> as the limit</description></item>
+/// <item><description>Custom implementations with fixed limits (e.g., <c>ParallelLimit3</c> with a limit of 3)</description></item>
+/// </list>
+/// </para>
+/// <para>
+/// Example usage:
+/// <code>
+/// // Apply to an assembly to limit all tests
+/// [assembly: ParallelLimiter&lt;DefaultParallelLimit&gt;]
+/// 
+/// // Apply to a class to limit tests in that class
+/// [ParallelLimiter&lt;ParallelLimit3&gt;]
+/// public class MyTestClass
+/// {
+///     // Tests in this class will run with a maximum of 3 in parallel
+/// }
+/// 
+/// // Apply to a specific test method
+/// [Test]
+/// [ParallelLimiter&lt;ParallelLimit1&gt;]
+/// public void MyTest()
+/// {
+///     // This test will run exclusively (limit of 1)
+/// }
+/// </code>
+/// </para>
+/// </remarks>
 [AttributeUsage(AttributeTargets.Assembly | AttributeTargets.Class | AttributeTargets.Method)]
 public sealed class ParallelLimiterAttribute<TParallelLimit> : TUnitAttribute, ITestRegisteredEventReceiver
     where TParallelLimit : IParallelLimit, new()
 {
+    /// <inheritdoc />
     public int Order => 0;
 
+    /// <inheritdoc />
     public ValueTask OnTestRegistered(TestRegisteredContext testRegisteredContext)
     {
         testRegisteredContext.SetParallelLimiter(new TParallelLimit());
--- TUnit.Core/Attributes/Tests/InheritsTestsAttribute.cs
+++ TUnit.Core/Attributes/Tests/InheritsTestsAttribute.cs
@@ -1,4 +1,11 @@
 ﻿namespace TUnit.Core;
 
+/// <summary>
+/// Marks a class as inheriting test methods from its base classes.
+/// </summary>
+/// <remarks>
+/// This attribute indicates to the TUnit test runner that test methods defined in base classes 
+/// should be considered part of the derived class's test suite.
+/// </remarks>
 [AttributeUsage(AttributeTargets.Class)]
 public sealed class InheritsTestsAttribute : TUnitAttribute;
