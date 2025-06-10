f0d6a29f38e09449926ec406144965919a531893
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-06-10 09:46:25 +0100

    Update Throws.ExceptionTests.cs

--- TUnit.Assertions.Tests/Assertions/Delegates/Throws.ExceptionTests.cs
+++ TUnit.Assertions.Tests/Assertions/Delegates/Throws.ExceptionTests.cs
@@ -27,7 +27,7 @@ public partial class Throws
         }
 
         [Test]
-        [UnconditionalSuppressMessage("Trimming", "IL2026:Members annotated with 'RequiresUnreferencedCodeAttribute' require dynamic access otherwise can break functionality when trimming application code", Justification = "<Pending>")]
+        [UnconditionalSuppressMessage("Trimming", "IL2026:Members annotated with 'RequiresUnreferencedCodeAttribute' require dynamic access otherwise can break functionality when trimming application assemblies.", Justification = "Test method.")]
         public async Task Returns_Exception_When_Awaited()
         {
             Exception exception = CreateCustomException();
@@ -50,21 +50,22 @@ public partial class Throws
             await Assert.That(sut).ThrowsNothing();
         }
 
-        private void ThrowsArgumentException() => throw new ArgumentException("Just for testing");
+        private void ThrowsArgumentException()
+            => throw new ArgumentException("Just for testing");
 
-  [Test]
-  public async Task ThrowsAsync_DoesNotCheckType()
-  {
-      await Assert.That(async () =>
-          await Assert.ThrowsAsync<OverflowException>(LocalTestFunction)
-      ).Throws<AssertionException>();
-      return;
+        [Test]
+        public async Task ThrowsAsync_DoesNotCheckType()
+        {
+            await Assert.That(async () =>
+                await Assert.ThrowsAsync<OverflowException>(LocalTestFunction)
+            ).Throws<AssertionException>();
+            return;
 
-    Task LocalTestFunction()
-    {
-      ThrowsArgumentException();
-      return Task.CompletedTask;
-    }
-  }
+            Task LocalTestFunction()
+            {
+                ThrowsArgumentException();
+                return Task.CompletedTask;
+            }
+        }
     }
 }
