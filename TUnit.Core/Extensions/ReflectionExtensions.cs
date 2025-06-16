3faef09c9f7e560257992cd052b04bd66ac20df8
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-06-16 16:19:18 +0100

    Extension

--- TUnit.Core/Extensions/ReflectionExtensions.cs
+++ TUnit.Core/Extensions/ReflectionExtensions.cs
@@ -2,9 +2,9 @@
 
 namespace TUnit.Core.Extensions;
 
-internal static class ReflectionExtensions
+public static class ReflectionExtensions
 {
-    public static string GetFormattedName(this Type type)
+    internal static string GetFormattedName(this Type type)
     {
         if (!type.IsGenericType)
         {
@@ -20,22 +20,166 @@ internal static class ReflectionExtensions
         }
         
         return $"{type.Name[..backtickIndex]}<{genericArguments}>";
-
     }
 
-    public static bool HasExactAttribute<T>(this MemberInfo member)
+    internal static bool HasExactAttribute<T>(this MemberInfo member)
     {
         return HasAttribute<T>(member, false);
     }
 
-    public static bool HasAttribute<T>(this MemberInfo member, bool inherit = true)
+    internal static bool HasAttribute<T>(this MemberInfo member, bool inherit = true)
     {
-        if (typeof(T).IsAssignableTo(typeof(Attribute)))
+        try
         {
-            return member.IsDefined(typeof(T), inherit);
-        }
+            if (typeof(T).IsAssignableTo(typeof(Attribute)))
+            {
+                return member.IsDefined(typeof(T), inherit);
+            }
 
-        return member.GetCustomAttributes(inherit)
-            .Any(x => x.GetType().IsAssignableTo(typeof(T)));
+            return member.GetCustomAttributes(inherit)
+                .Any(x => x.GetType().IsAssignableTo(typeof(T)));
+        }
+        catch (NotSupportedException ex) when (ex.Message.Contains("Generic types are not valid"))
+        {
+            // Fall back to safe method for .NET Framework
+            return member.GetCustomAttributesSafe(inherit)
+                .Any(x => x.GetType().IsAssignableTo(typeof(T)));
+        }
+    }
+
+    /// <summary>
+    /// Gets custom attributes from a MemberInfo, handling generic attributes on .NET Framework.
+    /// </summary>
+    /// <param name="member">The member to get attributes from</param>
+    /// <param name="inherit">Whether to search the inheritance chain</param>
+    /// <returns>An array of attributes</returns>
+    /// <remarks>
+    /// This method works around the limitation in .NET Framework where GetCustomAttributes
+    /// throws NotSupportedException for generic attributes.
+    /// </remarks>
+    public static Attribute[] GetCustomAttributesSafe(this MemberInfo member, bool inherit = true)
+    {
+        try
+        {
+            return member.GetCustomAttributes(inherit).Cast<Attribute>().ToArray();
+        }
+        catch (NotSupportedException ex) when (ex.Message.Contains("Generic types are not valid"))
+        {
+            // .NET Framework doesn't support generic attributes via GetCustomAttributes
+            // We need to use CustomAttributeData instead
+            return GetAttributesViaCustomAttributeData(member, inherit);
+        }
+    }
+
+    /// <summary>
+    /// Gets custom attributes of a specific type from a MemberInfo, handling generic attributes on .NET Framework.
+    /// </summary>
+    /// <typeparam name="T">The type of attribute to get</typeparam>
+    /// <param name="member">The member to get attributes from</param>
+    /// <param name="inherit">Whether to search the inheritance chain</param>
+    /// <returns>An array of attributes of the specified type</returns>
+    public static T[] GetCustomAttributesSafe<T>(this MemberInfo member, bool inherit = true) where T : Attribute
+    {
+        try
+        {
+            return member.GetCustomAttributes(typeof(T), inherit).Cast<T>().ToArray();
+        }
+        catch (NotSupportedException ex) when (ex.Message.Contains("Generic types are not valid"))
+        {
+            // .NET Framework doesn't support generic attributes via GetCustomAttributes
+            // We need to use CustomAttributeData instead
+            return GetAttributesViaCustomAttributeData(member, inherit)
+                .OfType<T>()
+                .ToArray();
+        }
+    }
+
+    private static Attribute[] GetAttributesViaCustomAttributeData(MemberInfo member, bool inherit)
+    {
+        var attributes = new List<Attribute>();
+        
+        // Get attributes from the member itself
+        var customAttributeDataList = CustomAttributeData.GetCustomAttributes(member);
+        
+        foreach (var attributeData in customAttributeDataList)
+        {
+            try
+            {
+                var attribute = CreateAttributeInstance(attributeData);
+                if (attribute != null)
+                {
+                    attributes.Add(attribute);
+                }
+            }
+            catch
+            {
+                // Skip attributes that can't be instantiated
+            }
+        }
+        
+        // If inherit is true and member is a Type, get attributes from base types
+        if (inherit && member is Type type)
+        {
+            var baseType = type.BaseType;
+            while (baseType != null && baseType != typeof(object))
+            {
+                attributes.AddRange(GetAttributesViaCustomAttributeData(baseType, false));
+                baseType = baseType.BaseType;
+            }
+        }
+        
+        return attributes.ToArray();
+    }
+
+    [System.Diagnostics.CodeAnalysis.UnconditionalSuppressMessage("AOT", "IL2072:Target type's member does not satisfy requirements", Justification = "Attribute types are preserved by the runtime")]
+    [System.Diagnostics.CodeAnalysis.UnconditionalSuppressMessage("AOT", "IL3050:Calling members annotated with 'RequiresDynamicCodeAttribute' may break functionality when AOT compiling.", Justification = "Attribute instantiation is required for .NET Framework compatibility")]
+    private static Attribute? CreateAttributeInstance(CustomAttributeData attributeData)
+    {
+        var attributeType = attributeData.AttributeType;
+        
+        // Skip if it's a generic type definition (not a constructed generic type)
+        if (attributeType.IsGenericTypeDefinition)
+        {
+            return null;
+        }
+        
+        // Get constructor arguments
+        var constructorArgs = attributeData.ConstructorArguments
+            .Select(arg => arg.Value)
+            .ToArray();
+        
+        // Try to create instance
+        Attribute? attribute = null;
+        
+        if (constructorArgs.Length == 0)
+        {
+            // Try parameterless constructor
+            attribute = Activator.CreateInstance(attributeType) as Attribute;
+        }
+        else
+        {
+            // Try constructor with parameters
+            attribute = Activator.CreateInstance(attributeType, constructorArgs) as Attribute;
+        }
+        
+        if (attribute == null)
+        {
+            return null;
+        }
+        
+        // Set named arguments (properties and fields)
+        foreach (var namedArg in attributeData.NamedArguments)
+        {
+            if (namedArg.MemberInfo is PropertyInfo property)
+            {
+                property.SetValue(attribute, namedArg.TypedValue.Value);
+            }
+            else if (namedArg.MemberInfo is FieldInfo field)
+            {
+                field.SetValue(attribute, namedArg.TypedValue.Value);
+            }
+        }
+        
+        return attribute;
     }
 }
