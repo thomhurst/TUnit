4866b07e69bcf996fc70882cf707dfe9f514eed2
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-06-20 13:37:36 +0100

    Tests

---
+++ TUnit.TestProject/PropertyInitTest.cs
@@ -0,0 +1,37 @@
+using TUnit.Core;
+using TUnit.Core.Interfaces;
+using TUnit.Assertions;
+
+namespace TUnit.TestProject;
+
+public class PropertyInitTest
+{
+    [SimpleDataGenerator]
+    public required InitializableService Service { get; set; }
+    
+    [Test]
+    public async Task Service_Should_Be_Initialized()
+    {
+        await Assert.That(Service).IsNotNull();
+        await Assert.That(Service.IsInitialized).IsTrue();
+    }
+}
+
+public class InitializableService : IAsyncInitializer
+{
+    public bool IsInitialized { get; private set; }
+    
+    public Task InitializeAsync()
+    {
+        IsInitialized = true;
+        return Task.CompletedTask;
+    }
+}
+
+public class SimpleDataGeneratorAttribute : DataSourceGeneratorAttribute<InitializableService>
+{
+    protected override IEnumerable<Func<InitializableService>> GenerateDataSources(DataGeneratorMetadata dataGeneratorMetadata)
+    {
+        yield return () => new InitializableService();
+    }
+}
