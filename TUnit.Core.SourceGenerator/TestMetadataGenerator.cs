b9522058db6a39e554a52989d4740f80fff6f95e
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-06-23 19:24:24 +0100

    WIP

--- TUnit.Core.SourceGenerator/TestMetadataGenerator.cs
+++ TUnit.Core.SourceGenerator/TestMetadataGenerator.cs
@@ -122,22 +122,120 @@ public class TestMetadataGenerator : IIncrementalGenerator
             {
                 // Determine if we can use StaticTestDefinition (AOT-compatible)
                 bool canUseStaticDefinition = DetermineIfStaticTestDefinition(testInfo);
+                
+                using (writer.BeginBlock("try"))
+                {
 
-                if (canUseStaticDefinition)
-                {
-                    writer.AppendLine("var testDescriptors = new System.Collections.Generic.List<ITestDescriptor>();");
-                    writer.AppendLine();
-                    GenerateStaticTestDefinitions(writer, testInfo, className, methodName, requiredProperties, constructorWithParameters, hasParameterlessConstructor);
-                    writer.AppendLine();
-                    writer.AppendLine("TestSourceRegistrar.RegisterTests(testDescriptors);");
+                    if (canUseStaticDefinition)
+                    {
+                        writer.AppendLine("var testDescriptors = new System.Collections.Generic.List<ITestDescriptor>();");
+                        writer.AppendLine();
+                        GenerateStaticTestDefinitions(writer, testInfo, className, methodName, requiredProperties, constructorWithParameters, hasParameterlessConstructor);
+                        writer.AppendLine();
+                        writer.AppendLine("TestSourceRegistrar.RegisterTests(testDescriptors);");
+                    }
+                    else
+                    {
+                        writer.AppendLine("var testMetadata = new System.Collections.Generic.List<DynamicTestMetadata>();");
+                        writer.AppendLine();
+                        GenerateDynamicTestMetadata(writer, testInfo, className, methodName, requiredProperties, constructorWithParameters, hasParameterlessConstructor);
+                        writer.AppendLine();
+                        writer.AppendLine("TestSourceRegistrar.RegisterTests(testMetadata.Cast<ITestDescriptor>().ToList());");
+                    }
                 }
-                else
+                
+                using (writer.BeginBlock("catch (System.Exception ex)"))
                 {
-                    writer.AppendLine("var testMetadata = new System.Collections.Generic.List<DynamicTestMetadata>();");
-                    writer.AppendLine();
-                    GenerateDynamicTestMetadata(writer, testInfo, className, methodName, requiredProperties, constructorWithParameters, hasParameterlessConstructor);
-                    writer.AppendLine();
-                    writer.AppendLine("TestSourceRegistrar.RegisterTests(testMetadata.Cast<ITestDescriptor>().ToList());");
+                    writer.AppendLine("// Register a failure test");
+                    writer.AppendLine("var errorMessage = \"Runtime initialization failed: \" + ex.GetType().Name + \": \" + ex.Message;");
+                    
+                    // Check which type we were trying to create
+                    if (canUseStaticDefinition)
+                    {
+                        writer.AppendLine("var testDescriptors = new System.Collections.Generic.List<ITestDescriptor>();");
+                        
+                        using (writer.BeginObjectInitializer($"var failureDef = new StaticTestDefinition", ";"))
+                        {
+                            writer.AppendLine($"TestId = \"{className}.{methodName}_RuntimeFailure_0\",");
+                            writer.AppendLine($"DisplayName = \"{methodName} [RUNTIME INITIALIZATION FAILED]\",");
+                            writer.AppendLine($"TestFilePath = @\"{testInfo.FilePath.Replace("\\", "\\\\").Replace("\"", "\\\"")}\",");
+                            writer.AppendLine($"TestLineNumber = {testInfo.LineNumber},");
+                            writer.AppendLine($"IsAsync = true,");
+                            writer.AppendLine($"IsSkipped = false,");
+                            writer.AppendLine($"SkipReason = null,");
+                            writer.AppendLine($"Timeout = null,");
+                            writer.AppendLine($"RepeatCount = 1,");
+                            writer.AppendLine($"TestClassType = typeof({className}),");
+                            writer.AppendLine($"TestMethodInfo = null,");
+                            writer.AppendLine($"ClassFactory = args => new {className}(),");
+                            writer.AppendLine($"MethodInvoker = async (instance, args) => throw new InvalidOperationException(errorMessage),");
+                            writer.AppendLine($"PropertyValuesProvider = () => new[] {{ new System.Collections.Generic.Dictionary<string, object?>() }},");
+                            writer.AppendLine($"ClassDataProvider = new TUnit.Core.EmptyDataProvider(),");
+                            writer.AppendLine($"MethodDataProvider = new TUnit.Core.EmptyDataProvider()");
+                        }
+                        
+                        writer.AppendLine("testDescriptors.Add(failureDef);");
+                        writer.AppendLine("TestSourceRegistrar.RegisterTests(testDescriptors);");
+                    }
+                    else
+                    {
+                        writer.AppendLine("var testMetadata = new System.Collections.Generic.List<DynamicTestMetadata>();");
+                        
+                        using (writer.BeginObjectInitializer($"var failureMetadata = new DynamicTestMetadata", ";"))
+                        {
+                            writer.AppendLine($"TestIdTemplate = \"{className}.{methodName}_RuntimeFailure_{{{{TestIndex}}}}\",");
+                            writer.AppendLine($"TestClassTypeReference = {CodeGenerationHelpers.GenerateTypeReference(testInfo.TypeSymbol)},");
+                            writer.AppendLine($"TestClassType = typeof({className}),");
+                            writer.AppendLine($"TestClassFactory = args => throw new InvalidOperationException(errorMessage),");
+                            
+                            using (writer.BeginObjectInitializer($"MethodMetadata = new global::TUnit.Core.MethodMetadata", ","))
+                            {
+                                writer.AppendLine($"Type = typeof({className}),");
+                                writer.AppendLine($"TypeReference = {CodeGenerationHelpers.GenerateTypeReference(testInfo.TypeSymbol)},");
+                                writer.AppendLine($"Name = \"{methodName}_RuntimeFailure\",");
+                                writer.AppendLine($"GenericTypeCount = 0,");
+                                writer.AppendLine($"ReturnType = typeof(global::System.Threading.Tasks.Task),");
+                                writer.AppendLine($"ReturnTypeReference = global::TUnit.Core.TypeReference.CreateConcrete(\"System.Threading.Tasks.Task, System.Private.CoreLib\"),");
+                                writer.AppendLine($"Attributes = new global::TUnit.Core.AttributeMetadata[] {{ }},");
+                                writer.AppendLine($"Parameters = new global::TUnit.Core.ParameterMetadata[] {{ }},");
+                                
+                                using (writer.BeginObjectInitializer($"Class = new global::TUnit.Core.ClassMetadata", ","))
+                                {
+                                    writer.AppendLine($"Name = \"{testInfo.TypeSymbol.Name}\",");
+                                    writer.AppendLine($"Type = typeof({className}),");
+                                    writer.AppendLine($"TypeReference = {CodeGenerationHelpers.GenerateTypeReference(testInfo.TypeSymbol)},");
+                                    writer.AppendLine($"Namespace = \"{testInfo.TypeSymbol.ContainingNamespace.ToDisplayString()}\",");
+                                    writer.AppendLine($"Attributes = new global::TUnit.Core.AttributeMetadata[] {{ }},");
+                                    writer.AppendLine($"Properties = new global::TUnit.Core.PropertyMetadata[] {{ }},");
+                                    writer.AppendLine($"Parameters = new global::TUnit.Core.ParameterMetadata[] {{ }},");
+                                    writer.AppendLine($"Parent = null,");
+                                    
+                                    writer.AppendLine($"Assembly = new global::TUnit.Core.AssemblyMetadata");
+                                    writer.AppendLine("{");
+                                    writer.AppendLine($"    Name = \"{testInfo.TypeSymbol.ContainingAssembly.Name}\",");
+                                    writer.AppendLine($"    Attributes = new global::TUnit.Core.AttributeMetadata[] {{ }}");
+                                    writer.AppendLine("}");
+                                }
+                                
+                                writer.AppendLine($"ReflectionInformation = null");
+                            }
+                            
+                            writer.AppendLine($"TestFilePath = @\"{testInfo.FilePath.Replace("\\", "\\\\").Replace("\"", "\\\"")}\",");
+                            writer.AppendLine($"TestLineNumber = {testInfo.LineNumber},");
+                            writer.AppendLine($"ClassDataSources = System.Array.Empty<global::TUnit.Core.IDataSourceProvider>(),");
+                            writer.AppendLine($"MethodDataSources = System.Array.Empty<global::TUnit.Core.IDataSourceProvider>(),");
+                            writer.AppendLine($"PropertyDataSources = new System.Collections.Generic.Dictionary<System.Reflection.PropertyInfo, global::TUnit.Core.IDataSourceProvider>(),");
+                            writer.AppendLine($"DisplayNameTemplate = \"{methodName} [RUNTIME INITIALIZATION FAILED]\",");
+                            writer.AppendLine($"RepeatCount = 1,");
+                            writer.AppendLine($"IsAsync = true,");
+                            writer.AppendLine($"IsSkipped = false,");
+                            writer.AppendLine($"SkipReason = null,");
+                            writer.AppendLine($"Timeout = null");
+                        }
+                        
+                        writer.AppendLine("testMetadata.Add(failureMetadata);");
+                        writer.AppendLine("TestSourceRegistrar.RegisterTests(testMetadata.Cast<ITestDescriptor>().ToList());");
+                    }
                 }
             }
         }
@@ -231,8 +329,8 @@ public class TestMetadataGenerator : IIncrementalGenerator
                     writer.AppendLine($"DisplayNameTemplate = \"{methodName} [SOURCE GENERATION FAILED]\",");
                     writer.AppendLine($"RepeatCount = 1,");
                     writer.AppendLine($"IsAsync = true,");
-                    writer.AppendLine($"IsSkipped = true,");
-                    writer.AppendLine($"SkipReason = \"Source generation failed: {ex.Message.Replace("\"", "\\\"")}\",");
+                    writer.AppendLine($"IsSkipped = false,");
+                    writer.AppendLine($"SkipReason = null,");
                     writer.AppendLine($"Timeout = null");
                 }
                 
