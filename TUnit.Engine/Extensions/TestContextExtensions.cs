d53e1c1823f3301163fd075ba495418590bbf471
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-07-09 22:58:12 +0100

    GetEligibleEventObjects

---
+++ TUnit.Engine/Extensions/TestContextExtensions.cs
@@ -0,0 +1,20 @@
+﻿using TUnit.Core;
+
+namespace TUnit.Engine.Extensions;
+
+internal static class TestContextExtensions
+{
+    public static IEnumerable<object> GetEligibleEventObjects(this TestContext testContext)
+    {
+        return GetInternal(testContext).OfType<object>();
+    }
+
+    private static IEnumerable<object?> GetInternal(TestContext testContext) =>
+    [
+        testContext.Events,
+        ..testContext.TestDetails.Attributes,
+        ..testContext.TestDetails.TestMethodArguments,
+        ..testContext.TestDetails.TestClassArguments,
+        ..testContext.TestDetails.TestClassInjectedPropertyArguments.Select(x => x.Value),
+    ];
+}
