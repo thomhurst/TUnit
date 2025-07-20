06659920ca0045c18d34428996940d6c18d689b2
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-07-20 15:22:20 +0100

    Remove junk files

--- test_generic_combinations.ps1
+++
@@ -1,3 +0,0 @@
-Write-Host "Testing generic test combinations..."
-Set-Location TUnit.TestProject
-dotnet test --filter "FullyQualifiedName~SimpleGenericClassTests" --logger "console;verbosity=minimal" 2>&1 | Select-String -Pattern "Passed|Failed|Total|Error|test" | ForEach-Object { Write-Host $_ }
