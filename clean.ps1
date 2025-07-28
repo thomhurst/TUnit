26b1310a466c44203a444c470a5c4ff8d293497c
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-07-28 23:26:32 +0100

    Tidy snaps

--- clean.ps1
+++
@@ -1,1 +0,0 @@
-Get-ChildItem .\ -include bin,obj -Recurse | foreach ($_) { remove-item $_.fullname -Force -Recurse }
--- test-classhooks.ps1
+++
@@ -1,2 +0,0 @@
-cd TUnit.TestProject
-dotnet test -f net9.0 --no-build --filter "FullyQualifiedName~ClassHooks.Test1"
