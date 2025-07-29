f46080f542d80374974afd3f9dacbed44aee5734
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-07-29 20:11:30 +0100

    chore: add dotnet-tools configuration for template authoring

--- TUnit.Templates.Tests/Snapshots/InstantiationTest.TUnit.AspNet._.verified/TUnit.AspNet/TUnit.AspNet/GlobalSetup.cs
+++
@@ -1,4 +0,0 @@
-﻿// Here you could define global logic that would affect all tests
-
-// You can use attributes at the assembly level to apply to all tests in the assembly
-[assembly: System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
--- TUnit.Templates.Tests/Snapshots/InstantiationTestWithFSharp.TUnit.AspNet.FSharp._.verified/TUnit.AspNet.FSharp/TUnit.AspNet.FSharp/GlobalSetup.fs
+++
@@ -1,7 +0,0 @@
-﻿namespace TUnit.AspNet.FSharp
-
-// Here you could define global logic that would affect all tests
-
-// You can use attributes at the assembly level to apply to all tests in the assembly
-[<assembly: System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage>]
-do ()
