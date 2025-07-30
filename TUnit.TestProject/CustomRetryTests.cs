e7a5e4db4d8f7b1ec533cf568d184c7d325ae952
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-07-31 00:03:23 +0100

    refactor: rename retry count variables to execution count for clarity

--- TUnit.TestProject/CustomRetryTests.cs
+++ TUnit.TestProject/CustomRetryTests.cs
@@ -3,16 +3,16 @@
 [RetryOperationCancelledException(3), NotInParallel(nameof(CustomRetryTests), Order = 1)]
 public class CustomRetryTests
 {
-    public static int RetryCount1 { get; private set; }
-    public static int RetryCount2 { get; private set; }
-    public static int RetryCount3 { get; private set; }
-    public static int RetryCount4 { get; private set; }
+    public static int ExecutionCount1 { get; private set; }
+    public static int ExecutionCount2 { get; private set; }
+    public static int ExecutionCount3 { get; private set; }
+    public static int ExecutionCount4 { get; private set; }
 
     [Test]
     [RetryOperationCancelledException(1)]
     public void One()
     {
-        RetryCount1++;
+        ExecutionCount1++;
         throw new OperationCanceledException();
     }
 
@@ -20,31 +20,31 @@ public class CustomRetryTests
     [RetryNullReferenceException(2)]
     public void Two()
     {
-        RetryCount2++;
+        ExecutionCount2++;
         throw new OperationCanceledException();
     }
 
     [Test]
     public void Three()
     {
-        RetryCount3++;
+        ExecutionCount3++;
         throw new OperationCanceledException();
     }
 
     [Test]
     public void Four()
     {
-        RetryCount4++;
+        ExecutionCount4++;
         throw new NullReferenceException();
     }
 
     [Test, NotInParallel(nameof(CustomRetryTests), Order = 2)]
     public async Task AssertCounts()
     {
-        await Assert.That(RetryCount1).IsEqualTo(2);
-        await Assert.That(RetryCount2).IsEqualTo(1);
-        await Assert.That(RetryCount3).IsEqualTo(4);
-        await Assert.That(RetryCount4).IsEqualTo(1);
+        await Assert.That(ExecutionCount1).IsEqualTo(2);
+        await Assert.That(ExecutionCount2).IsEqualTo(1);
+        await Assert.That(ExecutionCount3).IsEqualTo(4);
+        await Assert.That(ExecutionCount4).IsEqualTo(1);
     }
 
     public class RetryOperationCancelledExceptionAttribute(int times) : RetryAttribute(times)
