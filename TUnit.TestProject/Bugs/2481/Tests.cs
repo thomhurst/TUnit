f023aae49dbbaa4b8feb8fea570b75b3be4649b0
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-07-30 21:44:39 +0100

    fix: remove obsolete test classes from TUnit.Engine.Tests suite

--- TUnit.TestProject/Bugs/2481/Tests.cs
+++ TUnit.TestProject/Bugs/2481/Tests.cs
@@ -1,5 +1,8 @@
-﻿namespace TUnit.TestProject.Bugs._2481;
+﻿using TUnit.TestProject.Attributes;
 
+namespace TUnit.TestProject.Bugs._2481;
+
+[EngineTest(ExpectedResult.Pass)]
 public class Tests
 {
     [Test]
