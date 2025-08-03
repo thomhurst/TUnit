1afbbf2402f69b6760ae00b8f9f31653d54f6405
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-08-03 13:18:10 +0100

    fix: refactor metadata creation methods for improved clarity and consistency

--- TUnit.Engine/Building/MetadataBuilder.cs
+++ TUnit.Engine/Building/MetadataBuilder.cs
@@ -11,70 +11,47 @@ internal static class MetadataBuilder
     }
 
     [UnconditionalSuppressMessage("AOT", "IL2067:'Type' argument does not satisfy 'DynamicallyAccessedMemberTypes' in call to 'TUnit.Core.ParameterMetadata.ParameterMetadata(Type)'", Justification = "Parameter types are known at compile time")]
-    private static ParameterMetadata CreateParameterMetadata(Type parameterType, string? name, int index, System.Reflection.ParameterInfo? reflectionInfo = null)
+    private static ParameterMetadata CreateParameterMetadata(Type parameterType, string? name, int index, System.Reflection.ParameterInfo reflectionInfo)
     {
         return new ParameterMetadata(parameterType)
         {
             Name = name ?? $"param{index}",
             TypeReference = CreateTypeReference(parameterType),
-            ReflectionInfo = reflectionInfo
+            ReflectionInfo = reflectionInfo,
+            Type = parameterType,
+            IsNullable = parameterType.IsGenericType && parameterType.GetGenericTypeDefinition() == typeof(Nullable<>)
+                || Nullable.GetUnderlyingType(parameterType) != null
         };
     }
     [UnconditionalSuppressMessage("AOT", "IL2072:'value' argument does not satisfy 'DynamicallyAccessedMemberTypes' in call to 'TUnit.Core.ClassMetadata.Type.init'", Justification = "Type annotations are handled by source generators")]
     [UnconditionalSuppressMessage("AOT", "IL2070:'this' argument does not satisfy 'DynamicallyAccessedMemberTypes.PublicConstructors' in call to 'System.Type.GetConstructors(BindingFlags)'", Justification = "Constructor discovery needed for metadata")]
-    private static ClassMetadata CreateClassMetadataInternal(Type type)
+    private static ClassMetadata CreateClassMetadata(Type type)
     {
-        return ClassMetadata.GetOrAdd(type.FullName ?? type.Name, () => 
+        return ClassMetadata.GetOrAdd(type.FullName ?? type.Name, () =>
         {
             // Get constructor parameters for the class
             var constructors = type.GetConstructors(System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance);
             var constructor = constructors.FirstOrDefault();
-            
+
             var constructorParameters = constructor?.GetParameters()
                 .Select((p, i) => CreateParameterMetadata(p.ParameterType, p.Name, i, p))
                 .ToArray() ?? [];
-            
+
             return new ClassMetadata
             {
                 Name = type.Name,
                 Type = type,
                 TypeReference = CreateTypeReference(type),
                 Namespace = type.Namespace ?? string.Empty,
-                Assembly = AssemblyMetadata.GetOrAdd(type.Assembly.FullName ?? "Unknown", () => new AssemblyMetadata
+                Assembly = AssemblyMetadata.GetOrAdd(type.Assembly.FullName, () => new AssemblyMetadata
                 {
-                    Name = type.Assembly.GetName().Name ?? "Unknown",
+                    Name = type.Assembly.GetName().Name ?? "Unknown"
                 }),
                 Parameters = constructorParameters,
                 Properties = [],
-                Parent = null,
+                Parent = null
             };
         });
-    }
-
-    public static ClassMetadata CreateClassMetadata(TestMetadata metadata)
-    {
-        return CreateClassMetadataInternal(metadata.TestClassType);
-    }
-
-    [UnconditionalSuppressMessage("AOT", "IL2072:'value' argument does not satisfy 'DynamicallyAccessedMemberTypes' in call to 'TUnit.Core.MethodMetadata.Type.init'", Justification = "Type annotations are handled by source generators")]
-    [UnconditionalSuppressMessage("AOT", "IL2067:'Type' argument does not satisfy 'DynamicallyAccessedMemberTypes' in call to 'TUnit.Core.ParameterMetadata.ParameterMetadata(Type)'", Justification = "Parameter types are known at compile time")]
-    public static MethodMetadata CreateMethodMetadata(TestMetadata metadata)
-    {
-        var parameters = metadata.ParameterTypes
-            .Select((type, index) => CreateParameterMetadata(type, null, index))
-            .ToArray();
-
-        return new MethodMetadata
-        {
-            Name = metadata.TestMethodName,
-            Type = metadata.TestClassType,
-            TypeReference = CreateTypeReference(metadata.TestClassType),
-            Class = CreateClassMetadata(metadata),
-            Parameters = parameters,
-            GenericTypeCount = 0,
-            ReturnTypeReference = CreateTypeReference(typeof(Task)),
-            ReturnType = typeof(Task),
-        };
     }
 
     /// <summary>
@@ -90,13 +67,13 @@ internal static class MetadataBuilder
             Name = method.Name,
             Type = type,
             TypeReference = CreateTypeReference(type),
-            Class = CreateClassMetadataInternal(type),
+            Class = CreateClassMetadata(type),
             Parameters = method.GetParameters()
                 .Select((p, i) => CreateParameterMetadata(p.ParameterType, p.Name ?? "unnamed", i, p))
                 .ToArray(),
             GenericTypeCount = method.IsGenericMethodDefinition ? method.GetGenericArguments().Length : 0,
             ReturnTypeReference = CreateTypeReference(method.ReturnType),
-            ReturnType = method.ReturnType,
+            ReturnType = method.ReturnType
         };
     }
 }
