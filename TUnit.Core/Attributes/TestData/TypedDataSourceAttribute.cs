00f22fd5849b8c8382dbc45a87ec916df38b4287
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-08-03 15:18:16 +0100

    feat: introduce ITypedDataSourceAttribute and TypedDataSourceAttribute for typed data source support

---
+++ TUnit.Core/Attributes/TestData/TypedDataSourceAttribute.cs
@@ -0,0 +1,17 @@
+﻿using TUnit.Core.Helpers;
+
+namespace TUnit.Core;
+
+public abstract class TypedDataSourceAttribute<T> : Attribute, ITypedDataSourceAttribute<T>
+{
+    public abstract IAsyncEnumerable<Func<Task<T>>> GetTypedDataRowsAsync(DataGeneratorMetadata dataGeneratorMetadata);
+    
+    public async IAsyncEnumerable<Func<Task<object?[]?>>> GetDataRowsAsync(DataGeneratorMetadata dataGeneratorMetadata)
+    {
+        await foreach (var row in GetTypedDataRowsAsync(dataGeneratorMetadata))
+        {
+            var result = await row();
+            yield return () => Task.FromResult<object?[]?>(result.ToObjectArray());
+        }
+    }
+}
