ef07598b229fe931f7460a2889078550b4470a51
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-08-04 13:39:20 +0100

    Fix Polyfill issue

---
+++ TUnit.TestProject.Library/Polyfills/ModuleInitializerAttribute.cs
@@ -0,0 +1,5 @@
+﻿// ReSharper disable once CheckNamespace
+namespace System.Runtime.CompilerServices;
+
+// ReSharper disable once RedundantExtendsListEntry
+sealed partial class ModuleInitializerAttribute : Attribute;
