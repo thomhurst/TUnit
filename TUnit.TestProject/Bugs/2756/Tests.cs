7fd1b939c59f283d81c2ce2edbd6542d9d299fa2
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-08-07 12:18:50 +0100

    feat: Add test class for performance measurement with dynamic data source

---
+++ TUnit.TestProject/Bugs/2756/Tests.cs
@@ -0,0 +1,27 @@
+﻿namespace TUnit.TestProject.Bugs._2756;
+
+public class Tests
+{
+    private static DateTimeOffset? _start;
+
+    [Test]
+    [MethodDataSource(nameof(Ints))]
+    public async Task Test1(int i)
+    {
+        await Assert.That(i).IsGreaterThan(0);
+    }
+
+    public static IEnumerable<int> Ints() => Enumerable.Range(1, 10000);
+
+    [After(Class)]
+    public static void Before()
+    {
+        _start = DateTimeOffset.UtcNow;
+    }
+
+    [After(Class)]
+    public static void After()
+    {
+        Console.WriteLine(@$"Test duration: {(DateTimeOffset.UtcNow - _start!.Value).TotalMilliseconds} ms");
+    }
+}
