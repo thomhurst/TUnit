befce90d1c537382c0f7b25c49999f6edcde594f
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-09-12 23:16:31 +0100

    feat: add path assertion extensions and verified tests for path assertions

--- TUnit.Assertions.SourceGenerator.Tests/AssertionMethodGeneratorTests.cs
+++ TUnit.Assertions.SourceGenerator.Tests/AssertionMethodGeneratorTests.cs
@@ -9,48 +9,33 @@ internal class AssertionMethodGeneratorTests : TestsBase<AssertionMethodGenerato
             "TUnit.Assertions",
             "Assertions",
             "CharAssertionExtensions.cs"),
-        async generatedFiles =>
-        {
-            await Verify(generatedFiles);
-        });
+        _ => Task.CompletedTask);
 
     [Test]
     public Task GeneratesEnumAssertions() => RunTest(Path.Combine(Sourcy.Git.RootDirectory.FullName,
             "TUnit.Assertions",
             "Assertions",
             "EnumAssertionExtensions.cs"),
-        async generatedFiles =>
-        {
-            await Verify(generatedFiles);
-        });
+        _ => Task.CompletedTask);
 
     [Test]
     public Task GeneratesPathAssertions() => RunTest(Path.Combine(Sourcy.Git.RootDirectory.FullName,
             "TUnit.Assertions",
             "Assertions",
             "PathAssertionExtensions.cs"),
-        async generatedFiles =>
-        {
-            await Verify(generatedFiles);
-        });
+        _ => Task.CompletedTask);
 
     [Test]
     public Task GeneratesStringAssertions() => RunTest(Path.Combine(Sourcy.Git.RootDirectory.FullName,
             "TUnit.Assertions",
             "Assertions",
             "StringAssertionExtensions.cs"),
-        async generatedFiles =>
-        {
-            await Verify(generatedFiles);
-        });
+        _ => Task.CompletedTask);
 
     [Test]
     public Task GeneratesUriAssertions() => RunTest(Path.Combine(Sourcy.Git.RootDirectory.FullName,
             "TUnit.Assertions",
             "Assertions",
             "UriAssertionExtensions.cs"),
-        async generatedFiles =>
-        {
-            await Verify(generatedFiles);
-        });
+        _ => Task.CompletedTask);
 }
