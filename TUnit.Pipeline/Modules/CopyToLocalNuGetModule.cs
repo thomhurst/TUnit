f4d48528bcae181296a0858f7f47c2f21d9216e5
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-09-13 02:05:06 +0100

    feat: enable overwriting of existing NuGet packages during local copy

--- TUnit.Pipeline/Modules/CopyToLocalNuGetModule.cs
+++ TUnit.Pipeline/Modules/CopyToLocalNuGetModule.cs
@@ -12,6 +12,6 @@ public class CopyToLocalNuGetModule : Module<List<File>>
     protected override async Task<List<File>?> ExecuteAsync(IPipelineContext context, CancellationToken cancellationToken)
     {
         var folder = await GetModule<AddLocalNuGetRepositoryModule>();
-        return context.Git().RootDirectory.GetFiles(x => x.Extension.EndsWith("nupkg")).Select(x => x.CopyTo(folder.Value!)).ToList();
+        return context.Git().RootDirectory.GetFiles(x => x.Extension.EndsWith("nupkg")).Select(x => x.CopyTo(folder.Value!, overwrite: true)).ToList();
     }
 }
