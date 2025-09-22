fa8ebba50b42dd481d3b4cfeee41cd07c11dd051
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-09-22 20:25:58 +0100

    fix: improve formatting for floating-point values in different cultures (#3178)
    * fix: improve formatting for floating-point values in different cultures
    
    * fix: update floating-point arguments to use double precision in tests

---
+++ TUnit.Core.SourceGenerator/Extensions/ObjectExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace TUnit.Core.SourceGenerator.Extensions;
+
+public static class ObjectExtensions
+{
+    public static string? ToInvariantString(this object? obj)
+    {
+        if(obj is IFormattable formattable)
+        {
+            return formattable.ToString(null, System.Globalization.CultureInfo.InvariantCulture);
+        }
+
+        return obj?.ToString();
+    }
+}
