2a936ae85b2a45b7743244eec4758a0b58d0561e
Martin Gasser <m-gasser@gmx.net>
2025-09-22 22:27:24 +0200

    Ensure assertion data from async lambdas is not executed too early.

--- TUnit.Assertions/AssertConditions/Interfaces/ISource.cs
+++ TUnit.Assertions/AssertConditions/Interfaces/ISource.cs
@@ -6,7 +6,7 @@ public interface ISource
 {
     string? ActualExpression { get; }
     internal Stack<BaseAssertCondition> Assertions { get; }
-    internal ValueTask<AssertionData> AssertionDataTask { get; }
+    internal LazyAssertionData LazyAssertionData { get; }
     internal StringBuilder ExpressionBuilder { get; }
 
     ISource AppendExpression(string expression);
--- TUnit.Assertions/AssertConditions/Operators/ValueDelegateOr.cs
+++ TUnit.Assertions/AssertConditions/Operators/ValueDelegateOr.cs
@@ -14,7 +14,7 @@ public class ValueDelegateOr<TActual>(AssertionBuilder assertionBuilder) : IValu
     string? ISource.ActualExpression => ((ISource) assertionBuilder).ActualExpression;
 
     Stack<BaseAssertCondition> ISource.Assertions => ((ISource) assertionBuilder).Assertions;
-    ValueTask<AssertionData> ISource.AssertionDataTask => ((ISource) assertionBuilder).AssertionDataTask;
+    LazyAssertionData ISource.LazyAssertionData => ((ISource) assertionBuilder).LazyAssertionData;
     StringBuilder ISource.ExpressionBuilder => ((ISource) assertionBuilder).ExpressionBuilder;
 
     ISource ISource.AppendExpression(string expression)
