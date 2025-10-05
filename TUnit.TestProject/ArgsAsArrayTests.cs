145a92935d0a8ae0a28287c565e0b253b1794b36
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-10-05 19:52:33 +0100

    feat: enhance parameter handling in test sources and add new tests for params keyword usage (#3292)

--- TUnit.TestProject/ArgsAsArrayTests.cs
+++ TUnit.TestProject/ArgsAsArrayTests.cs
@@ -14,16 +14,6 @@ public class ArgsAsArrayTests
 
     [Test]
     [Arguments("arg1", "arg2", "arg3")]
-    public void NonParams(string[] arguments)
-    {
-        foreach (var argument in arguments)
-        {
-            Console.WriteLine(argument);
-        }
-    }
-
-    [Test]
-    [Arguments("arg1", "arg2", "arg3")]
     public void ParamsEnumerable(params IEnumerable<string> arguments)
     {
         foreach (var argument in arguments)
@@ -33,18 +23,8 @@ public class ArgsAsArrayTests
     }
 
     [Test]
-    [Arguments("arg1", "arg2", "arg3")]
-    public void Enumerable(IEnumerable<string> arguments)
-    {
-        foreach (var argument in arguments)
-        {
-            Console.WriteLine(argument);
-        }
-    }
-
-    [Test]
     [Arguments(1, "arg1", "arg2", "arg3")]
-    public void Following_Non_Params(int i, IEnumerable<string> arguments)
+    public void Following_Non_Params(int i, params IEnumerable<string> arguments)
     {
         foreach (var argument in arguments)
         {
