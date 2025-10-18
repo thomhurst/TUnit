450e32a459052b68edbd64b25e042ead4fc549d2
Adem Catamak <ademcatamak@gmail.com>
2025-10-18 20:04:14 +0200

    Playwright | additional constructor for page testing (#3418)
    * Playwright | additional constructor for page testing
    
    * remove redundant base
    
    * Public Api Snapshot Files

--- TUnit.Playwright/ContextTest.cs
+++ TUnit.Playwright/ContextTest.cs
@@ -5,15 +5,19 @@ namespace TUnit.Playwright;
 
 public class ContextTest : BrowserTest
 {
+    public ContextTest()
+    {
+    }
+
+    public ContextTest(BrowserTypeLaunchOptions options) : base(options)
+    {
+    }
+
     public IBrowserContext Context { get; private set; } = null!;
 
     public virtual BrowserNewContextOptions ContextOptions(TestContext testContext)
     {
-        return new()
-        {
-            Locale = "en-US",
-            ColorScheme = ColorScheme.Light,
-        };
+        return new() { Locale = "en-US", ColorScheme = ColorScheme.Light, };
     }
 
     [Before(HookType.Test, "", 0)]
@@ -23,7 +27,7 @@ public class ContextTest : BrowserTest
         {
             throw new InvalidOperationException($"Browser is not initialized. This may indicate that {nameof(BrowserTest)}.{nameof(BrowserSetup)} did not execute properly.");
         }
-        
+
         Context = await NewContext(ContextOptions(testContext)).ConfigureAwait(false);
     }
 }
