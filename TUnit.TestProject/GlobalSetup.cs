968c2d44d3a4dbd3897f3a95a7f807bf7d6c0289
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-10-25 22:43:08 +0100

    feat: implement AOT converter registration in GlobalSetup and enhance AotConverterRegistry

---
+++ TUnit.TestProject/GlobalSetup.cs
@@ -0,0 +1,18 @@
+using TUnit.Core.Converters;
+
+namespace TUnit.TestProject;
+
+public class GlobalSetup
+{
+    [Before(TestDiscovery)]
+    public void SetupAotConverters()
+    {
+        AotConverterRegistry.Register<string, MixedMatrixTestsUnion1>(value => new MixedMatrixTestsUnion1(value));
+        AotConverterRegistry.Register<MixedMatrixTests.Enum4, MixedMatrixTestsUnion1>(value => new MixedMatrixTestsUnion1(value));
+        AotConverterRegistry.Register<MixedMatrixTests.Enum5, MixedMatrixTestsUnion1>(value => new MixedMatrixTestsUnion1(value));
+
+        AotConverterRegistry.Register<string, MixedMatrixTestsUnion2>(value => new MixedMatrixTestsUnion2(value));
+        AotConverterRegistry.Register<MixedMatrixTests.Enum4, MixedMatrixTestsUnion2>(value => new MixedMatrixTestsUnion2(value));
+        AotConverterRegistry.Register<MixedMatrixTests.Enum5, MixedMatrixTestsUnion2>(value => new MixedMatrixTestsUnion2(value));
+    }
+}
