0c170f71a1990b27b0907c682d2eb84aef5dbf05
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-10-28 18:59:07 +0000

    Remove experimental attribute from DynamicTestBuilderAttribute class

--- TUnit.Core/Attributes/DynamicTestBuilderAttribute.cs
+++ TUnit.Core/Attributes/DynamicTestBuilderAttribute.cs
@@ -3,5 +3,4 @@ using System.Runtime.CompilerServices;
 
 namespace TUnit.Core;
 
-[Experimental("TUnitWIP0001")]
 public class DynamicTestBuilderAttribute([CallerFilePath] string file = "", [CallerLineNumber] int line = 0) : BaseTestAttribute(file, line);
