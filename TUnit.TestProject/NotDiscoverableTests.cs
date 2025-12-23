eec7a054d9d83b04ec58bd6e72e7d18f7cc3cf83
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-12-23 23:15:11 +0000

    test: add conditional NotDiscoverable test

--- TUnit.TestProject/NotDiscoverableTests.cs
+++ TUnit.TestProject/NotDiscoverableTests.cs
@@ -38,3 +38,24 @@ public class NotDiscoverableClassTests
         // All tests in this class should be hidden from discovery
     }
 }
+
+public class ConditionalNotDiscoverableAttribute : NotDiscoverableAttribute
+{
+    public ConditionalNotDiscoverableAttribute() : base("Conditionally hidden") { }
+
+    public override Task<bool> ShouldHide(TestRegisteredContext context)
+    {
+        // Only hide if environment variable is set
+        return Task.FromResult(Environment.GetEnvironmentVariable("HIDE_TEST") == "true");
+    }
+}
+
+public class ConditionalNotDiscoverableTests
+{
+    [Test]
+    [ConditionalNotDiscoverable]
+    public void Test_WithConditionalNotDiscoverable_HidesBasedOnCondition()
+    {
+        // This test is hidden only when HIDE_TEST=true
+    }
+}
