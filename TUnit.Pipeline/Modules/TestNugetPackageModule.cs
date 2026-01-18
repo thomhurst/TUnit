b2e44061db9c2d1fb5ea222a56e7693da6ce867e
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2026-01-18 11:29:41 +0000

    fix: remove platform restrictions from FSharp/VB nuget test modules

--- TUnit.Pipeline/Modules/TestNugetPackageModule.cs
+++ TUnit.Pipeline/Modules/TestNugetPackageModule.cs
@@ -16,13 +16,11 @@ public class TestNugetPackageModule : AbstractTestNugetPackageModule
     public override string ProjectName => "TUnit.NugetTester.csproj";
 }
 
-[RunOnWindowsOnly, RunOnLinuxOnly]
 public class TestFSharpNugetPackageModule : AbstractTestNugetPackageModule
 {
     public override string ProjectName => "TUnit.NugetTester.FSharp.fsproj";
 }
 
-[RunOnWindowsOnly, RunOnLinuxOnly]
 public class TestVBNugetPackageModule : AbstractTestNugetPackageModule
 {
     public override string ProjectName => "TUnit.NugetTester.VB.vbproj";
