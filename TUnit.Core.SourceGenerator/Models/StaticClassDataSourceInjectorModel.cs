68ab409ce872ef43121b34789cd797fdc9671643
Timothy Makkison <timmakkison@gmail.com>
2026-01-27 12:16:14 +0000

    cleanup: remove dead and unused code

--- TUnit.Core.SourceGenerator/Models/StaticClassDataSourceInjectorModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TUnit.Core.SourceGenerator.Models;
-
-public record StaticClassDataSourceInjectorModel
-{
-    public required string FullyQualifiedTypeName { get; init; }
-    public required string PropertyName { get; init; }
-    public required string InjectableType { get; init; }
-    public required string MinimalTypeName { get; set; }
-}
