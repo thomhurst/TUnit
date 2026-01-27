ee788008e9e4a53b93753b7ddca3a7e53edf789a
Timothy Makkison <timmakkison@gmail.com>
2026-01-27 20:05:51 +0000

    feat: add source generator benchmarks

---
+++ TUnit.SourceGenerator.Benchmarks/Program.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkDotNet.Running;
+using Microsoft.Build.Locator;
+using TUnit.SourceGenerator.Benchmarks;
+
+MSBuildLocator.RegisterDefaults();
+
+if (args is { Length: > 0 })
+{
+    BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+}
+else
+{
+    BenchmarkRunner.Run<TestMetadataGeneratorBenchmarks>();
+}
---
+++ TUnit.SourceGenerator.Benchmarks/TestMetadataGeneratorBenchmarks.cs
@@ -0,0 +1,34 @@
+﻿using BenchmarkDotNet.Attributes;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.MSBuild;
+using TUnit.Core.SourceGenerator.Generators;
+using TUnit.SourceGenerator.Benchmarks;
+
+namespace TUnit.SourceGenerator.Benchmarks;
+
+[MemoryDiagnoser]
+[InProcess]
+public class TestMetadataGeneratorBenchmarks
+{
+    private const string SampleProjectPath = "../TUnit.TestProject/TUnit.TestProject.csproj";
+
+    private MSBuildWorkspace? _workspace;
+    private GeneratorDriver? _sampleDriver;
+    private Compilation? _sampleCompilation;
+
+    [GlobalSetup(Target = nameof(Compile))]
+    public void SetupCompile() =>
+        (_sampleCompilation, _sampleDriver, _workspace) =
+        WorkspaceHelper.SetupAsync<TestMetadataGenerator>(SampleProjectPath)
+            .GetAwaiter()
+            .GetResult();
+
+    [Benchmark]
+    public GeneratorDriver Compile() => _sampleDriver!.RunGeneratorsAndUpdateCompilation(_sampleCompilation!, out _, out _);
+
+    [GlobalCleanup]
+    public void Cleanup()
+    {
+        _workspace?.Dispose();
+    }
+}
