14c827aacd63e6b200f5a50000474896b4f376a6
Simon Cropp <simon.cropp@gmail.com>
2026-02-21 21:12:51 +1100

    avoid some redundant enumerable alloc

--- TUnit.Core/Attributes/TestData/AsyncUntypedDataSourceSourceGeneratorAttribute.cs
+++ TUnit.Core/Attributes/TestData/AsyncUntypedDataSourceSourceGeneratorAttribute.cs
@@ -10,14 +10,11 @@ public abstract class AsyncUntypedDataSourceGeneratorAttribute : Attribute, IAsy
 
     protected abstract IAsyncEnumerable<Func<Task<object?[]?>>> GenerateDataSourcesAsync(DataGeneratorMetadata dataGeneratorMetadata);
 
-    public async IAsyncEnumerable<Func<Task<object?[]?>>> GenerateAsync(DataGeneratorMetadata dataGeneratorMetadata)
+    public IAsyncEnumerable<Func<Task<object?[]?>>> GenerateAsync(DataGeneratorMetadata dataGeneratorMetadata)
     {
         // Data source initialization is now handled externally by DataSourceInitializer
         // This follows SRP - the attribute is only responsible for generating data, not initialization
-        await foreach (var generateDataSource in GenerateDataSourcesAsync(dataGeneratorMetadata))
-        {
-            yield return generateDataSource;
-        }
+        return GenerateDataSourcesAsync(dataGeneratorMetadata);
     }
 
     public IAsyncEnumerable<Func<Task<object?[]?>>> GetDataRowsAsync(DataGeneratorMetadata dataGeneratorMetadata)
