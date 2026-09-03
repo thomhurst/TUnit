efcad9198225ec12fd4885836e34ca988f6731af
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2026-09-03 22:41:50 +0100

    refactor: drop unused timeout constructor

--- src/TUnit.Core/Exceptions/TimeoutException.cs
+++ src/TUnit.Core/Exceptions/TimeoutException.cs
@@ -6,10 +6,6 @@ public class TimeoutException : TUnitException
     {
     }
 
-    internal TimeoutException(string? message, Exception? innerException) : base(message, innerException)
-    {
-    }
-
     private static string GetMessage(TimeSpan timeSpan)
     {
         return $"The test timed out after {timeSpan.TotalMilliseconds} milliseconds";
