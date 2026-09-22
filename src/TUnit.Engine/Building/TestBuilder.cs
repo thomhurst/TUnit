7b7a5c8ca469d1347fe37f3a7b6acd53c1c83ddc
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2026-09-22 21:07:30 +0100

    chore: drop stray BOM from TestBuilder.cs

--- src/TUnit.Engine/Building/TestBuilder.cs
+++ src/TUnit.Engine/Building/TestBuilder.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Concurrent;
+using System.Collections.Concurrent;
 using System.Diagnostics.CodeAnalysis;
 using Microsoft.Testing.Platform.Extensions.Messages;
 using Microsoft.Testing.Platform.Requests;
